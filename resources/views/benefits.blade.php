f80a064402bf0f1f169f23027666812d8bfacad4
Jay Potter <Jay.Potter@Materialink.com>
2016-02-12 23:16:25 -0500

    products description

--- resources/views/benefits.blade.php
+++ resources/views/benefits.blade.php
@@ -5,112 +5,76 @@
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
     <h1>Member Benefits</h1>
-    <div class="form-group benefits">
-                <div class="col-md-12">
-                   <p>Eco-nomix is a unique company dedicated in delivering the highest quality products to its customers. <br> This purpose will help them improve their lives <a href="\physically">physically</a>, <a href="\emotionally">emotionally</a>, <a href="\spiritually">spiritually</a> and <a href="\economically">economically</a>.
-                   </p>
-                </div>
-            </div>
-    <div class="form-group benefits">
-         <div class="col-md-12">
-            <p class="hangingindent">1. A member typically receives a <a href="\discount">10-30% discount</a> from the suggested retail price.  The membership fee is usually recovered in just the first few purchases of products.</p>
-         </div>
-    </div>
-    <div class="form-group benefits">
-         <div class="col-md-12">
-            <p class="hangingindent">2. The products carried by Eco-nomix are not typically carried by your local retailers.  We specialize in products that are made from recycled materials, helping you reduce your carbon footprint.  Products are also offered to help you
-            produce your own 100% organic foods, improve your gardening experience, and eliminate impact of pesticides in your families heath.  Another product area is helping you to reduce toxic levels of unhealthy materials in your food, water and home.  This off course promotes
-             healthier living.  We also offer products to help you reduce the power demands of your home and produce your own power, including solar and wind turbines.  You will also see products to improve your existing cars mileage and reduce pollution.  In addition, we are sources for multi-fuel back-up generators, to keep your lights on, when everything else fails.  Specialty Camping and Survival
-              products are also offered. </p>
-              <p class="indent"> The basic idea is to help you create a lifestyle that is in tune with nature, that is 100% sustainable and as independent as possible while helping to reduce the negative impact to the environment. Any product that can help you live better, on or off the grid, increase your spirituality, help you get through hard times and prepare for the future will eventually be available at Eco-nomix.</p>
-         </div>
-    </div>
-    <div class="form-group benefits">
-         <div class="col-md-12">
-            <p class="hangingindent">3. A member can promote the companies products and receive a <a href="\referral">referral fee</a> of 10% of the selling price to referred members.</p>
-         </div>
-    </div>
+<div class="form-group benefits">
+    <div class="col-md-12 indent">
+        <p>Eco-nomix is a unique company dedicated in delivering the highest quality products to its customers. <br> This purpose will help them improve their lives <a href="\physically">physically</a>, <a href="\emotionally">emotionally</a>, <a href="\spiritually">spiritually</a> and <a href="\economically">economically</a>.
+        </p>
 
-    <div class="form-group benefits">
-         <div class="col-md-12">
-            <p class="hangingindent">4. The referral fees continue for up to 5 generations of referrals in total.  The member receives a 4% referral fee on the 2nd to 5th generations.
-            Because the 5 generation of down-line members can grow both <a href="\limitations">extremely wide</a> and deep,  the number of people downline can be grow over time to a large organization.  Each sale through
-            5 generations will result in referral fees for the member.  This can start to add up pretty quickly.  Since there are no on-going fees to participate this could represent <a href="\potential">significant income</a>.</p>
-         </div>
-    </div>
+        <ol>
+            <li> <p>A member typically receives a <a href="\discount">10-30% discount</a> from the suggested retail price.  The membership fee is usually recovered in just the first few purchases of products.</p>
+            </li>
+            <li>
+                <p>The <a href="\selection">products</a> carried by Eco-nomix are not typically carried by your local retailers.  We specialize in products that are made from recycled materials, helping you reduce your carbon footprint.  Products are also offered to help you
+                produce your own 100% organic foods, improve your gardening experience, and eliminate impact of pesticides in your families heath.  Another product area is helping you to reduce toxic levels of unhealthy materials in your food, water and home.  This off course promotes
+                healthier living.  We also offer products to help you reduce the power demands of your home and produce your own power, including solar and wind turbines.  You will also see products to improve your existing cars mileage and reduce pollution.  In addition, we are sources for multi-fuel back-up generators, to keep your lights on, when everything else fails.  Specialty Camping and Survival
+                products are also offered.</p>
+                <p> The basic idea is to help you create a lifestyle that is in tune with nature, that is 100% sustainable and as independent as possible while helping to reduce the negative impact to the environment. Any product that can help you live better, on or off the grid, increase your spirituality, help you get through hard times and prepare for the future will eventually be available at Eco-nomix.</p>
 
-    <div class="form-group benefits">
-         <div class="col-md-12">
-             <p class="hangingindent">5. 1% of all sales are <a href="\donations">donated</a> to charities.  The member gets to determine where a portion of those proceeds will go. </p>
-         </div>
-    </div>
+            </li>
+            <li>
+                <p>A member can promote the companies products and receive a <a href="\referral">referral fee</a> of 10% of the selling price to referred members.</p>
+            </li>
+            <li>
+               <p>The referral fees continue for up to 5 generations of referrals in total.  The member receives a 4% referral fee on the 2nd to 5th generations.
+                Because the 5 generation of down-line members can grow both <a href="\limitations">extremely wide</a> and deep,  the number of people downline can be grow over time to a large organization.  Each sale through
+                5 generations will result in referral fees for the member.  This can start to add up pretty quickly.  Since there are no on-going fees to participate this could represent <a href="\potential">significant income</a>.</p>
 
-    <div class="form-group benefits">
-        <div class="col-md-12">
-            <p class="hangingindent">6. All referral fees are paid through a debit card as often as <a href="\transfers">requested by the member</a>.  No waiting to the end of the month for processing, no delays due to the mail.</p>
-        </div>
-    </div>
-    <div class="form-group benefits">
-       <div class="col-md-12">
-            <p class="hangingindent">7. All referral fees are credited immediately to the member.  The site provides <a href="\accounting">instant updates</a> so the member always is aware of makeup of their group, down through 5 generations and any fees that have been processed to their credit.  </p>
-       </div>
-    </div>
+            </li>
+            <li>
+                <p>1% of all sales are <a href="\donations">donated</a> to charities.  The member gets to determine where a portion of those proceeds will go. </p>
 
-    <div class="form-group benefits">
-        <div class="col-md-12">
-                <p class="hangingindent">8. The <a href="\debitcards">debit card</a> is mastercharge or visa card and can be used anywhere a major credit card is accepted including bank ATM's</p>
-        </div>
-    </div>
+            </li>
+            <li>
+                <p>All referral fees are paid through a debit card as often as <a href="\transfers">requested by the member</a>.  No waiting to the end of the month for processing, no delays due to the mail.</p>
+            </li>
+            <li>
+                <p>All referral fees are credited immediately to the member.  The site provides <a href="\accounting">instant updates</a> so the member always is aware of makeup of their group, down through 5 generations and any fees that have been processed to their credit.  </p>
 
-    <div class="form-group benefits">
-        <div class="col-md-12">
-            <p class="hangingindent">9. The member receives 100 <a href="\businesscards">business cards</a> with their name printed on them plus a member's referring link.  This link can be delivered even using electronic links.  This enables a member to refer someone simply by given them a card.  When the prospective member receives the link, they will be directed to this site.  If they join the referral is already established so you get the credit for the referral.</p>
-        </div>
-    </div>
-    <div class="form-group benefits">
-        <div class="col-md-12">
-            <p class="hangingindent">10. There are no <a href="\requirements">personal purchases</a> required to receive any referral fees from those that you refer.  All product is sold through this website.  You do not make any direct sales and collect no money from customers.</p>
-        </div>
-    </div>
-    <div class="form-group benefits">
-        <div class="col-md-12">
-            <p class="hangingindent">11. There are <a href="\membercost">no hidden fees</a> or charges.  Even a small sponsored organization can generate a positive income for the member.</p>
-        </div>
-    </div>
+            </li>
+            <li>
+                <p>The <a href="\debitcards">debit card</a> is mastercharge or visa card and can be used anywhere a major credit card is accepted including bank ATM's</p>
 
-    <div class="form-group benefits">
-        <div class="col-md-12">
-           <p class="hangingindent">12. The $35.00 <a href="\membercost">membership fee</a> is a one time fee, with no annual fees due.</p>
-        </div>
-    </div>
-    <div class="form-group benefits">
-        <div class="col-md-12">
-           <p class="hangingindent">13. The <a href="\selection">products chosen</a> to be marketed by Eco-nomix are determined first to be priced competitively, even against the major brand stores.  The products used high quality materials and have a benefit to the ecology.  All the products are tested against the manufactures claims and can be relied upon.
-           </p>
-        </div>
-    </div>
+            </li>
+            <li>
+                <p>The member receives 100 <a href="\businesscards">business cards</a> with their name printed on them plus a member's referring link.  This link can be delivered even using electronic links.  This enables a member to refer someone simply by given them a card.  When the prospective member receives the link, they will be directed to this site.  If they join the referral is already established so you get the credit for the referral.</p>
 
-    <div class="form-group benefits">
-        <div class="col-md-12">
-           <p class="hangingindent">14. Member's of Eco-nomix share their <a href="\experiences">experiences</a>, evaluate and recommend products.  You are not alone in deciding what products will benefit your family the most.
-           </p>
-        </div>
-    </div>
+            </li>
+            <li>
+                <p>There are no <a href="\requirements">personal purchases</a> required to receive any referral fees from those that you refer.  All product is sold through this website.  You do not make any direct sales and collect no money from customers.</p>
+            </li>
+            <li>
+                <p>There are <a href="\membercost">no hidden fees</a> or charges.  Even a small sponsored organization can generate a positive income for the member.</p>
+            </li>
+            <li>
+                <p>The $35.00 <a href="\membercost">membership fee</a> is a one time fee, with no annual fees due.</p>
+            </li>
+            <li>
+                <p>The <a href="\selection">products chosen</a> to be marketed by Eco-nomix are determined first to be priced competitively, even against the major brand stores.  The products used high quality materials and have a benefit to the ecology.  All the products are tested against the manufactures claims and can be relied upon.
+            </li>
+            <li>
+                <p>Member's of Eco-nomix share their <a href="\experiences">experiences</a>, evaluate and recommend products.  You are not alone in deciding what products will benefit your family the most.</p>
+            </li>
+            <li>
+                <p>There are no mandatory automatic <a href="\autoship">auto-ship products</a>.  (Yes there will be some products where you can opt to receive them by auto-ship for a lower price).
+                No filling up your garage with unwanted or overstocked products.</p>
+            </li>
+            <li>
+                <p>Eco-nomix is poised to create the maximum income potential in <a href="\comparison">comparison</a> to other Multi-Level marketing programs.
+            </li>
+        </ol>
 
-    <div class="form-group benefits">
-        <div class="col-md-12">
-           <p class="hangingindent">15. There are no mandatory automatic <a href="\autoship">auto-ship products</a>.  (Yes there will be some products where you can opt to receive them by auto-ship for a lower price).
-           No filling up your garage with unwanted or overstocked products.
-           </p>
-        </div>
-    </div>
 
-    <div class="form-group benefits">
-        <div class="col-md-12">
-           <p class="hangingindent">16. Eco-nomix is poised to create the maximum income potential in <a href="\comparison">comparison</a> to other Multi-Level marketing programs.
-           </p>
-        </div>
-    </div>
+
 
     <div class="form-group benefits">
        <div class="col-md-12 standout">
@@ -119,6 +83,7 @@
              charitible organizations that are making worthwhile contributions to the planet. </p><p> And last of all is an on-going income stream that is coming to you, not because of having to make constant purchases,  but because of
              simple referrals to an organization that thinks like you.</p><p> At last there is a company you can look forward to working with.</p>
        </div>
+    </div>
     </div>
 
 </div>
--- resources/views/layouts/navigation2.blade.php
+++ resources/views/layouts/navigation2.blade.php
@@ -1,84 +1,80 @@
-{{--@set('sealed', isset($sealed_bid_id) && ($sealed_bid_status == 'sealed)))--}}
-<div id="navcontainer" >
-    {{--<div class="container-fluid">--}}
 
+    <div class="navcontainer" >
         <div class="navbar-brand brand-image">
-                <a href="/test/"><img src="/images/Economix3731_Fotor.jpg" width="150" height="24" /></a>
+             <a href="/test/"><img src="/images/Economix3731_Fotor.jpg" width="150" height="24" /></a>
+        </div>
+        <div style="padding:13px;">
+            <ul>
+                <li><a href="/products">Products<span class="caret"></span></a>
+                    <ul>
+                        <li><a href="/selection">Product Selection</a></li>
+                        <li><a href="/food">Food Production</a></li>
+                        <li><a href="/water">Water Purification</a></li>
+                        <li><a href="/energy">Energy Production</a></li>
+                        <li><a href="/recycling">Recycling</a></li>
+                        <li><a href="/camping">Survival, Camping</a></li>
+                        <li><a href="/cooking">Cooking Systems</a></li>
+                        <li><a href="/health">Home Health</a></li>
+                        <li><a href="/house">Household Products</a></li>
+                        <li><a href="/books">Books and Guides</a></li>
+                        <li><a href="/training">Training</a></li>
+                        <li><a href="/autoship">Auto-Ship Policy</a></li>
+                    </ul>
+                </li>
+                <li><a href="/people">People<span class="caret"></span></a>
+                    <ul>
+                        <li><a href="/founders">Founders</a></li>
+                        <li><a href="/members">Members</a></li>
+                        <li><a href="/charities">Charities</a></li>
+                        <li><a href="/groups">Sponsored Groups</a></li>
+                        <li><a href="/experiences">Experiences</a></li>
+                    </ul>
+                </li>
+                <li><a href="/purpose">Purpose<span class="caret"></span></a>
+                    <ul>
+                        <li><a href="/physically">Physically</a></li>
+                        <li><a href="/emotionally">Emotionally</a></li>
+                        <li><a href="/spiritually">Spiritually</a></li>
+                        <li><a href="/economically">Economically</a></li>
+                    </ul>
+                </li>
+                <li><a href="/plans">Plans<span class="caret"></span></a>
+                    <ul>
+                        <li><a href="/membercost">Membership Cost</a></li>
+                        <li><a href="/discount">Member Discount</a></li>
+                        <li><a href="/referral">Referral Fees</a></li>
+                        <li><a href="/debitcards">Debit Card</a></li>
+                        <li><a href="/businesscards">Business Cards</a></li>
+                        <li><a href="/requirements">Requirements for Bonuses</a></li>
+                        <li><a href="/limitations">Limitations on Recruiting</a></li>
+                        <li><a href="/accounting">On-line Accounting</a></li>
+                        <li><a href="/transfers">Instant Transfers</a></li>
+                        <li><a href="/potential">Potentials</a></li>
+                        <li><a href="/donations">Donations</a></li>
+                        <li><a href="/comparison">Eco-nomix compared to Multi-Level</a></li>
+                        <li><a href="/benefits">Member Benefits</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="pull-right">
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+
+                @if ($user_name == '' or $user_name == null )
+                    <li><a href="/login">Login</a></li>
+                    <li><a href="/register">Register</a></li>
+                @else
+
+                    <li><a href="/homepage/{{$user_id}}">{{$user_name}}<span class="caret"></span></a>
+                        <ul>
+                            <li><a href="/homepage/{{$user_id}}">Home Page</a></li>
+                            <li><a href="/money/{{$user_id}}">Got Money</a></li>
+                            <li><a href="/logout2">Logout</a></li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+
         </div>
 
-
-        <div style="padding:5,0,0,0">
-
-<ul id="drop-nav" >
-  <li><a href="/products">Products<span class="caret"></span></a>
-      <ul>
-        <li><a href="/selection">Product Selection</a></li>
-        <li><a href="/food">Food Production</a></li>
-        <li><a href="/water">Water Purification</a></li>
-        <li><a href="/energy">Energy Production</a></li>
-        <li><a href="/recycling">Recycling</a></li>
-        <li><a href="/camping">Survival, Camping</a></li>
-        <li><a href="/cooking">Cooking Systems</a></li>
-        <li><a href="/health">Home Health</a></li>
-        <li><a href="/house">Household Products</a></li>
-        <li><a href="/books">Books and Guides</a></li>
-        <li><a href="/training">Training</a></li>
-        <li><a href="/autoship">Auto-Ship Policy</a></li>
-      </ul>
-    </li>
-  <li><a href="/people">People<span class="caret"></span></a>
-      <ul>
-        <li><a href="/founders">Founders</a></li>
-        <li><a href="/members">Members</a></li>
-        <li><a href="/charities">Charities</a></li>
-        <li><a href="/groups">Sponsored Groups</a></li>
-        <li><a href="/experiences">Experiences</a></li>
-      </ul>
-    </li>
-  <li><a href="/purpose">Purpose<span class="caret"></span></a>
-    <ul>
-      <li><a href="/physically">Physically</a></li>
-      <li><a href="/emotionally">Emotionally</a></li>
-      <li><a href="/spiritually">Spiritually</a></li>
-      <li><a href="/economically">Economically</a></li>
-    </ul>
-  </li>
-  <li><a href="/plans">Plans<span class="caret"></span></a>
-    <ul>
-      <li><a href="/membercost">Membership Cost</a></li>
-      <li><a href="/discount">Member Discount</a></li>
-      <li><a href="/referral">Referral Fees</a></li>
-      <li><a href="/debitcards">Debit Card</a></li>
-      <li><a href="/businesscards">Business Cards</a></li>
-      <li><a href="/requirements">Requirements for Bonuses</a></li>
-      <li><a href="/limitations">Limitations on Recruiting</a></li>
-      <li><a href="/accounting">On-line Accounting</a></li>
-      <li><a href="/transfers">Instant Transfers</a></li>
-      <li><a href="/potential">Potentials</a></li>
-      <li><a href="/donations">Donations</a></li>
-      <li><a href="/comparison">Eco-nomix compared to Multi-Level</a></li>
-      <li><a href="/benefits">Member Benefits</a></li>
-    </ul>
-  </li>
-  <ul class="pull-right">
-  <li><a href="/about">About</a></li>
-  <li><a href="/contact">Contact</a></li>
-
-  @if ($user_name == '' or $user_name == null )
-    <li><a href="/login">Login</a></li>
-    <li><a href="/register">Register</a></li>
-  @else
-
-  <li><a href="/homepage/{{$user_id}}">{{$user_name}}<span class="caret"></span></a>
-      <ul>
-        <li><a href="/homepage/{{$user_id}}">Home Page</a></li>
-        <li><a href="/money/{{$user_id}}">Got Money</a></li>
-        <li><a href="/logout2">Logout</a></li>
-      </ul>
-    </li>
-  @endif
-  </ul>
-</ul>
-        </div>
-
-</div>
+    </div>

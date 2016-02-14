972a595a0845ac97722e5a6d2ed00b6d7c3bc8de
Jay Potter <Jay.Potter@Materialink.com>
2016-02-14 11:16:11 -0500

    fix for link

--- resources/views/emails/email_verification.blade.php
+++ resources/views/emails/email_verification.blade.php
@@ -1,7 +1,7 @@
 <body>
     Dear {{$user->first_name.' '.$user->last_name}}
 
-    To verify your email click on the following Link :  <a href="{{getenv('Site_URL')}}/emailverified/{{$user->user_link}}"
+    To verify your email click on the following Link :  <a href="/emailverified/{{$user->user_link}}"
 
-    <img src="{{$message->embed(getenv('ECONOMIX_LOGO'))}}" width="150" height="24" />
+    <img src="{{$message->embed('/images/Economix3731_Fotor.jpg')}}" width="150" height="24" />
 </body>

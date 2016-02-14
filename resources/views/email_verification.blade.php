16799332c9f9d83b32e2b7aefc10ff5eb79f21ba
Jay Potter <Jay.Potter@Materialink.com>
2016-02-14 12:35:38 -0500

    fix for link

--- resources/views/email_verification.blade.php
+++ resources/views/email_verification.blade.php
@@ -4,20 +4,22 @@
 
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
-    <h1>Thank you for registering</h1>
-
-
-    <h1>An Email was sent to {{$email}} with a link that will confirm your email address.</h1>
-
-    <h1>Go to your Email and click on the link to verify your email address</h1>
-
+<div style="position:absolute; top:52px; z-index:-1">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Thank you for registering
+                    <div class="form-group">
+                        <label class="col-md-8 col-md-offset-2 control-label">An Email was sent to {{$email}} with a link that will confirm your email address.</label>
+                    </div>
+                    <div class="form-group">
+                         <label class="col-md-8 col-md-offset-2 control-label">Go to your Email and click on the link to verify your email address</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-
-
-
-
-
-
-
 
 @stop

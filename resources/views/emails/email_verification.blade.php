74cb8730de726fbf90e403b529bd3e553bc51f3b
Jay Potter <Jay.Potter@Materialink.com>
2016-02-14 12:47:34 -0500

    fix for link

--- resources/views/emails/email_verification.blade.php
+++ resources/views/emails/email_verification.blade.php
@@ -3,10 +3,12 @@
 <div>
     Dear {{$user->first_name.' '.$user->last_name}};
 </div>
+<br>
 <div>
     Thank you for registering with Eco-nomix
 </div>
 <div>
     To verify your email click on the following Link :  <a href="http://eco-nomix.org/emailverified/{{$user->user_link}}">Verify Email</a>
 </div>
+<br><br>Eco-nomix Administration
     </body>

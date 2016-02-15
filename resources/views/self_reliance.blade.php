309e87f58350c8515fd569558743c9777cd88ee4
Jay Potter <Jay.Potter@Materialink.com>
2016-02-15 17:22:31 -0500

    add transactions

--- resources/views/self_reliance.blade.php
+++ resources/views/self_reliance.blade.php
@@ -5,12 +5,15 @@
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
     <h1>Self-Reliance</h1>
+    <div class="display">
+    There are many factors that will impact your ability to be self-reliant.  This charts reflect a number of them.
+    </div>
 
-</div>
+
 <embed src="/images/self-reliance.pdf" width="1050" height="850" type="application/pdf"></embed>
 
 
-
+</div>
 
 
 

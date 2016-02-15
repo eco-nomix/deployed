b282733845a4a3eed1a8cc2e2dc2df9a7e3459d1
Jay Potter <Jay.Potter@Materialink.com>
2016-02-15 11:54:20 -0500

    add transactions

--- resources/views/referrallinks.blade.php
+++ resources/views/referrallinks.blade.php
@@ -21,14 +21,10 @@
                             </div>
                         </div>
                 </div>
-                <div class="form-group col-md-12 standout">
+            </div>
+            <br/>
+            <div class="form-group col-md-12 standout">
                             Suggestions:  These links can be placed on business cards, added to emails,  facebook, tweets,  anyway you want to get the word out.  If anyone uses one of these links.  You get the credit
-
-                </div>
-
-
-
-
             </div>
         </div>
     </div>

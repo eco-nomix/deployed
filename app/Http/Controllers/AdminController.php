0de6f98b314dc4fd3824bd865ebd76c8c277fd8c
Jay Potter <Jay.Potter@Materialink.com>
2016-03-23 00:42:18 -0400

    first search

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -79,7 +79,10 @@ class AdminController extends Controller
         \Log::info("in search");
         $lastName = $request->input('last_name');
         \Log::info("lastname=$lastName");
-        $users = Users::where('last_name','like',$lastName.'%')->get();
+        $users = Users::where('last_name','like',$lastName.'%')
+                    ->orWhere('first_name',$lastName)
+                    ->get();
+
 
         $result = "<option value=''>select User</option>";
         foreach($users as $user){

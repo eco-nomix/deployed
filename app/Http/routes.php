62bdd3877d8c430545c78c39453b6c6ab1c3b30c
Jay Potter <Jay.Potter@Materialink.com>
2016-04-12 19:11:35 -0400

    sitemaps

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,6 +22,14 @@ Route::get('/sitemap', function()
        return Response::make($content, 200, array('content-type'=>'application/xml'));
     }
 });
+Route::get('/sitemap.xml', function()
+{
+    $file = public_path(). "/download/sitemap.xml";  // <- Replace with the path to your .xml file
+    if (file_exists($file)) {
+        $content = file_get_contents($file);
+        return Response::make($content, 200, array('content-type'=>'application/xml'));
+    }
+});
 Route::get('/google5692887e91ddc94b.html', function()
 {
     $file = public_path(). "/download/google5692887e91ddc94b.html";  // <- Replace with the path to your .xml file

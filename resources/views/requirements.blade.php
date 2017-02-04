d88cb58572cbfb98ffa4a26829ffe2ba3027ee33
Jay Potter <Jay.Potter@Materialink.com>
2017-02-04 14:34:48 -0600

    referral links

--- resources/views/requirements.blade.php
+++ resources/views/requirements.blade.php
@@ -7,18 +7,18 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default display">
-                <div class="panel-heading">Requirements for Referral Bonuses</div>
+                <div class="panel-heading">Requirements for earning Referral Bonuses</div>
                 <div class="panel-body">
 
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                          <div class="form-group col-md-12 ">
-                            There are two types of registration statuses for clients of Eco-nomix.  Registered and Member.
+                            Members can be Registered or Fully Registered.
                          </div>
                          <div class="form-group col-md-12 ">
-                            Both types are eligible to earn bonuses by referring clients to Eco-nomix.  However, to receive the bonuses, you must be a member and have received the Eco-nomix debit card.
+                            All Members are eligible to earn bonuses by referring clients to Eco-nomix.  However, to use the bonuses, you must be a Fully Registered Member.
                          </div>
                          <div class="form-group col-md-12 ">
-                               What do you have to do to receive a referral bonus.
+                               What do you have to do to earn a referral bonus?
                          </div>
                          <div class="form-group col-md-12 ">
                                 <ul>
@@ -35,10 +35,10 @@
                          <div class="form-group col-md-12 ">
                                <ul>
                                     <li>No minimum personal sales requirements</li>
-                                    <li>No direct selling of products - all sales are done through the site</li>
+                                    <li>No direct selling of products - all sales are done through the Eco-nomix website</li>
                                     <li>No collecting of money</li>
                                     <li>No required marketing meetings</li>
-                                    <li>No minimum number in your organization to 'qualify'</li>
+                                    <li>No minimum number of sponsered members in your organization are required to 'qualify'</li>
                                     <li>No stocking of inventory</li>
                                     <li>No purchasing of product you don't want or need</li>
                                     <li>No confusing Marketing plan</li>

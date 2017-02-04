b84c8af13d79f595278f29498cf2f5df69639a75
Jay Potter <Jay.Potter@Materialink.com>
2017-02-04 16:03:49 -0600

    referral links

--- resources/views/autoship.blade.php
+++ resources/views/autoship.blade.php
@@ -13,13 +13,13 @@
 
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <div class="form-group col-md-12 ">
-                            There may be some products that will offer a discount for auto-shipping the product.  However Eco-nomix discourages using such auto-ship program until you first determine that the auto-ship policy fits within the consumption needs for your family.
+                            There may be some products that will offer a discount for auto-shipping the product.  However Eco-nomix discourages using such auto-ship programs until you first determine that the auto-ship policy fits within the consumption needs for your family.
                         </div>
                         <div class="form-group col-md-12 ">
                            If you find that product inventory is beginning to accumulate beyond your consumption, cancel or modify the auto-ship terms immediately.
                          </div>
                        <div class="form-group col-md-12 ">
-                          All auto-ship products sold through Eco-nomix allow a Member to modify the quantities and term for the auto-ship.  Please be aware that such modification may result to a change in the auto-ship price.  If the new terms are not acceptable and meet the needs of your family, please cancel the auto-ship completely.
+                          All auto-ship products sold through Eco-nomix allow a Member to cancel, modify the quantities and term for the auto-ship.  Please be aware that such modification may result to a change in the auto-ship price.  If the new terms are not acceptable and meet the needs of your family, please cancel the auto-ship completely.
                        </div>
                        <div class="form-group col-md-12 ">
                             Auto-ship products may be returned under the normal product return conditions.

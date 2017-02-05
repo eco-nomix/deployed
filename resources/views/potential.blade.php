e7eb061e8da4ddc145adc5c57b7fcb84cc007778
Jay Potter <Jay.Potter@Materialink.com>
2017-02-05 17:16:02 -0600

    fix potential

--- resources/views/potential.blade.php
+++ resources/views/potential.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.default')
 
-
-
 @section('content')
  <div style="position:absolute; top:52px; z-index:-1">
  <div class="container-fluid">
      <div class="row">
          <div class="col-md-8 col-md-offset-2">
+
              <div class="panel panel-default display">
                  <div class="panel-heading">Potentials</div>
                  <div class="panel-body">

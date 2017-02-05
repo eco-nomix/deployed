e87d0731f3622625110828b39af58b9799499616
Jay Potter <Jay.Potter@Materialink.com>
2017-02-05 16:24:22 -0600

    policies and procedures

--- resources/views/policies.blade.php
+++ resources/views/policies.blade.php
@@ -50,7 +50,7 @@
                          </ol>
                         </div>
                         <div class="form-group col-md-12 ">
-                        1.2 Eco-Nomix Policies and Compensation Plan Incorporated into the Member<br> <p style="margin-left: 20px"> Agreement</p>
+                        1.2 Eco-Nomix Policies and Compensation Plan Incorporated into the Member<br> <p style="margin-left: 40px"> Agreement</p>
                         </div>
                         <div class="form-group col-md-12 ">
                          <ol type="A">

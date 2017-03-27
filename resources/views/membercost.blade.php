25d2d72a30e0cb00af3ccc4c914b05a0544fd07d
Jay Potter <Jay.Potter@Materialink.com>
2017-03-26 23:10:27 -0500

    fix for prod

--- resources/views/membercost.blade.php
+++ resources/views/membercost.blade.php
@@ -4,44 +4,49 @@
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default display">
-                <div class="panel-heading">Member Costs for Eco-nomix</div>
-                <div class="panel-body">
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                        <div class="form-group col-md-12 ">
-                            There is NO membership costs for clients of Eco-nomix to participate in its Referral Bonus Program.
-                        </div>
-                        <div class="form-group col-md-12 ">
-                            The only requirement to participate in Eco-nomix’s referral program is to register as a Member.   </div>
-                        <div class="form-group col-md-12 ">
-                           Because this program involves payments and bonuses, sufficient information to satisfy government requirements will be required to complete the registration.</div>
-                         <div class="form-group col-md-12 ">Registration is a two step process
-                            <ul>
-                                <li>Provide your basic information (name, email, desired username and password), </li>
-                                <li>Provide Contact Info, Tax Information (necessary for receiving Referral Bonuses)</li>
-                            </ul>
-                        <div class="form-group col-md-12 ">
-                           Benefits from Registration</div>
-                        <div class="form-group col-md-12 ">
-                           <ul>
-                               <li>Registered members will typically receive a discount on products from the Suggested Retail Price.</li>
-                               <li>Registered members participate in the Referral Bonus Program and build an organization</li>
-                               <li>Fully Registered members may utilize Referral Bonuses to purchase or apply payments to Eco-nomix Products.</li>
-                               <li>Fully Registered members using Referral Bonuses, may "purchase" Eco-nomix Products with no out of pocket money.</li>
-                               <li>One such product is the Economics Start-up Package.</li>
-                           </ul>
-                        </div>
+    <div class="pagecontainer"><img src="/images/MemberCosts.jpg" style="width:1000px;"></div>
 
-                         <div class="form-group col-md-12 ">
-                            After Login, if 'Register' still appears in the upper right hand corner, you have yet to complete your registration.
-                         </div>
-                </div>
-            </div>
+    {{--<div class="row">--}}
+        {{--<div class="col-md-8 col-md-offset-2">--}}
 
-        </div>
-    </div>
+
+
+            {{--<div class="panel panel-default display">--}}
+                {{--<div class="panel-heading">Member Costs for Eco-nomix</div>--}}
+                {{--<div class="panel-body">--}}
+                        {{--<input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
+                        {{--<div class="form-group col-md-12 ">--}}
+                            {{--There is NO membership costs for clients of Eco-nomix to participate in its Referral Bonus Program.--}}
+                        {{--</div>--}}
+                        {{--<div class="form-group col-md-12 ">--}}
+                            {{--The only requirement to participate in Eco-nomix’s referral program is to register as a Member.   </div>--}}
+                        {{--<div class="form-group col-md-12 ">--}}
+                           {{--Because this program involves payments and bonuses, sufficient information to satisfy government requirements will be required to complete the registration.</div>--}}
+                         {{--<div class="form-group col-md-12 ">Registration is a two step process--}}
+                            {{--<ul>--}}
+                                {{--<li>Provide your basic information (name, email, desired username and password), </li>--}}
+                                {{--<li>Provide Contact Info, Tax Information (necessary for receiving Referral Bonuses)</li>--}}
+                            {{--</ul>--}}
+                        {{--<div class="form-group col-md-12 ">--}}
+                           {{--Benefits from Registration</div>--}}
+                        {{--<div class="form-group col-md-12 ">--}}
+                           {{--<ul>--}}
+                               {{--<li>Registered members will typically receive a discount on products from the Suggested Retail Price.</li>--}}
+                               {{--<li>Registered members participate in the Referral Bonus Program and build an organization</li>--}}
+                               {{--<li>Fully Registered members may utilize Referral Bonuses to purchase or apply payments to Eco-nomix Products.</li>--}}
+                               {{--<li>Fully Registered members using Referral Bonuses, may "purchase" Eco-nomix Products with no out of pocket money.</li>--}}
+                               {{--<li>One such product is the Economics Start-up Package.</li>--}}
+                           {{--</ul>--}}
+                        {{--</div>--}}
+
+                         {{--<div class="form-group col-md-12 ">--}}
+                            {{--After Login, if 'Register' still appears in the upper right hand corner, you have yet to complete your registration.--}}
+                         {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+
+        {{--</div>--}}
+    {{--</div>--}}
 </div>
 </div>
 @endsection
--- resources/views/startup.blade.php
+++ resources/views/startup.blade.php
@@ -5,44 +5,46 @@
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default display">
-                <div class="panel-heading">Eco-nomix Start-Up Package</div>
-                <div class="panel-body">
+    <div class="pagecontainer"><img src="/images/StartUpPackage.jpg" style="width:1000px;"></div>
 
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                        <div class="form-group col-md-12   ">
-                            Members can OPTIONALLY purchase an Eco-nomix Start-up Package.    </div>
-                        <div class="form-group col-md-12 ">
-                           The Start-up Package is comprised of:
-                            <ul>
-                                <li>The Eco-nomix Debit Card  retail  $25 (us)/ $45 (non-us) </li>
-                                <li>500 Business Cards        retail  $20 </li>
-                            </ul>
-                        </div>
-                        <div class="form-group col-md-12 ">
-                           This is a special discount package to help our members get started.
-                        </div>
+    {{--<div class="row">--}}
+        {{--<div class="col-md-8 col-md-offset-2">--}}
+            {{--<div class="panel panel-default display">--}}
+                {{--<div class="panel-heading">Eco-nomix Start-Up Package</div>--}}
+                {{--<div class="panel-body">--}}
+
+                        {{--<input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
+                        {{--<div class="form-group col-md-12   ">--}}
+                            {{--Members can OPTIONALLY purchase an Eco-nomix Start-up Package.    </div>--}}
+                        {{--<div class="form-group col-md-12 ">--}}
+                           {{--The Start-up Package is comprised of:--}}
+                            {{--<ul>--}}
+                                {{--<li>The Eco-nomix Debit Card  retail  $25 (us)/ $45 (non-us) </li>--}}
+                                {{--<li>500 Business Cards        retail  $20 </li>--}}
+                            {{--</ul>--}}
+                        {{--</div>--}}
+                        {{--<div class="form-group col-md-12 ">--}}
+                           {{--This is a special discount package to help our members get started.--}}
+                        {{--</div>--}}
 
 
-                        <div class="form-group col-md-12 ">
-                            The Member cost for the Eco-nomix Start-up Package is $35.00 ($55.00 for Members outside of the US).<br>
-                            This is a savings of $10 from the normal retail price.
-                        </div>
+                        {{--<div class="form-group col-md-12 ">--}}
+                            {{--The Member cost for the Eco-nomix Start-up Package is $35.00 ($55.00 for Members outside of the US).<br>--}}
+                            {{--This is a savings of $10 from the normal retail price.--}}
+                        {{--</div>--}}
 
-                        <div class="form-group col-md-12 ">
-                            Normal Annual renewal fees for the Debit Card are (US) $20.00  and (non-US) $40.00.
+                        {{--<div class="form-group col-md-12 ">--}}
+                            {{--Normal Annual renewal fees for the Debit Card are (US) $20.00  and (non-US) $40.00.--}}
 
-                        </div>
+                        {{--</div>--}}
 
-                         <div class="form-group col-md-12 ">
-                            Cost for this and any Eco-Nomix product may be paid for through accrued Referral Bonuses.
-                         </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                         {{--<div class="form-group col-md-12 ">--}}
+                            {{--Cost for this and any Eco-Nomix product may be paid for through accrued Referral Bonuses.--}}
+                         {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
 </div>
 </div>
 @endsection

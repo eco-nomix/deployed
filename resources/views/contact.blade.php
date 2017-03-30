16f5d3e030a6ddc1a87dbe57eccf03f80ce4f81a
Jay Potter <Jay.Potter@Materialink.com>
2017-03-30 10:50:36 -0500

    update for contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,8 +4,50 @@
 
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
-    <h1>Contact Eco-nomix</h1>
-</div>
+<div class="container-fluid">
+    <div class="row">
+           <div class="col-md-8 col-md-offset-2">
+               <div class="panel panel-default display">
+                   <div class="panel-heading">Contact Eco-nomix Inc.</div>
+                   <div class="panel-body">
+                       <div class="form-group col-md-12 ">
+
+                            <table>
+                                <tr><td>Corporate Mailing<br> Address:</td></tr>
+                                <tr><td></td><td>Eco-nomix Inc.</td><tr>
+                                <tr><td></td><td>8029 Highway 641 S.</td></tr>
+                                <tr><td></td><td>Paris, Tennessee</td></tr>
+                                <tr><td></td><td>38242</td></tr>
+                              </table>
+
+                       </div>
+                       <br><br>
+                       <div class="form-group col-md-12 ">
+                            <table>
+                                 <tr><td>Corporate Phone<br> Number:</td></tr>
+                                 <tr><td></td><td>864-608-5404</td><tr>
+                            </table>
+                       </div>
+
+                       <div class="form-group col-md-12 ">
+                            <table>
+                                 <tr><td>Corporate Email:</td></tr>
+                                 <tr><td></td><td>CEO</td><tr>
+                                 <tr><td></td><td>Jay Potter</td><tr>
+                                 <tr><td></td><td>jay@eco-nomix.org</td><tr>
+                                 <tr><td>&nbsp;</td></tr>
+                                 <tr><td></td><td>Administration</td><tr>
+                                 <tr><td></td><td>admin@eco-nomix.org</td><tr>
+                            </table>
+                       </div>
+
+                   </div>
+               </div>
+
+           </div>
+       </div>
+   </div>
+   </div>
 
 
 

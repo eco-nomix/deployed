79210a20c52126b1483993ca219c9ae8e8417e45
Jay Potter <Jay.Potter@Materialink.com>
2017-12-09 08:37:43 -0600

    update for link navigation colors

--- resources/views/layouts/video.blade.php
+++ resources/views/layouts/video.blade.php
@@ -12,7 +12,47 @@
     @yield('header')
     <script src="/js/jquery-1.12.1.min.js"></script>
     <script src="/js/editing.js"></script>
-
+    <style type="text/css">
+        .navbar-fixed-top{
+            background-color: darkgreen;
+        }
+        .custom-header-h6{
+            color: #fff;
+        }
+        .nav.navbar-nav a{
+            color: #fff;
+            margin-bottom: 0;
+        }
+        .nav.navbar-nav .dropdown-menu li:hover a:hover,.nav.navbar-nav .dropdown-menu li:hover a:hover{
+            background-color: lightgreen;
+        }
+        .nav.navbar-nav li:hover a:hover{
+            background-color: lightgreen;
+        }
+        .nav.navbar-nav li.dropdown ul.dropdown-menu li{
+            background-color: darkgreen;
+            border: 1px solid darkgreen;
+        }
+        .nav.navbar-nav li.dropdown:hover a:hover{
+            background-color: lightgreen;
+        }
+        .nav > li > a{
+            /*padding-left: 0px;*/
+        }
+        .navbar-nav{
+            margin-left: -10px;
+        }
+        @media(min-width: 768px){
+            .navbar-nav > li > a{
+                padding-top: 0;
+                padding-bottom: 10px;
+            }
+        }
+        .navbar-nav .dropdown-menu{
+            padding: 0;
+            min-width: 100%;
+        }
+    </style>
 </head>
 <body class="economixbody">
     @include('layouts.navigation2')

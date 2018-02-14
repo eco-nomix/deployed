77c17ecbb35efe8b16ee87112e0daa094995c774
Jay Potter <Jay.Potter@Materialink.com>
2018-02-13 23:26:41 -0600

    update mining

--- resources/views/goldintroduction.blade.php
+++ resources/views/goldintroduction.blade.php
@@ -7,53 +7,78 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default display">
-                <div class="panel-heading">CryptoCurrency Mining</div>
+                <div class="panel-heading">CryptoCurrency Mining of Digital Gold</div>
                 <div class="panel-body">
 
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <div class="form-group col-md-12 ">
-                            Eco-Coin is a new crypto-currency that is tied directly to the price of Gold.  Please see the white paper as how this operates.
+                            Digital Gold is a new crypto-currency that is tied directly to the price of Gold.  Please see the white paper as how this operates.
                         </div>
                         <div class="form-group col-md-12 ">
-                        Eco-Coins has been designed from the ground-up to allow Agents to Mine the currency through their labor.
+                        Digital Gold has been designed from the ground-up to allow Agents to Mine the currency through their labor.
                          We utilize a MLM referral program with a number of twists to make this really exciting.
                          It was designed to eliminate the member/agent Churn that MLM's typically experience. If you don't
-                         know what I'm talking about - member/agent Churn is the member turnover that takes place in an organization as member's give up, fail and drop out. Analysis of the major MLMs finds that 60-90% of the total membership drop out annually and need to be replaced.
+                         know what I'm talking about - member/agent Churn is the member turnover that takes place
+                         in an organization as member's give up, fail and drop out. Analysis of the major MLMs finds
+                         that 60-90% of the total membership drop out annually and need to be replaced.
                         </div>
                         <div class="form-group col-md-12 ">
-                        For me, it was always getting close to some organization goal, only to have my down-line dropping out as fast as I could recruit replacements. That is really frustrating, and the bottom line, it costs you serious big bucks.
+                        For me, it was always getting close to some organization goal, only to have my down-line dropping
+                        out as fast as I could recruit replacements. That is really frustrating, and the bottom line,
+                        it costs you serious big bucks.
                         </div>
                         <div class="form-group col-md-12 ">
-                        A good example of a system that works is a grocery store discount card. About a year ago, after a move to a new state, I looked at my keychain and I had over 10 different grocery store discount cards on it. Why didn't I throw them away earlier? Well I MIGHT just go to that store, and would want to get the discount. Besides, it didn't cost me anything to keep them around, so on they stayed. When I looked at them closely there were cards there I hadn't used in years, and they still would have been there, if there was that store within a 100 miles of me.
+                        A good example of a system that works is a grocery store discount card. About a year ago, after
+                        a move to a new state, I looked at my keychain and I had over 10 different grocery store discount
+                        cards on it. Why didn't I throw them away earlier? Well I MIGHT just go to that store, and would
+                        want to get the discount. Besides, it didn't cost me anything to keep them around, so on they
+                        stayed. When I looked at them closely there were cards there I hadn't used in years, and they
+                        still would have been there, if there was that store within a 100 miles of me.
                         </div>
                         <div class="form-group col-md-12 ">
-                        What does it cost someone in your downline to participate in your MLM opportunity on a monthly basis? Each time a member has to write out a check or send a payment in, they will re-evaluate do they really want to keep doing this. If they are making more money each month than it costs them, they will stick around, as long as it doesn't costs too much in their time.
+                        What does it cost someone in your downline to participate in your MLM opportunity on a monthly
+                        basis? Each time a member has to write out a check or send a payment in, they will re-evaluate
+                        do they really want to keep doing this. If they are making more money each month than it costs
+                        them, they will stick around, as long as it doesn't costs too much in their time.
                         </div>
                         <div class="form-group col-md-12 ">
-                        I saw a study on the major MLM's and the average member was making just $13.00 per month. Now that is just not enough to write home to mom about. Especially when they had to buy $20 or more in products to qualify to receive commissions.
+                        I saw a study on the major MLM's and the average member was making just $13.00 per month. Now
+                        that is just not enough to write home to mom about. Especially when they had to buy $20 or more
+                        in products to qualify to receive commissions.
                         </div>
                         <div class="form-group col-md-12 ">
-                        Is your garage getting full of products that you intend to sell, but every time you find someone interested enough to buy them, they become a member to get the discount? Well that sounds great as they are in your downline, until they go away, never having bought your products, you never made a dime off them and your garage is just getting fuller and fuller, because you have to purchase so much every month, more than you use.
+                        Is your garage getting full of products that you intend to sell, but every time you find someone
+                        interested enough to buy them, they become a member to get the discount? Well that sounds great as
+                        they are in your downline, until they go away, never having bought your products, you never made a
+                        dime off them and your garage is just getting fuller and fuller, because you have to purchase so
+                        much every month, more than you use.
                         </div>
                         <div class="form-group col-md-12 ">
-                        Do you really want to buy those products? The better question is would you buy them if you weren't a member trying to maintain a monthly sales requirement? If the answer is no, then those products are truly a cost of doing business. If you are one of the lucky few that is making more money than it costs them to operate (the vast majority), then it might be working for you.
+                        Do you really want to buy those products? The better question is would you buy them if you weren't
+                        a member trying to maintain a monthly sales requirement? If the answer is no, then those products
+                        are truly a cost of doing business. If you are one of the lucky few that is making more money than
+                        it costs them to operate (the vast majority), then it might be working for you.
                         </div>
                         <div class="form-group col-md-12 ">
                         Any question why there is so much member churn out there?
                         </div>
                         <div class="form-group col-md-12 ">
-                        Eco-coin is introducing a whole new way of growing a business, one where there is no motivation for your down-line to get discouraged and quit your organization. What would happen to your organization size if everyone simply stayed? Over time it would grow to be HUGE. And that could translate to a significant income for you.
+                        Digital Gold is introducing a whole new way of growing a business, one where there is no motivation
+                        for your down-line to get discouraged and quit your organization. What would happen to your
+                        organization size if everyone simply stayed? Over time it would grow to be HUGE. And that could
+                        translate to a significant income for you.
                         </div>
                         <div class="form-group col-md-12 ">
-                        Well what is Eco-coin all about? It is a company that specializes
-                        in allowing individuals to benefit from owning Gold without all the negatives that are associated with keeping physical gold.
+                        Well what is Digital Gold all about? It is a company that specializes
+                        in allowing individuals to benefit from owning Gold without all the negatives that are associated
+                        with keeping physical gold.
 
                         </div>
                         <div class="form-group col-md-12 ">
-                        But it is marking its product through MLM with a difference.
+                        But it is marketing its product through MLM with a difference.
                         </div>
                         <div class="form-group col-md-12 ">
-                            First of all, there is minimal cost to become a agent for Eco-Coin.
+                            First of all, there is minimal cost to become a agent for Digital Gold.
                             Just Register to be a Agent, there is a one-time $10 processing fee.
                              Once you are an Agent you can earn Referral Bonuses and even create an organization of Agents.
                         </div>
@@ -69,7 +94,7 @@
                             </li>
                             <li>
                             Someone within 5 generations of your referrals makes a purchase of Gold.
-                            After all purchases are what drives a MLM company.
+                            After all, purchases are what drives a MLM company.
                             </li>
                         </ul>
                         </div>
@@ -82,7 +107,7 @@
                         <div class="form-group col-md-12 ">
                             <ul>
                                 <li>
-                                1st generation (your direct referrals) - you receive 10% of the sales price (sales tax not included)
+                                1st generation (your direct referrals) - you receive 10% of the sales price in Digital Gold Coins(sales tax not included)
                                 </li>
                                 <li>
                                 2nd Generation (referrals made by your first generation) - you receive 4% of the sales price
@@ -99,22 +124,30 @@
                             </ul>
                         </div>
                         <div class="form-group col-md-12 ">
-                        Every Agent automatically earns these bonuses regardless of the size of their organization or personal purchases.
+                        Every Agent automatically earns these bonuses regardless of the size of their organization or
+                        personal purchases.
                         </div>
                         <div class="form-group col-md-12 ">
                         The bonuses are credited immediately at the time of sale to all 5 generations.
                         </div>
                         <div class="form-group col-md-12 ">
-                           The bonuses of Agents are in the form Eco-Coins.  They can be used throughout the Eco-Coin system.
-                            This means that anyone can benefit from being an agent of Eco-Coin with the regestration fee being the only risk.
+                           The bonuses of Agents are in the form of Digital Gold.  They can be used throughout the Digital Gold cryptocurrency System
+                           system.
+                            This means that anyone can benefit from being an agent of Eco-Coin with the registration
+                            fee being the only risk.
                         </div>
                         <div class="form-group col-md-12 ">
-                            In addition, Agents may puchase an Eco-Coin Payroll Debit Card.  The Earned Bonuses may be transferred onto the Payroll debit card, so they can be utilized when you want, where you want,  at any location a standard debit or credit card can be used.
-                            The cost of this Payroll Debit Card may be deducted from earned Referral Bonuses, therefore no risk at all.
+                            In addition, Agents may purchase an Digital Gold Payroll Debit Card.  The Earned Bonuses may
+                            be transferred onto the Payroll debit card, so they can be utilized when you want, where
+                            you want,  at any location a standard debit or credit card can be used.
+                            The cost of this Payroll Debit Card may be deducted from earned Referral Bonuses, therefore
+                            no risk at all.
                         </div>
                         <div class="form-group col-md-12 ">
-                            Business cards may also be paid for through earned Referral Bonuses.  Therefore there is NO FINANCIAL RISK to participate in the entire Eco-nomix system.<br><br>
-                            Members may elect to purchase the business cards and / or Payroll Debit Cards before there are sufficient earnings to accelerate their organization and make it more convenient for purchases, but even that is just a $35.00 fee for the products.
+                            Business cards may also be paid for through earned Referral Bonuses.  Therefore there is NO
+                            FINANCIAL RISK to participate in the entire Digital Gold system beyond the registration fee.<br><br>
+                            Members may elect to purchase the business cards  before there
+                            are sufficient earnings to accelerate their organization, but even that is just a $20.00 fee for the business cards.
                         </div>
                         <div class="form-group col-md-12 ">
                         Now lets see what you don't have to do.
@@ -122,7 +155,7 @@
                         <div class="form-group col-md-12 ">
                             <ul>
                                 <li>
-                                No direct selling of products - all sales are done through the site.
+                                No direct selling of products - all sales are done through the site using block chain transfers.
                                 </li>
                                 <li>
                                 No minimum personal sales requirements.
@@ -169,17 +202,17 @@
                           out of pocket expenses. Once income starts to come in, would someone have a reason to quit?
                         </div>
                         <div class="form-group col-md-12 ">
-                        Your downline doesn't go away. If they ever purchase anything from Eco-Coin, you get your
+                        Your downline doesn't go away. If they ever purchase anything from Digital Gold, you get your
                         bonus, no exceptions, even years from now. The income doesn't flow to the top to some
                         original Agent that is making an outrageous income.
                         </div>
                         <div class="form-group col-md-12 ">
                         So how wide can you go? Unlimited. Because of the ease of becoming an Agent, the ability to
-                        'test drive' with the company with no out of pocket expense, the low cost of becoming an Agent,
+                        'test drive' with the company with almost no out of pocket expense, the low cost of becoming an Agent,
                         the odds are significantly improved for someone willing to join your organization. Your
                         business card contains a url that when used, automatically gives you the credit for any sale
                         or new Agent that results from it, even it they don't make the decision now, but later,
-                        even if they then use the standard eco-coin.com url.
+                        even if they then use the standard Digitalgold.com url.
                         </div>
                         <div class="form-group col-md-12 ">
                         To start your ‘Test Drive’, simply register and pay the $10.00 registration fee, as
@@ -203,7 +236,7 @@
                         We all have seen the charts and potential associated with exponential growth, now become part of a company that makes that growth possible.
                         </div>
                         <div class="form-group col-md-12 ">
-                        Jay Potter    Eco-Coin.
+                        Jay Potter    Digital Gold.
                         </div>
                 </div>
             </div>

246bd2a367abf2a0cda48d0196913f3833d7966e
Jay Potter <Jay.Potter@Materialink.com>
2018-05-25 11:27:18 -0700

    test3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
 @section('content')
    {{--<div style="z-index:-5; height:1400px; background-attachment:fixed; background-size:100% 100%; background-image:  url({{$imageUrl}});">--}}
    {{--</div>--}}
-   hi
+ 
    {{--<div class="message">--}}
-       {{--<p>{{$message}}</p>--}}
+      hi
    {{--</div>--}}
 @stop

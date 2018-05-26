d75b646371b5c6bf7271699fe3fff351463c3fe8
Jay Potter <Jay.Potter@Materialink.com>
2018-05-26 09:25:51 -0700

    member costs

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,14 +4,100 @@
 
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
-    <h1>About Economix</h1>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default display">
+                <div class="panel-heading">Introduction to KineticGold</div>
+                <div class="panel-body">
+
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <div class="form-group col-md-12 ">
+                        KineticGold is a new cryptocurrency that is designed to be used in your daily life.
+                        </div>
+
+                        <div class="form-group col-md-12 ">
+                        KineticGold is first and foremost a banking platform that uses a decentralized block-chain to record transactions and provide access to your account.
+                        </div>
+                        <div class="form-group col-md-12 ">
+                        One of the significant features is every coin is backed up by physical tangible gold that is on deposit.  KineticGold's coin is actually a warehouse receipt that sow ownership of that gold.</div>
+                        <div class="form-group col-md-12 ">
+                        The goldd will in the vaults of Dubai and other locations throughout the world. </div>
+                        <div class="form-group col-md-12 ">
+                        Every coin will be insured by Lloyds of London that it represents one ounce of Gold on deposit.
+                        </div>
+                        <div class="form-group col-md-12 ">
+                        Kinetic Gold has a unique systems of Rewards and Profit Sharing that will enable even the newest and smallest user able to generate immediate profit</div>
+                        <div class="form-group col-md-12 ">
+                        No longer is the value of your cryptocurrency establishing on a whim but is based upon a recognized and tangible value - Gold. </div>
+
+
+                        <div class="form-group col-md-12 ">
+                        Every member of the KineticGold associate is given the potential to mine Gold and have it added to their accounts where it can be spent anywhere in the world.
+                        </div>
+                        <div class="form-group col-md-12 ">
+
+                        </div>
+
+
+
+                        <div class="form-group col-md-12 ">
+                          Never before have these benefits been offered to a novice user.  The opportunity to make real money.  </div>
+                        <div class="form-group col-md-12 ">
+                         Check out the informational video and power point presentation and make an informed and realistic decision for you.  </div>
+                        <div class="form-group col-md-12 ">
+                            I remember when I had the opportunity to purchase bitcoin when it was below 25 cents.  When I think that I turned it down because it was unknown and I didn't want to take the risk, I feel a bit foolish now.  Kinetic Gold was designed eto eliminate even that risk.</div>
+                        <div class="form-group col-md-12 ">
+                        Now lets see what you don't have to do.
+                        </div>
+                        <div class="form-group col-md-12 ">
+                            <ul>
+                                <li>
+                                No direct selling of products - all sales are done through the site.
+                                </li>
+                                <li>
+                                No minimum personal sales requirements.
+                                </li>
+                                <li>
+                                No collection of money
+                                </li>
+                                <li>
+                                No required marketing meetings
+                                </li>
+                                <li>
+                                No minimum number in your organization to 'qualify'
+                                </li>
+                                <li>
+                                No purchasing of product you don't want or need
+                                </li>
+                                <li>
+                                No stocking of inventory
+                                </li>
+                                <li>
+                                No confusing Marketing plan
+                                </li>
+                                <li>
+                                No Waiting for the check that is in the mail.
+                                </li>
+                                <li>
+                                No hidden Fees
+                                </li>
+                                <li>
+                                No hard sell - hand out a business card, let the site do the work for you.
+                                </li>
+                            </ul>
+                        </div>
+
+                         <div class="form-group col-md-12 ">
+                                                 Check out the informational video and power point presentation and make an informed and realistic decision for you.  </div>
+
+
+
+
+                      
+            </div>
+        </div>
+    </div>
 </div>
-
-
-
-
-
-
-
-
-@stop
+</div>
+@endsection

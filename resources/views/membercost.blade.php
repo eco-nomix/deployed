f6c5982a07b488ae16b86b6143fdf354ccc0e6e9
Jay Potter <Jay.Potter@Materialink.com>
2018-05-26 08:54:57 -0700

    member costs

--- resources/views/membercost.blade.php
+++ resources/views/membercost.blade.php
@@ -12,11 +12,11 @@
 
 
             <div class="panel panel-default display">
-                <div class="panel-heading">Member Costs for Kinetic Gold</div>
+                <div class="panel-heading">Member Costs for KineticGold</div>
                 <div class="panel-body">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <div class="form-group col-md-12 ">
-                            Kinetic Gold is an association that is focused on the use and expansion of the Kinetic Gold banking and Cryptocurrency platform.
+                            KineticGold is an association that is focused on the use and expansion of the KineticGold banking and Cryptocurrency platform.
                         </div>
                         <div class="form-group col-md-12 ">
                             To become a member of the association there is a one-time registration fee of $39.95.   </div>
@@ -25,20 +25,20 @@
                            Provide your basic information (name, email, desired username and password and contact information)</div>
 
                         <div class="form-group col-md-12 ">
-                           Benefits from membership in Kinetic Gold's Association</div>
+                           Benefits from membership in KineticGold's Association</div>
                         <div class="form-group col-md-12 ">
                            <ul>
-                               <li>Members will receive a Kinetic Gold cryptocurrency account.</li>
+                               <li>Members will receive a KineticGold cryptocurrency account.</li>
                                <li>Members will receive an e-wallet which can access this account</li>
-                               <li>Members will receive a banking account at an offshore bank affiliated with Kinetic Gold.</li>
+                               <li>Members will receive a banking account at an offshore bank affiliated with KineticGold.</li>
                                <li>The banking account will interact directly with their cryptocurrency account.</li>
                                <li>The banking account will include a Debit card where you can access your accounts anywhere in the world through the Visa/Mastercard platform.</li>
-                               <li>Members automatically qualify for Kinetic Gold rewards program.  Rewards are automatically credited into your savings account at your offshore bank account.</li>
-                               <li>Members can participate in Kinetic Gold's profit sharing and mining program</li>
+                               <li>Members automatically qualify for KineticGold rewards program.  Rewards are automatically credited into your savings account at your offshore bank account.</li>
+                               <li>Members can participate in KineticGold's profit sharing and mining program</li>
                            </ul>
                         </div>
 
-                       
+
                 </div>
             </div>
 

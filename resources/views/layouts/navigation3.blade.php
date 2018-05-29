1edea8be779005f0b1aede757758ee08952ac1d8
Jay Potter <Jay.Potter@Materialink.com>
2018-05-29 10:22:23 -0700

    welcome

--- resources/views/layouts/navigation3.blade.php
+++ resources/views/layouts/navigation3.blade.php
@@ -1,6 +1,6 @@
 
     <div class="navcontainer" >
-        <div class="navbar-brand brand-image KineticSm">
+        <div class="KineticSm">
              KineticGold
         </div>
         <div class="referral" >

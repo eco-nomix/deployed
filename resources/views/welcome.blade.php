cd5a6c723640fbf66faa1201a42867ffc6e44474
Jay Potter <Jay.Potter@Materialink.com>
2018-05-29 10:18:00 -0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,25 +18,31 @@
     <img src="{{URL::asset('images/denmark.jpg')}}" >
     <div class="display container-fluid back-white" >
             <p>Kinetic Gold is a new cryptocurrency that is in its Pre-Launch phase.  </p>
-            <p>Unlike any other cryptocurrency, Kinetic Gold has been designed to be utilized like a standard bank account.
+            <p>Unlike any other cryptocurrency, Kinetic Gold has been designed to be utilized like a standard bank account
+               on a regular basis for normal banking activities.
                Features like payroll deposit, ATM access, Debit Card Access and on-line banking are among the standard
                banking features to be available.</p>
+            <p>You will be able to access your Kinetic Gold account to make payments at any local merchants that accept Visa/Mastercard.</p>
+            <p>There are no minimum or maximum transaction sizes.</p>
+            <p>Transaction charge is 1/2 of 1% of the transaction.</p>
             <p>Unlike your standard bank account, your account is denominated in ounces of Gold, insured to the full value
                of your deposits (no $250,000 limit) by Lloyds of London, and can be used within any country.</p>
             <p>The cryptocurrency feature of decentralized transactions allows full privacy of banking, having no
                regards to national borders or transaction amounts.</p>
 
-            <p>By its design Kinetic Gold is designed to a benefit to the small account holder, allowing them the
+            <p>By its design Kinetic Gold is designed to be a benefit to the small account holder, allowing them the
                same benefits as the large account holder.  The Rewards and Profit Sharing applies for all account holders.
                Just by using Kinetic Gold like your standard bank account you automatically qualify for a significant amount
-               of Rewards.  The Rewards alone can add up to hundreds of dollars worth of Gold to your account on a monthly basis. </p>
+               of Rewards.  Without changing your income or spending habits, you see large benefits.  The Rewards alone can
+               add up to hundreds of dollars worth of Gold to your account on a monthly basis.
+               </p>
     </div>
      <div class="center kinetic">
              <p>Kinetic Gold's Coin</p>
     </div>
     <div class="display container-fluid back-white" >
             <p>Kinetic Gold's coin is equivalent to 1 oz of fine gold at spot value.  This is established by
-               requiring before any coin is issued that there there will be set aside one of the ounces of Gold
+               requiring before any coin is issued that there there will be set aside one ounce of Gold
                in our insured vaults.  The coin technically represents a warehouse receipt for 1 ounce of Gold
                at spot price.</p>
             <p>With any member's deposit into their account the deposit funds are used to purchase additional
@@ -51,24 +57,41 @@
                coins are being added continuously along with additional gold deposits.</p>
     </div>
     <div class="center kinetic">
-            <p>Vaulted Storage</p>
+            <p>Blockchain</p>
     </div>
     <div class="display container-fluid back-white" >
-            <p>At the minimum there will be no less than one ounce of Gold on deposit and insured for every coin issued.</p>
-            <p>The initial vault will be located in Dubai.</p>
-            <p>Insurance for all depositors for the full amount of deposit will be provided by Lloyds of London.</p>
-    </div>
+            <p>Kinetic Gold's blockchain is one of the fastest block chains available.</p>
+            <p>Transaction times are in the millisecond range, making transactions seem almost instantaneous from any
+               two points in the world.</p>
+            <p>Initally Kinetic Gold will use starchain and will migrate over to even a faster system that will be able to keep
+               up with the anticipated growth and number of transactions of Kinetic Gold.</p>
+            <p>All transactions will automatically encrypt all elements of the transaction, keeping your private transactions private.</p>
 
+    </div>
+    <div class="center kinetic">
+            <p>Vaulted Storage</p>
+    </div>
+     <div class="display container-fluid back-white" >
+                <p>At the minimum there will be no less than one ounce of Gold on deposit and insured for every coin issued.</p>
+                <p>The initial vault will be located in Dubai.</p>
+                <p>Insurance for all depositors for the full amount of deposit will be provided by Lloyds of London.</p>
+                <p>The Vaulted Storage will interface with the Off-shore Banking system to convert Gold back into fiat currency
+                   when needed to cover banking transactions.  Only when Coins are converted into fiat currency and are removed
+                   from the system can equivalent amount of Gold be sold</p>
+                <p>Transactions of Gold are controlled by the blockchain.</p>
+     </div>
     <div class="center kinetic">
             <p>Off-shore Banking</p>
     </div>
     <div class="display container-fluid back-white" >
-            <p>During the Pre-Launch phase an off-shore bank will be obtained on the island of Nevis.</p>
+            <p>During the ICO phase an off-shore bank will be obtained on the island of Nevis.</p>
             <p>This bank will have full on-line banking capabilities for our members accounts.</p>
             <p>This bank will also provide debit card access and process for our members accounts.</p>
             <p>Members will have access to funds from anywhere in the world.</p>
             <p>Account will be denominated in gold ounces.</p>
             <p>Account is fully linked to the cryptocurrency account.</p>
+            <p>Coins in the cryptocurrency account can be converted to fiat currency or physical gold, quickly and easily</p>
+            <p>All transactions are controlled by the blockchain.</p>
     </div>
 
     <div class="center kinetic">
@@ -76,9 +99,9 @@
     </div>
     <div class="display container-fluid back-white" >
           <p>Programming of the block-chain will commence</p>
-          <p>Creation of the Foreign Trust</p>
+          <p>Creation of the Foreign Trust will be completed</p>
           <p>During the Pre-Launch Phase, members will be accepted into Kinetic Gold's member association.</p>
-          <p>Member's may build their Mining organization.</p>
+          <p>Member's may build their Mining organization. (See details on Mining Kinetic Gold)</p>
     <div>
     <div class="center kinetic">
           <p>ICO</p>
@@ -86,7 +109,15 @@
     <div class="display container-fluid back-white" >
          <p>Final Testing of block-chain will completed</p>
          <p>Kinetic Gold will also obtain an off-shore bank that will provide the on-line access and Debit card access to the account.</p>
-         <p>Kinetic Gold will make an initial deposit into the insured vaults a minimum of 1000 kg of hallmarked gold prior to Launch.  This base amount of Gold is used during the deposit process to set the initial value of new coins.</p>
+         <p>Kinetic Gold will make an initial deposit into the insured vaults a minimum of 1000 kg of hallmarked gold prior to Launch.
+            This base amount of Gold is used during the deposit process to set the initial value of new coins.</p>
+         <p>Coins purchased during the ICO will be purchased at a discount below spot price.  The amount of the discount is determined
+            by the number of Coins purchased.  The ICO amount will be used to purchase the Gold represented by the ICO funds.  The gold
+            from the ICO will be used to back the coins issued once Kinetic Gold is launched.
+            This backing lasts only during the period that the fiat currency of the deposit is used to purchase additional gold.  Once
+            the additional gold is deposited the backing is no longer needed.  This backing period will be completed within 6 months and
+            then the Coins for the ICO will be issued.</p>
+         <p>Release of ICO coins are controlled by the blockchain.</p>
 
 
     </div>
@@ -98,5 +129,6 @@
         <p>Off-Shore bank in place with debit card</p>
         <p>Mining results from previous organization development will be deposited in accounts. </p>
         <p>Deposits and Mining results will result in additional gold on deposit in the vaults.</p>
+        <p>All transactions are controlled and recorded by the blockchain</p>
     </div>
 @stop

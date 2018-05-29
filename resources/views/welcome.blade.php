0d17bcf4a7d002c5d54c3242d35dac5661f61971
Jay Potter <Jay.Potter@Materialink.com>
2018-05-28 21:30:53 -0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,18 +19,41 @@
     <div class="display container-fluid back-white" >
                 <p>Kinetic Gold is a new cryptocurrency that is in its Pre-Launch phase.  </p>
                 <p>Unlike any other cryptocurrency, Kinetic Gold has been designed to be utilized like a standard bank account.  Features like payroll deposit, ATM access, Debit Card Access and on-line banking are among the standard banking features available.</p>
-                <p>Unlike your standard bank account, your account is denomonated in ounces of Gold, insured to the full value of your deposits (no $250,000 limit) by Lloyds of London, and can be used within any country.</p>
+                <p>Unlike your standard bank account, your account is denominated in ounces of Gold, insured to the full value of your deposits (no $250,000 limit) by Lloyds of London, and can be used within any country.</p>
                 <p>The cryptocurrency feature of decentralized transactions allows full privacy of banking, having no regards to national borders or transaction amounts.</p>
     </div>
      <div class="center kinetic">
                      <p>Kinetic Gold's Coin</p>
             </div>
         <div class="display container-fluid back-white" >
-                    <p>Kinetic Gold's coin is equivalent to 1 oz of fine gold at spot value.  This is established by that before any coin is issued that there there will be set aside one of the ounces of Gold in our insured vaults.  The coin technically represents a warehouse receipt for 1 ounce of Gold at spot price.</p>
+                    <p>Kinetic Gold's coin is equivalent to 1 oz of fine gold at spot value.  This is established by requiring before any coin is issued that there there will be set aside one of the ounces of Gold in our insured vaults.  The coin technically represents a warehouse receipt for 1 ounce of Gold at spot price.</p>
                     <p>With any member's deposit into their account the deposit funds are used to purchase additional gold for Kinetic Gold's reserves.</p>
                     <p>The Spot price of gold is essentially it's wholesale price.  This means that you control one ounce of Gold per Coin purchased without having to pay miniting costs, vaulting costs, insurance or shipping.  When a standard purchase of coins or bullion is made these costs will be incurred which could reach as high as 10% of the purchase.</p>
                     <p>If spot price changes just 1%, the value of your account will adjust accordingly.</p>
+                    <p>With any deposit (purchase of coins) the value of the coin is reestablished to spot price</p>
+                    <p>Unlike other cryptocurrencies, the value of the coin is not determined by scarcity as additional coins are being added continuously along with additional gold deposits.</p>
         </div>
+        <div class="center kinetic">
+                    <p>Vaulted Storage</p>
+        </div>
+        <div class="display container-fluid back-white" >
+                    <p>At the minimum there will be no less than one ounce of Gold on deposit and insured for every coin issued.</p>
+                    <p>The initial vault will be located in Dubai.</p>
+                    <p>Insurance for all depositors for the full amount of deposit will be provided by Lloyds of London.</
+        </div>
+
+        <div class="center kinetic">
+                            <p>Off-shore Banking</p>
+        </div>
+        <div class="display container-fluid back-white" >
+                            <p>During the Pre-Launch phase an off-shore bank will be obtained on the island of Nevis.</p>
+                            <p>This bank will have full on-line banking capabilities for our members accounts.</p>
+                            <p>This bank will also provide debit card access and process for our members accounts.</p>
+                            <p>Members will have access to funds from anywhere in the world.</p>
+                            <p>Account will be denominated in gold ounces.</p>
+                            <p>Account is fully linked to the cryptocurrency account.</p>
+        </div>
+
      <div class="center kinetic">
                  <p>Pre-Launch Phase</p>
         </div>

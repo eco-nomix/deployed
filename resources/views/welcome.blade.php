a64214783a813dbefd1f5dbb3d5f5cf31c7d5fc0
Jay Potter <Jay.Potter@Materialink.com>
2018-05-28 21:14:57 -0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,11 +22,22 @@
                 <p>Unlike your standard bank account, your account is denomonated in ounces of Gold, insured to the full value of your deposits (no $250,000 limit) by Lloyds of London, and can be used within any country.</p>
                 <p>The cryptocurrency feature of decentralized transactions allows full privacy of banking, having no regards to national borders or transaction amounts.</p>
     </div>
-    <div>  &nbsp;</div>
+     <div class="center kinetic">
+                     <p>Kinetic Gold's Coin</p>
+            </div>
+        <div class="display container-fluid back-white" >
+                    <p>Kinetic Gold's coin is equivalent to 1 oz of fine gold at spot value.  This is established by that before any coin is issued that there there will be set aside one of the ounces of Gold in our insured vaults.  The coin technically represents a warehouse receipt for 1 ounce of Gold at spot price.</p>
+                    <p>With any member's deposit into their account the deposit funds are used to purchase additional gold for Kinetic Gold's reserves.</p>
+                    <p>The Spot price of gold is essentially it's wholesale price.  This means that you control one ounce of Gold per Coin purchased without having to pay miniting costs, vaulting costs, insurance or shipping.  When a standard purchase of coins or bullion is made these costs will be incurred which could reach as high as 10% of the purchase.</p>
+                    <p>If spot price changes just 1%, the value of your account will adjust accordingly.</p>
+        </div>
+     <div class="center kinetic">
+                 <p>Pre-Launch Phase</p>
+        </div>
     <div class="display container-fluid back-white" >
-                <p>Kinetic Gold is a new cryptocurrency that is in its Pre-Launch phase.  </p>
-                <p>Unlike any other cryptocurrency, Kinetic Gold has been designed to be utilized like a standard bank account.  Features like payroll deposit, ATM access, Debit Card Access and on-line banking are among the standard banking features available.</p>
-                <p>Unlike your standard bank account, your account is denomonated in ounces of Gold, insured to the full value of your deposits (no $250,000 limit) by Lloyds of London, and can be used within any country.</p>
-                <p>The cryptocurrency feature of decentralized transactions allows full privacy of banking, having no regards to national borders or transaction amounts.</p>
+                <p>During the Pre-Launch Phase, members will be accepted into Kinetic Gold's member association.</p>
+                <p>Member's may build their Mining organization.</p>
+                <p>Kinetic Gold will make an initial deposit into the insured vaults a minimum of 1000 kg of hallmarked gold prior to Launch.  This base amount of Gold is used during the deposit process to set the initial value of new coins.</p>
+                <p>Kinetic Gold will also obtain an off-shore bank that will provide the on-line access and Debit card access to the account.</p>
     </div>
 @stop

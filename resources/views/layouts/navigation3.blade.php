3847d395cc297df6bb0660bf17957168e794be15
Jay Potter <Jay.Potter@Materialink.com>
2018-05-28 18:26:03 -0700

    teset2

--- resources/views/layouts/navigation3.blade.php
+++ resources/views/layouts/navigation3.blade.php
@@ -1,7 +1,7 @@
 
     <div class="navcontainer" >
         <div class="navbar-brand brand-image">
-             KineticGold xxxx
+             KineticGold
         </div>
         <div class="referral" >
         @if(!isset($referral_link))
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,8 +15,7 @@
     <div class="center kinetic">
              <p>Kinetic Gold</p>
     </div>
-    <div style="z-index:-5; height:1100px; background-attachment:fixed; background-size:100% 100%; background-image:  url({{$imageUrl}});">
-       </div>
+    <img src="..images/denmark.jpg" >
     <div >
                 <p>Kinetic Gold is a new cryptocurrency4</p>
     </div>

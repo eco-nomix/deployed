99edb206ad91919e6190335fa5f8a806ce5f09c3
Jay Potter <Jay.Potter@Materialink.com>
2018-05-30 13:32:56 -0700

    images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,8 @@
         <p>Kinetic Gold is a new cryptocurrency2</p>
       </div>
 
-    <div class="center kinetic400">
-             <p>Kinetic Gold</p>
+    <div class="center ">
+             <p class="kinetic400">Kinetic Gold</p>
     </div>
 
 

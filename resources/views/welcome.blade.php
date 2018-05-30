486a0932caf4b185e40d97113f22f70ff4409a4f
Jay Potter <Jay.Potter@Materialink.com>
2018-05-30 13:07:21 -0700

    images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,20 +4,21 @@
 
 @section('content')
    <div style="z-index:-5; height:1100px; background-attachment:fixed; background-size:100% 100%; background-image:url('/images/denmark.jpeg');">
-   </div>
-{{--<div class="kineticbackground">--}}
-   <div >
-     <p>Kinetic Gold is a new cryptocurrency1</p>
-   </div>
-   <div >
-     <p>Kinetic Gold is a new cryptocurrency2</p>
-   </div>
     <div >
         <p>Kinetic Gold is a new cryptocurrency1</p>
       </div>
       <div >
         <p>Kinetic Gold is a new cryptocurrency2</p>
       </div>
+       <div >
+           <p>Kinetic Gold is a new cryptocurrency1</p>
+         </div>
+         <div >
+           <p>Kinetic Gold is a new cryptocurrency2</p>
+         </div>
+   </div>
+{{--<div class="kineticbackground">--}}
+
     {{--<div class="center kinetic">--}}
              {{--<p>Kinetic Gold</p>--}}
     {{--</div>--}}

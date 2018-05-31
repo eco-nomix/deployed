416e9352e2bdd66119a3aea2e337427ddeb427a7
Jay Potter <Jay.Potter@Materialink.com>
2018-05-31 11:30:04 -0700

    purpose

--- resources/views/membercost.blade.php
+++ resources/views/membercost.blade.php
@@ -11,14 +11,14 @@
     <div class="trans row">
         <div class="col-md-8 col-md-offset-2">
             <div style="width:100%;">
-                            <div class="kinetic400">
+                            <div class="kinetic600">
                                   Member Costs for KineticGold
                             </div>
                         </div>
 
 
             <div class="panel panel-default display">
-               
+
                 <div class="panel-body">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <div class="form-group col-md-12 ">

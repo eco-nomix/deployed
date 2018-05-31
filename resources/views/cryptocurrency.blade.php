16f41507fdd28f5e81dda678db881b2a25beadbb
Jay Potter <Jay.Potter@Materialink.com>
2018-05-31 11:37:08 -0700

    purpose

--- resources/views/cryptocurrency.blade.php
+++ resources/views/cryptocurrency.blade.php
@@ -6,7 +6,7 @@
 <div style="position:absolute; top:52px; z-index:-1">
 <div style="width:100%">
 <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
-
+   <div class="skip">&nbsp;</div>
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div style="width:100%;">

d803c096d77e9c46e9037890090377a549d5bbd5
Jay Potter <Jay.Potter@Materialink.com>
2018-05-31 13:40:27 -0700

    interface

--- resources/views/bankinterface.blade.php
+++ resources/views/bankinterface.blade.php
@@ -20,33 +20,53 @@
 
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
                             <div class="form-group col-md-12 ">
-                            All Members will receive a KineticGold Debit Card to access their offshore account.
+                            Kinetic Gold is much more than another cryptocurrency.  It is a complete world accessible banking platform that is backed by Gold.
                              </div>
 
                              <div class="form-group col-md-12 ">
-                                This card will have the Visa or Mastercharge Logo on it.
+                                The banking platform is controlled by an Off-Shore bank that uses the Kinetic Gold blockchain to record all transactions.
                              </div>
                             <div class="form-group col-md-12 ">
-                             This card is good at any location which accepts credit and/or debit cards including ATM's.
+                             Since the blockchain is not hackable, the bank has a stable records platform that is totally secure even if attached by hackers.
                              </div>
                              <div class="form-group col-md-12 ">
-                            This card will be issued as soon as the Off-Shore bank is on line.
+                            The blockchain is totally decentralized, but also totally encrypted.
                             </div>
                             <div class="form-group col-md-12 ">
-                            KineticGold Profit-Sharing and Mining Bonuses are deposited directly in your Off-Shore account / cryptocurrency and can immediately be access through your Debit Card.
-                            </div>
-
-                            <div class="form-group col-md-12 ">
-                            The card balance is in ounces of Gold on deposit with Kinetic Gold and the value in fiat currency will vary as to the
-                            spot price of gold.
+                            This gives you, the account holder total security.
                             </div>
 
                             <div class="form-group col-md-12 ">
-                           The debit cards are only good for the current amount within your crypto-currency account / Off-shore account.
+                            Your account balance is insured by Lloyd's of London for the full value of your deposit.  There is no limit of $250,000 USD as when insured by USDA.
                             </div>
 
                             <div class="form-group col-md-12 ">
-                           The Debit cards will be a standard issue Debit Card issued by our bank.  It will have KineticGolds branding on it.
+                            The balance of your account at the Off-Shore account is always the value of your KineticGold cryptocurrency account.  There is no need to transfer from
+                            one account to the other, since they are reflections of one another.
+                            </div>
+
+                            <div class="form-group col-md-12 ">
+                            The initial services of the Off-Shore bank will include:
+                                <ul>
+                                    <li>Payroll Deposits</li>
+                                    <li>Debit Cards</li>
+                                    <li>On-Line Acconting</li>
+                                    <li>SWIFT wire transfers</li>
+                                    <li>linkage to your cryptocurrency account</li>
+                                    <li>ATM access</li>
+                                    <li>Credit Card Processing</li>
+                                    <li>Merchant Accounts - interface with Kinetic Gold</li>
+                                        <ul>
+                                            <li>No Visa/Mastercharge fees from Kinetic Gold account holders</li>
+                                            <li>World Wide Accessibility</li>
+                                            <li>Increase Profitability and transfer speed</li>
+                                        </ul>
+
+                                </ul>
+                            </div>
+
+                            <div class="form-group col-md-12 ">
+                            The Off-Shore bank will provide other banking services as the KineticGold System expands.
                             </div>
 
 

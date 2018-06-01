2282954faa471f430cad77032dd06a6d2f2b7715
Jay Potter <Jay.Potter@Materialink.com>
2018-05-31 19:41:47 -0700

    limitations

--- resources/views/referrallinks.blade.php
+++ resources/views/referrallinks.blade.php
@@ -19,13 +19,17 @@
 
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <div class="form-group col-md-12 ">
-                            All KineticGold Members have the opportunity to refer new clients to thKineticGoldix family simply by registering.
+                            All KineticGold Members have the opportunity to refer new clients to KineticGold family.
                         </div>
                         <div class="form-group col-md-12 ">
                             Once registered and logged in the Referral Link below will show 'Your Referral Link'.
                         </div>
                        <div class="form-group col-md-12 ">
-                            Use the Referral Link below to help grow your organization and your income possibilities.  Simply encourage potential clients to initially access the site using your referral link.  When they do, the site knows who referred them and if they register, you will receive the credit of 'Sponsoring' them . They will have full access to the site and can determine first if they want to complete their registeration or not. </div>
+                            Use the Referral Link below to help grow your organization and your income possibilities.
+                            Simply encourage potential clients to initially access the site using your referral link.
+                            When they do, the site knows who referred them and if they register, you will receive the
+                            credit of 'Sponsoring' them . They will have full access to the site and can determine
+                            first if they want to complete their registeration or not. </div>
                         <div class="form-group">
                             <label class="col-md-4 control-label">Referral Link</label>
                             <div class="col-md-6">
@@ -35,12 +39,16 @@
                 </div>
             </div>
             <div class="form-group col-md-12 standout">
-                  Suggestions:  These links can be placed on business cards, added to emails,  facebook, tweets,  anyway you want to get the word out.  If anyone uses one of these links.  You get the credit
+                  Suggestions:  These links can be placed on business cards, added to emails,  facebook, tweets,
+                  anyway you want to get the word out.  If anyone uses one of these links.  You get to Mine Gold
             </div>
         </div>
           <div class="skip">&nbsp;</div>
           <div class="skip">&nbsp;</div>
-            <div class="skip">&nbsp;</div>
+          <div class="skip">&nbsp;</div>
+          <div class="skip">&nbsp;</div>
+          <div class="skip">&nbsp;</div>
+          <div class="skip">&nbsp;</div>
     </div>
     </div>
 </div>

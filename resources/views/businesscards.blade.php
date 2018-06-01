5d1f2a3e97da3c024b48a139f2141fd06c5ab087
Jay Potter <Jay.Potter@Materialink.com>
2018-06-01 13:00:17 -0700

    businesscards

--- resources/views/businesscards.blade.php
+++ resources/views/businesscards.blade.php
@@ -55,6 +55,8 @@
                         <div class="form-group col-md-12 ">
                             If you wish to have your own business cards printed locally using the KineticGold graphics, you may download the image here.
                             <button>Download</button>
+                            Suggestion, a lowcost solution is the use the images with VistaPrint.com or Gotprint.com.  They have packages that start from $10.00 including shipping.
+
                         </div>
 
                 </div>

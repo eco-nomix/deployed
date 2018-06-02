4e28de46c7a62c5f2d50b8d217db8db26ece6c61
Jay Potter <Jay.Potter@Materialink.com>
2018-06-02 10:27:59 -0700

    businesscards

--- resources/views/memberterms.blade.php
+++ resources/views/memberterms.blade.php
@@ -10,7 +10,7 @@
                      <div class="col-md-8 col-md-offset-2">
                          <div style="width:100%;">
                              <div class="kinetic600">
-                                  Potentials
+                                  Terms and Conditions
                              </div>
                          </div>
                          <div class="panel panel-default display">

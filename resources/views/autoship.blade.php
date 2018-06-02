9f0e40976cf805fe24acfa5daf6e200a50efb327
Jay Potter <Jay.Potter@Materialink.com>
2018-06-01 20:34:39 -0700

    businesscards

--- resources/views/autoship.blade.php
+++ resources/views/autoship.blade.php
@@ -22,21 +22,8 @@
                         <div class="form-group col-md-12 ">
                             Kinetic Gold does not sell products that have an Auto-Ship Option.
                         </div>
-                       
-                        <div class="form-group col-md-12 ">
-                            Since there are no on-going costs required to be a member, there is no reason for anyone in your down-line to quit, so your organization just keeps growing over time.
-                        </div>
-                        <div class="form-group col-md-12 ">
-                            Over time, your organization's total membership could grow to thousands of members.  Now how fast that occurs is determined by:
-                        </div>
-                        <div class="form-group col-md-12 ">
-                            <ul>
-                                <li>How well you understand the opportunity of KineticGold. <br>Read and understand the contents of KineticGold's web-site.</li>
-                                <li>How consistent you are in spreading the word. <br>Keep your business cards with you.</li>
-                                <li>How many business cards and referral links you hand out.</li>
-                                <li>Encourage your down-line Members to do the same.</li>
-                            </ul>
-                        </div>
+
+                      
                         <div class="form-group col-md-12 ">
                             Become creative in how you get the word out,  your referral link is key to your success.
                         </div>

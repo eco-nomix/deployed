c906cbf498c3499c129aa1bd2cc868268f75f79e
Jay Potter <Jay.Potter@Materialink.com>
2018-06-01 20:08:46 -0700

    businesscards

--- resources/views/autoship.blade.php
+++ resources/views/autoship.blade.php
@@ -4,10 +4,11 @@
 
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
-<div style="width:100%;">
-    <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
-       <div class="skip">&nbsp;</div>
-        <div class="trans row">
+<div style="width:100%">
+    {{--<div class="pagecontainer"><img src="{{URL::to('/')}}/images/On-Line Accounting-small.jpg" ></div>--}}
+<div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
+   <div class="skip">&nbsp;</div>
+    <div class="trans row">
         <div class="col-md-8 col-md-offset-2">
          <div style="width:100%;">
                                 <div class="kinetic600">
@@ -34,7 +35,7 @@
                 <div class="skip">&nbsp;</div>
                  <div class="skip">&nbsp;</div>
                   <div class="skip">&nbsp;</div>
-                  
+
         </div>
     </div>
     </div>

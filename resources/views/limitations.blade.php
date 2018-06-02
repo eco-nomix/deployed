9e0156ffb57862dd48911dbe252f3fd510d5e5a0
Jay Potter <Jay.Potter@Materialink.com>
2018-06-01 20:23:37 -0700

    businesscards

--- resources/views/limitations.blade.php
+++ resources/views/limitations.blade.php
@@ -5,21 +5,19 @@
 
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
-<div style="width:100%">
+    <div style="width:100%">
     {{--<div class="pagecontainer"><img src="{{URL::to('/')}}/images/On-Line Accounting-small.jpg" ></div>--}}
-<div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
-   <div class="skip">&nbsp;</div>
-    <div class="trans row">
-        <div class="col-md-8 col-md-offset-2">
-         <div style="width:100%;">
-                        <div class="kinetic600">
-                            Limitations on Sponsoring
-                        </div>
+        <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
+            <div class="skip">&nbsp;</div>
+            <div class="trans row">
+            <div class="col-md-8 col-md-offset-2">
+                <div style="width:100%;">
+                    <div class="kinetic600">
+                        Limitations on Sponsoring
                     </div>
-            <div class="panel panel-default display">
-
-                <div class="panel-body">
-
+                </div>
+                <div class="panel panel-default display">
+                    <div class="panel-body">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <div class="form-group col-md-12 ">
                             There are no limits to the number of clients you can personally sponsor.
@@ -67,19 +65,19 @@
                          </div>
 
 
+                    </div>
                 </div>
+                <div class="skip">&nbsp;</div>
+                <div class="skip">&nbsp;</div>
+                <div class="skip">&nbsp;</div>
+                <div class="skip">&nbsp;</div>
+                <div class="skip">&nbsp;</div>
+                <div class="skip">&nbsp;</div>
+                <div class="skip">&nbsp;</div>
+                <div class="skip">&nbsp;</div>
+                <div class="skip">&nbsp;</div>
             </div>
- <div class="skip">&nbsp;</div>
-  <div class="skip">&nbsp;</div>
-    <div class="skip">&nbsp;</div>
-      <div class="skip">&nbsp;</div>
-        <div class="skip">&nbsp;</div>
-          <div class="skip">&nbsp;</div>
-            <div class="skip">&nbsp;</div>
-            <div class="skip">&nbsp;</div>
-                        <div class="skip">&nbsp;</div>
         </div>
     </div>
-</div>
 </div>
 @endsection

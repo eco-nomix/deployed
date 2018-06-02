a0c962cfe333467e2546f01debbce8af2e0cd44e
Jay Potter <Jay.Potter@Materialink.com>
2018-06-02 13:32:04 -0700

    businesscards

--- resources/views/profitsharing.blade.php
+++ resources/views/profitsharing.blade.php
@@ -114,7 +114,7 @@
                                  Let's assume that they average $1000 per month in deposits and spending.
                                <ul>
                                   <li>&nbsp;&nbsp;10 members in the 1st Generation -  a total of $10,000 in deposits - $100/month in Profit-Sharing.</li>
-                                  <li>&nbsp;&nbsp;50 members in the 2st Generation - a total of $50,000 in deposits - $200/month in Profit-Sharing.</li>
+                                  <li>&nbsp;&nbsp;50 members in the 2nd Generation - a total of $50,000 in deposits - $200/month in Profit-Sharing.</li>
                                   <li>150 members in the 3rd Generation - a total of $150,000 in deposits - $600/month in Profit-Sharing.</li>
                                   <li>a total of $900 per month</li>
                                </ul>

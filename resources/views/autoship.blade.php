e1c44c0dc1a0d4b72d31972cbb82f1f3eeeeb083
Jay Potter <Jay.Potter@Materialink.com>
2018-06-01 20:06:47 -0700

    businesscards

--- resources/views/autoship.blade.php
+++ resources/views/autoship.blade.php
@@ -4,36 +4,39 @@
 
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
-<div class="container-fluid">
-    <div class="row">
+<div style="width:100%;">
+    <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
+       <div class="skip">&nbsp;</div>
+        <div class="trans row">
         <div class="col-md-8 col-md-offset-2">
+         <div style="width:100%;">
+                                <div class="kinetic600">
+                                    Auto-Ship Policy
+                                </div>
+                            </div>
             <div class="panel panel-default display">
                 <div class="panel-heading">Auto-Ship Policy</div>
                 <div class="panel-body">
 
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <div class="form-group col-md-12 ">
-                            There may be some products that will offer a discount for auto-shipping the product for planned consumption.  However KineticGold discourages using such auto-ship programs until you first determine that the auto-ship policy fits within the consumption needs for your family.
-                        </div>
-                        <div class="form-group col-md-12 ">
-                            Try before you commit to reoccuring purchases.
-                        </div>
-                        <div class="form-group col-md-12 ">
-                           If you find that product inventory is beginning to accumulate beyond your consumption, cancel or modify the auto-ship terms immediately.
-                         </div>
-                       <div class="form-group col-md-12 ">
-                          All auto-ship products sold through KineticGold allow a Member to cancel, modify the quantities and term for the auto-ship.  Please be aware that such modification may result to a change in the auto-ship price.  If the new terms are not acceptable and meet the needs of your family, please cancel the auto-ship completely.
-                       </div>
-                       <div class="form-group col-md-12 ">
-                            Your reoccuring products will be found under your personal menu.
-                       </div>
-                       <div class="form-group col-md-12 ">
-                            Auto-ship products may be returned under the normal product return conditions.
-                       </div>
+                            Kinetic Gold does not sell products that require or utilize Auto-Ship    </div>
+
                 </div>
             </div>
-
+         <div class="skip">&nbsp;</div>
+          <div class="skip">&nbsp;</div>
+           <div class="skip">&nbsp;</div>
+            <div class="skip">&nbsp;</div>
+             <div class="skip">&nbsp;</div>
+              <div class="skip">&nbsp;</div>
+               <div class="skip">&nbsp;</div>
+                <div class="skip">&nbsp;</div>
+                 <div class="skip">&nbsp;</div>
+                  <div class="skip">&nbsp;</div>
+                  
         </div>
+    </div>
     </div>
 </div>
 </div>

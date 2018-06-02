42e9b3dbd241b209b8e242847db0c9c8e7b9c553
Jay Potter <Jay.Potter@Materialink.com>
2018-06-01 23:49:27 -0700

    businesscards

--- resources/views/comparison.blade.php
+++ resources/views/comparison.blade.php
@@ -5,50 +5,49 @@
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
 <div class="container-fluid">
-    <div class="pagecontainer"><img src="{{URL::to('/')}}/images/comparison-small.jpg" ></div>
 
-    {{--<div class="row">--}}
-        {{--<div class="col-md-8 col-md-offset-2">--}}
-            {{--<div class="panel panel-default display">--}}
-                {{--<div class="panel-heading">Comparison of KineticGoldld referral marketing with standard Multi-Level Marketing</div>--}}
-                {{--<div class="panel-body">--}}
 
-                        {{--<input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                           {{--Unlike standard Multi-Level programs, there are no on-going costs to participate, no required purchases, no products to market or inventory, no required sales, no collection of money, and no hassles.--}}
-                        {{--</div>--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                          {{--With KineticGoldGoldix referral bonus system, there are no reasons to quit the program, so 'churn' is effectively eliminated, allowing member organizations to grow at their maximum potential and leading to the maximum potential income.--}}
-                        {{--</div>--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                          {{--Can you imagine what can happen to an organization over time, that is continually grKineticGoldicGoldomix has effectively harnessed the potential of exponential growth.--}}
-                        {{--</div>--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                          {{--People will get involved because they are interested in the prKineticGoldeticGold-nomix carries and the referral bonus program is a no-risk benefit.--}}
-                        {{--</div>--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                          {{--Over time a given member's organization can grow very wide, with the same potential being available to each member of the organization.--}}
-                        {{--</div>--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                          {{--Success isn't limited to just the few at the 'top' of the organization, as each person is always qualified to receive all their bonuses.  People aren't skipped over to enrich the elite.--}}
-                        {{--</div>--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                          {{--Eco-nomics is a real opportunity not only for those that are seeking the best eco friendly products and to buy them at the best possible prices.  It is also an opportunity to benefit from--}}
-                          {{--sharing your experiences with Eco-nomics and its products.  The opportunity to make a supplemental income by sharing in products you believe in is especially interesting, as it is not limited--}}
-                          {{--to where you live, or even who you know.  There is finally a system where the average 'Joe' can make some extra money initially, and over time build a real income.--}}
-                        {{--</div>--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                        KineticGoldineticGoldco-nomix represents a way to achieve a greater degree of independence, not only physically but also financially.  For many, the ability to earn a few hundred dollars a month, can make all the difference.  If this describesKineticGolduKineticGold Eco-nomix is for you.  If you have the dream of financial independence, that also can be addressed.  It won't be overnight, but nothing worth while ever is.--}}
-                        {{--</div>--}}
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default display">
+                <div class="panel-heading">Comparison of KineticGold Profit Sharing (Mining)  with standard Multi-Level Marketing</div>               <div class="panel-body">
 
-                        {{--<div class="form-group col-md-12 ">--}}
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <div class="form-group col-md-12 ">
+                           Unlike standard Multi-Level programs, there are no on-going costs to participate, no required purchases, no products to market or inventory, no required sales, no collection of money, and no hassles.
+                        </div>
+                        <div class="form-group col-md-12 ">
+                          With KineticGoldGoldix referral bonus system, there are no reasons to quit the program, so 'churn' is effectively eliminated, allowing member organizations to grow at their maximum potential and leading to the maximum potential income.
+                        </div>
+                        <div class="form-group col-md-12 ">
+                          Can you imagine what can happen to an organization over time, that is continually grKineticGoldicGoldomix has effectively harnessed the potential of exponential growth.
+                        </div>
+                        <div class="form-group col-md-12 ">
+                          People will get involved because they are interested in the prKineticGoldeticGold-nomix carries and the referral bonus program is a no-risk benefit.
+                        </div>
+                        <div class="form-group col-md-12 ">
+                          Over time a given member's organization can grow very wide, with the same potential being available to each member of the organization.
+                        </div>
+                        <div class="form-group col-md-12 ">
+                          Success isn't limited to just the few at the 'top' of the organization, as each person is always qualified to receive all their bonuses.  People aren't skipped over to enrich the elite.
+                        </div>
+                        <div class="form-group col-md-12 ">
+                          Eco-nomics is a real opportunity not only for those that are seeking the best eco friendly products and to buy them at the best possible prices.  It is also an opportunity to benefit from
+                          sharing your experiences with Eco-nomics and its products.  The opportunity to make a supplemental income by sharing in products you believe in is especially interesting, as it is not limited
+                          to where you live, or even who you know.  There is finally a system where the average 'Joe' can make some extra money initially, and over time build a real income.
+                        </div>
+                        <div class="form-group col-md-12 ">
+                        KineticGoldineticGoldco-nomix represents a way to achieve a greater degree of independence, not only physically but also financially.  For many, the ability to earn a few hundred dollars a month, can make all the difference.  If this describesKineticGolduKineticGold Eco-nomix is for you.  If you have the dream of financial independence, that also can be addressed.  It won't be overnight, but nothing worth while ever is.
+                        </div>
 
-                        {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
+                        <div class="form-group col-md-12 ">
 
-        {{--</div>--}}
-    {{--</div>--}}
+                        </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </div>
 </div>
 @endsection

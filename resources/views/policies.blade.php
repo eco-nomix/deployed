d25b4336154cafdf7b8bb01bdaf06c799f635174
Jay Potter <Jay.Potter@Materialink.com>
2018-06-02 14:43:02 -0700

    businesscards

--- resources/views/policies.blade.php
+++ resources/views/policies.blade.php
@@ -4,16 +4,21 @@
 
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default display">
-                <div class="panel-heading">KineticGoldld Inc. Policies and Procedures</div>
-                <div class="panel-body">
-
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                        <div class="form-group col-md-12 ">
+ <div style="width:100%">
+     <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
+         <div class="skip">&nbsp;</div>
+             <div class="trans row">
+                 <div class="col-md-8 col-md-offset-2">
+                     <div style="width:100%;">
+                         <div class="kinetic600">
+                              Policies and Procedures
                          </div>
+                     </div>
+                     <div class="panel panel-default display">
+
+                        <div class="panel-body">
+
+                         <input type="hidden" name="_token" value="{{ csrf_token() }}">    <div class="col-md-12 indent">
                         <div class="form-group col-md-12 ">
                     	1.1 Mutual Commitment Statement
                     	</div>

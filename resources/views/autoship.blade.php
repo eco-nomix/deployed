1454fcbe9ac5d453dcb8b91cf5fa82eb9969f858
Jay Potter <Jay.Potter@Materialink.com>
2018-06-01 20:27:16 -0700

    businesscards

--- resources/views/autoship.blade.php
+++ resources/views/autoship.blade.php
@@ -27,49 +27,12 @@
                         <div class="form-group col-md-12 ">
                             KineticGold does not sell products that have an autoship option
                         </div>
+
                         <div class="form-group col-md-12 ">
-                            Your personal organization depth is limited to 5 generations.
                         </div>
                         <div class="form-group col-md-12 ">
-                            There are no limitations to the total number of people in your organization.
-                         </div>
-                        <div class="form-group col-md-12 ">
-                            Since there are no on-going costs required to be a member, there is no reason for anyone in your down-line to quit, so your organization just keeps growing over time.
+
                         </div>
-                        <div class="form-group col-md-12 ">
-                            Over time, your organization's total membership could grow to thousands of members.  Now how fast that occurs is determined by:
-                        </div>
-                        <div class="form-group col-md-12 ">
-                            <ul>
-                                <li>How well you understand the opportunity of KineticGold. <br>Read and understand the contents of KineticGold's web-site.</li>
-                                <li>How consistent you are in spreading the word. <br>Keep your business cards with you.</li>
-                                <li>How many business cards and referral links you hand out.</li>
-                                <li>Encourage your down-line Members to do the same.</li>
-                            </ul>
-                        </div>
-                        <div class="form-group col-md-12 ">
-                            Become creative in how you get the word out,  your referral link is key to your success.
-                        </div>
-                         <div class="form-group col-md-12 ">
-                            Put your referral link in your email, ads, or notices.
-                         </div>
-                         <div class="form-group col-md-12 ">
-                             Obviously, your organizations size alone will not create any income for you.  All Profit Sharing is earned
-                             from deposits that are made,  so encourage your down-line to know what works to spread the word, and to discover
-                             how they can be utilized.
-                          </div>
-                         <div class="form-group col-md-12 ">
-                             Remember that a great many people are highly motivated to become self-sufficient.  As these perilous times further unfold, more and more people will want to become such.
-                         </div>
-
-
-                         <div class="form-group col-md-12 ">
-                          </div>
-                         <div class="form-group col-md-12 ">
-
-                         </div>
-
-
                     </div>
                 </div>
                 <div class="skip">&nbsp;</div>

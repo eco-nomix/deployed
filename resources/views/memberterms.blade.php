09ec452d2d47c3716e98b77bdf71de959db4144d
Jay Potter <Jay.Potter@Materialink.com>
2018-06-02 10:17:10 -0700

    businesscards

--- resources/views/memberterms.blade.php
+++ resources/views/memberterms.blade.php
@@ -17,91 +17,92 @@
                          of the Terms and Conditions without notice.
                         <br><br>
                         KineticGold.org reserves the right to change these terms and conditions at any time, and you agree to abide
-                        by the most recent version of this Terms and Conditions Agreement each time you view and use the Website KineticGold, KineticGold's
+                        by the most recent version of this Terms and Conditions Agreement each time you view and use the website KineticGold, KineticGold's
                          debit cards, and/or training material.  Do not use the business cards, debit cards and/or training material
                          if you do not agree to all of these terms and conditions.
                         <br><br>
-                         ILLEGAL ACTIVITY
+                         ILLEGAL ACTIVITY<br>
                         The member agrees not to use KineticGold.org or any of it's linked websites for promotions of any page, newsgroup, email or any distribution
                         method that KineticGold, its Internet Service Providers, or otherwise is considered unlawful
                         according to any controlling legal authority.  This includes, but is not limited to, pornography, computer viruses,
                         prostitution, obscenity or Spam.
                         <br><br>
-                        THIRD-PART
+                        THIRD-PARTY <br>
                         KineticGold will in no way be liable for the actions of third parties that may in any way cause members harm.
                         <br><br>
                         KineticGold reserves the right to update, modify, add or change these terms and conditions without notice.
                         <br><br>
-                        TERMINATION
+                        TERMINATION<br>
                         The registration and/or Membership of any reg KineticGold user can be terminated at anytime by KineticGold without giving any reason.  Registered Users can cancel their account anytime directly from their Personal Options pages on the website.
                         <br><br>
                         PAYMENT PROCESSOR
                         KineticGold Rewards and Profit Sharing is paid through the KineticGold account and its related Debit card.
 
                         <br><br>
-                        ETHICS
+                        ETHICS<br>
                         Any Registered User acting in an unethical or unprofessional manner may be terminated with all future Profit Sharing and Rewards forfeited.
                         Any accrued Profit Sharing or Rewards earned prior to termination will be available for the usage of the User.
                         <br><br>
-                        CANCELLATION/TERMINATION OF MEMBERSHIP
+                        CANCELLATION/TERMINATION OF MEMBERSHIP<br>
                         Any Registered User that is canceled/terminated by KineticGold will loose all benefits associated with KineticGold.  Any sponsored organization
                         be reassigned to KineticGold.org.  The member will be allow to close out and receive all funds associated with the account.
                         <br><br>
-                        RELATIONSHIP OF PARTIES
+                        RELATIONSHIP OF PARTIES<br>
                         While the parties shall work hand-in-hand for the benefit of both, the parties acknowledge and agree that the Registered User shall,
                         from a legal perspective, act as an independent contractor and is not an employee of KineticGold.  Nothing in this Agreement or these
                         Terms and Conditions shall create a partnership, joint venture, agency, or franchise between the parties in the legal sense of these
                         terms.  The Registered Use has no authority to sign any document in behalf of KineticGold.
                         <br><br>
-                        DISPUTES
+                        DISPUTES<br>
                         Agreements shall be interpreted under the the laws of the Nevis.  <br><br>
-                        COLATERIAL
+                        COLATERIAL<br>
                         All KineticGold logos, and graphics contained within KineticGold.org are copyrighted.
                          <br><br>
-                        USE
+                        USE<br>
                           Distribution or copyrighted KineticGold material is expressly prohibited.  KineticGold.org provides Registered Users with approved KineticGold literature
                           for marketing.  Any 3rd party or self-created KineticGold material must be first approved by KineticGold.org before it may be used.
                         <br><br>
-                        DAMAGING INTENT
+                        DAMAGING INTENT<br>
                         Any Registered User of KineticGold who engages in chat, email, posting of any material about KineticGold, or that could be damaging to KineticGold or KineticGold.org,
                         may be santioned by KineticGold.  Depending on seriousness, KineticGold may deem it appropriate to take appropriate action.
                         <br><br>
-                         UCE/UBE or SPAM
+                         UCE/UBE or SPAM<br>
                           KineticGold.org strictly prohibits the use of SPAM.  This enforcement is at the sole discretion of KineticGold.org and for the
                     benefit of all Registered Users.  Registered Users proven to be participating in such activities will have their
                     account(s) terminated and they will forfeit any and all current or future Profit Sharing.
                         <br><br>
-                        CONTACT INFORMATION
+                        CONTACT INFORMATION<br>
                     It is the responsibility of the Registered User to Keep their personal records up to date at the members area.
                     KineticGold.org will not be responsible for communication error due to incorrect or out of date contact information.
                     Continued failed attempts to make contact with you may result in the termination of your account.
                         <br><br>
-                        TAX
+                        TAX<br>
                         You, the Registered User, are responsible for any and all taxes payable in your resident jurisdiction, for any income you
                         receive either from KineticGolds Rewards or Profit Sharing.
                         <br><br>
-                        NO WARRANTY
+                        NO WARRANTY<br>
                         All information, training, etc. is provided “As Is” and without any warranty, express, implied, or otherwise,
                         regarding its accuracy or performance.
                         <br><br>
-                        NO ASSIGNMENT
+                        NO ASSIGNMENT<br>
                         Registered Users may not assign or transfer any rights or obligations under this Agreement without the prior
                         written consent of KineticGold.
                         <br><br>
                 SEVERABILITY
+                <br>
                         Registered Users and KineticGold.org agree that if any portion of this agreement is found illegal or unenforceable,
                         that portion shall be severed and the remainder of the agreement shall be given full force and effect.
-                        <br><br>  LIMITATION OF LIABILITY; SOLE AND EXCLUSIVE: INDEMNIFICATION
+                        <br><br>  LIMITATION OF LIABILITY; SOLE AND EXCLUSIVE: INDEMNIFICATION<br>
                         You agree that KineticGold.org is NOT responsible for the success or failure of your business or your business decisions
                         relating to any information presented by our company or our companies produces and/or services.
                         <br><br>
-              ACCURATENESS OF INFORMATION
+              ACCURATENESS OF INFORMATION<br>
                         KineticGold.org makes no representations as to the accurateness or reliability of any advertising message or
                         marketing tool offered through any referral, outside company or on this site.  While we believe the people/companies
                         we work with to be honest and ethical, we recommend you do your own due diligence relating to any specific claims
                         or promised made in the marketing message, as it relates to your use and purchase.
                         <br><br>
-                        CROSS SPONSORING/CROSS RECRUITING
+                        CROSS SPONSORING/CROSS RECRUITING<br>
                         Cross sponsoring, cross recruiting and cross line jumping are prohibited per the company’s terms and conditions and the
                         Participants agreement.  “Cross Sponsoring” means a soliciting a Register User or any closely related person or
                         entity into a downline different from the existing downline for the Associate, or a closely related person or entity.
@@ -110,7 +111,7 @@
                          is any person in the household of the Registered User [e.g. Spouse, Son, Daughter, Parents living in the same household]
                          or any Legal entity which is controlled by the Registered User.
                         <br><br>
-                        INCOME REPRESENTATION
+                        INCOME REPRESENTATION<br>
                         Marketing Associates shall not make any income representations except those set forth herein or otherwise specifically
                         set forth in official company material
                         <br><br>

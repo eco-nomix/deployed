d3adb21f11f834b5f71f383abf04cffc6d0a3c6a
Jay Potter <Jay.Potter@Materialink.com>
2018-06-02 00:15:31 -0700

    businesscards

--- resources/views/comparison.blade.php
+++ resources/views/comparison.blade.php
@@ -64,6 +64,9 @@
             </div>
 
         </div>
+        <div class="skip">&nbsp;</div>
+        <div class="skip">&nbsp;</div>
+        <div class="skip">&nbsp;</div>
     </div>
     </div>
 </div>

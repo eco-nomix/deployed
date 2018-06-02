86222a3c928199c7e2f52997062107a7f4b3cdce
Jay Potter <Jay.Potter@Materialink.com>
2018-06-01 20:34:04 -0700

    businesscards

--- resources/views/autoship.blade.php
+++ resources/views/autoship.blade.php
@@ -22,12 +22,7 @@
                         <div class="form-group col-md-12 ">
                             Kinetic Gold does not sell products that have an Auto-Ship Option.
                         </div>
-                        <div class="form-group col-md-12 ">
-                            Your personal organization depth is limited to 5 generations.
-                        </div>
-                        <div class="form-group col-md-12 ">
-                            There are no limitations to the total number of people in your organization.
-                         </div>
+                       
                         <div class="form-group col-md-12 ">
                             Since there are no on-going costs required to be a member, there is no reason for anyone in your down-line to quit, so your organization just keeps growing over time.
                         </div>

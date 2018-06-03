c76ca2babb9d0e2d2b8b91409b8d47d7e6177d0f
Jay Potter <Jay.Potter@Materialink.com>
2018-06-02 20:01:26 -0700

    businesscards

--- resources/views/returns.blade.php
+++ resources/views/returns.blade.php
@@ -60,6 +60,10 @@
                 </div>
             </div>
             <div class="skip">&nbsp;</div>
+              <div class="skip">&nbsp;</div>
+              <div class="skip">&nbsp;</div>
+              <div class="skip">&nbsp;</div>
+              <div class="skip">&nbsp;</div>
         </div>
     </div>
 </div>

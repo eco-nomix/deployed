38c38cffa0d7a345a92e6e0d0b5608665b3c5ae5
Jay Potter <Jay.Potter@Materialink.com>
2018-06-02 20:13:06 -0700

    businesscards

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,9 +4,17 @@
 
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
-<div class="container-fluid">
-    <div class="row">
-           <div class="col-md-8 col-md-offset-2">
+    <div style="width:100%">
+    {{--<div class="pagecontainer"><img src="{{URL::to('/')}}/images/On-Line Accounting-small.jpg" ></div>--}}
+        <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
+            <div class="skip">&nbsp;</div>
+            <div class="trans row">
+            <div class="col-md-8 col-md-offset-2">
+                <div style="width:100%;">
+                    <div class="kinetic600">
+                        Contact
+                    </div>
+                </div>
                <div class="panel panel-default display">
                    <div class="panel-heading">Contact KineticGold Inc.</div>
                    <div class="panel-body">
@@ -42,9 +50,15 @@
 
                    </div>
                </div>
-
+  <div class="skip">&nbsp;</div>
+    <div class="skip">&nbsp;</div>
+      <div class="skip">&nbsp;</div>
+        <div class="skip">&nbsp;</div>
+          <div class="skip">&nbsp;</div>
+          
            </div>
        </div>
+   </div>
    </div>
    </div>
 

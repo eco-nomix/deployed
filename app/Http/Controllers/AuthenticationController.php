651ccd57002e9c4e2f5aa30fffffeccd227e1ce1
Jay Potter <Jay.Potter@Materialink.com>
2018-06-03 09:20:12 -0700

    businesscards

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -504,7 +504,7 @@ class AuthenticationController extends Controller
         $data['errors'] = '';
         $data['title'] = 'Opportunity';
         $data['description'] = 'Opportunity to Change Your Life';
-        $data['imageUrl'] = '../images/HandsPlant.jpg';
+        $data['imageUrl'] = '../images/HandsPlant.jpeg';
         $data['message'] = 'An Opportunity to Change Your Life! ';
         $data['message2'] = 'What we choose to do today will start out small
             like a seedling, but over time can become great for all the world to

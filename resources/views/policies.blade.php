a0a2fd8e5bf87e8f98b22bc6978928603abad163
Jay Potter <Jay.Potter@Materialink.com>
2018-06-02 19:33:05 -0700

    businesscards

--- resources/views/policies.blade.php
+++ resources/views/policies.blade.php
@@ -605,55 +605,55 @@
                         </div>
                         <div class="form-group col-md-12 ">
                         5.3 Adjustments to Bonuses for Returned Products or Member Memberships.
-                        {{--</div>--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                             {{--Profit-Sharing bonuses are based on the actual deposit into KineticGold member bank accounts.--}}
-                                {{--Any adjustment to said deposits will cause Profit-Sharing bonuses to be recalculated and adjusted.</li>--}}
+                        </div>
+                        <div class="form-group col-md-12 ">
+                             Profit-Sharing bonuses are based on the actual deposit into KineticGold member bank accounts.
+                                Any adjustment to said deposits will cause Profit-Sharing bonuses to be recalculated and adjusted.</li>
 
-                        {{--</div>--}}
+                        </div>
 
-                        {{--<div class="form-group col-md-12 ">--}}
-                        {{--6.0 SATISFACTION GUARANTEED AND RETURN OF SALES AIDS--}}
-                        {{--</div>--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                         {{--<ol type="A">--}}
-                           {{--<li>KineticGold offers a one hundred percent (100%) thirty-day money back guarantee for all non customized products.</li>--}}
-                           {{--<li>With respect to return of Sales Aids, KineticGold  offers refund opportunities depending on the product or service purchased.--}}
-                           {{--Shipping and handling incurred will not be refunded.</li>--}}
-                           {{--<li>Customized products (such as business cards) may only be returned if the customized information (name, address, etc) is other than as ordered. </li>--}}
-                         {{--</ol>--}}
-                        {{--</div>--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                        {{--6.1 Return Process--}}
-                        {{--</div>--}}
-                       {{--<div class="form-group col-md-12 ">--}}
-                            {{--<ol type="A">--}}
-                              {{--<li>All returns, whether by a Customer, or Member, must be made as follows:--}}
-                                    {{--<ol type="I">--}}
-                                        {{--<li>Obtain Return Merchandise Authorization from KineticGold;</li>--}}
-                                        {{--<li>Ship items to the address provided to KineticGold Customer service when order was placed</li>--}}
-                                        {{--<li>A copy of the orinal invoice is returned with the returned products or service. Such invoice must reference the RMA and include the reason for the return.</li>--}}
-                                        {{--<li>Ship back product in manufacturer’s box exactly as was delivered.</li>--}}
-                                    {{--</ol></li>--}}
-                                {{--<li>All returns must be shipped to KineticGold pre-paid, as KineticGold does not accept shipping collect packages. KineticGold recommends shipping--}}
-                                {{--returned products by UPS or FedEx with tracking and insurance as risk of loss or damage in shipping of the returned product shall be borne solely--}}
-                                {{--by the Customer, or Member. If returned product is not received at KineticGold's Distribution Center, it is the responsibility of the Customer,--}}
-                                {{--or Member to trace the shipment and no credit will be applied.</li>--}}
-                                {{--<li>The return of $500 or more of products for a refund within a calendar year, by a Member, may constitute grounds for involuntary termination.</li>--}}
-                  {{--</ol>--}}
+                        <div class="form-group col-md-12 ">
+                        6.0 SATISFACTION GUARANTEED AND RETURN OF SALES AIDS
+                        </div>
+                        <div class="form-group col-md-12 ">
+                         <ol type="A">
+                           <li>KineticGold offers a one hundred percent (100%) thirty-day money back guarantee for all non customized products.</li>
+                           <li>With respect to return of Sales Aids, KineticGold  offers refund opportunities depending on the product or service purchased.
+                           Shipping and handling incurred will not be refunded.</li>
+                           <li>Customized products (such as business cards) may only be returned if the customized information (name, address, etc) is other than as ordered. </li>
+                         </ol>
+                        </div>
+                        <div class="form-group col-md-12 ">
+                        6.1 Return Process
+                        </div>
+                       <div class="form-group col-md-12 ">
+                            <ol type="A">
+                              <li>All returns, whether by a Customer, or Member, must be made as follows:
+                                    <ol type="I">
+                                        <li>Obtain Return Merchandise Authorization from KineticGold;</li>
+                                        <li>Ship items to the address provided to KineticGold Customer service when order was placed</li>
+                                        <li>A copy of the orinal invoice is returned with the returned products or service. Such invoice must reference the RMA and include the reason for the return.</li>
+                                        <li>Ship back product in manufacturer’s box exactly as was delivered.</li>
+                                    </ol></li>
+                                <li>All returns must be shipped to KineticGold pre-paid, as KineticGold does not accept shipping collect packages. KineticGold recommends shipping
+                                returned products by UPS or FedEx with tracking and insurance as risk of loss or damage in shipping of the returned product shall be borne solely
+                                by the Customer, or Member. If returned product is not received at KineticGold's Distribution Center, it is the responsibility of the Customer,
+                                or Member to trace the shipment and no credit will be applied.</li>
+                                <li>The return of $500 or more of products for a refund within a calendar year, by a Member, may constitute grounds for involuntary termination.</li>
+                  </ol>
 
-                        {{--</div>--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                        {{--7.0 PRIVACY POLICY--}}
-                        {{--</div>--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                        {{--7.1 Introduction--}}
-                        {{--</div>--}}
-                         {{--<div class="form-group col-md-12 ">--}}
-                        {{--<p style="KineticGoldft: 20px">This Privacy Policy is to ensure that all Customers and Members understand and adhere to KineticGold principles of confidetiality</p>--}}
-                        {{--</div>--}}
-                        {{--<div class="form-group col-md-12 ">--}}
-                        {{--7.2 Expectation of Privacy--}}
+                        </div>
+                        <div class="form-group col-md-12 ">
+                        7.0 PRIVACY POLICY
+                        </div>
+                        <div class="form-group col-md-12 ">
+                        7.1 Introduction
+                        </div>
+                         <div class="form-group col-md-12 ">
+                        <p style="KineticGoldft: 20px">This Privacy Policy is to ensure that all Customers and Members understand and adhere to KineticGold principles of confidetiality</p>
+                        </div>
+                        <div class="form-group col-md-12 ">
+                        7.2 Expectation of Privacy
                         {{--</div>--}}
                         {{--<div class="form-group col-md-12 ">--}}
                             {{--<ol type="A">--}}

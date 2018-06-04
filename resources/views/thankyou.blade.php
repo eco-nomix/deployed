db0aa49bf938c65ef3949affe04d2bbf9519ef54
Jay Potter <Jay.Potter@Materialink.com>
2018-06-04 14:47:22 -0700

    login

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -2,9 +2,17 @@
 
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+    <div style="width:100%">
+    {{--<div class="pagecontainer"><img src="{{URL::to('/')}}/images/On-Line Accounting-small.jpg" ></div>--}}
+        <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
+            <div class="skip">&nbsp;</div>
+            <div class="trans row">
+            <div class="col-md-8 col-md-offset-2">
+                <div style="width:100%;">
+                    <div class="kinetic600">
+                       Thank You
+                    </div>
+                </div>
             <div class="panel panel-default">
                 <div class="panel-heading">Thank You </div>
                 <div class="panel-body">
@@ -27,6 +35,7 @@
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </div>
 </div>

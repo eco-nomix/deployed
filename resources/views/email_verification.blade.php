f86c5fcb422be6a1f32b45a873f84ef94968a33b
Jay Potter <Jay.Potter@Materialink.com>
2018-06-04 14:04:09 -0700

    login

--- resources/views/email_verification.blade.php
+++ resources/views/email_verification.blade.php
@@ -4,12 +4,19 @@
 
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
-<div style="position:absolute; top:52px; z-index:-1">
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-2">
+    <div style="width:100%">
+    {{--<div class="pagecontainer"><img src="{{URL::to('/')}}/images/On-Line Accounting-small.jpg" ></div>--}}
+        <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
+            <div class="skip">&nbsp;</div>
+            <div class="trans row">
+            <div class="col-md-8 col-md-offset-2">
+                <div style="width:100%;">
+                    <div class="kinetic600">
+                        Thank you for Registering
+                    </div>
+                </div>
             <div class="panel panel-default">
-                <div class="panel-heading">Thank you for registering
+
                   <div class="panel-body">
                     <div class="form-group">
                         <label class="col-md-10 col-md-offset-1 control-label">An Email was sent to {{$email}} with a link that will confirm your email address.</label>
@@ -20,6 +27,18 @@
                   </div>
                 </div>
             </div>
+            <div class="skip">&nbsp;</div>
+            <div class="skip">&nbsp;</div>
+            <div class="skip">&nbsp;</div>
+            <div class="skip">&nbsp;</div>
+            <div class="skip">&nbsp;</div>
+            <div class="skip">&nbsp;</div>
+            <div class="skip">&nbsp;</div>
+            <div class="skip">&nbsp;</div>
+            <div class="skip">&nbsp;</div>
+            <div class="skip">&nbsp;</div>
+            <div class="skip">&nbsp;</div>
+            <div class="skip">&nbsp;</div>
         </div>
     </div>
 </div>

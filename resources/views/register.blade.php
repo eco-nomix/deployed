85187b72111cb31745cd1788a0bd490fe9c02ecc
Jay Potter <Jay.Potter@Materialink.com>
2018-06-04 13:55:07 -0700

    login

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,11 +2,18 @@
 
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+    <div style="width:100%">
+    {{--<div class="pagecontainer"><img src="{{URL::to('/')}}/images/On-Line Accounting-small.jpg" ></div>--}}
+        <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
+            <div class="skip">&nbsp;</div>
+            <div class="trans row">
+            <div class="col-md-8 col-md-offset-2">
+                <div style="width:100%;">
+                    <div class="kinetic600">
+                        Register
+                    </div>
+                </div>            <div class="panel panel-default">
+
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="/register">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -69,6 +76,7 @@
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </div>
 </div>

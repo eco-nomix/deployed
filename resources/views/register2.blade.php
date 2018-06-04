2259392eaa4ab3584d1ab3f11822180963345d41
Jay Potter <Jay.Potter@Materialink.com>
2018-06-04 14:08:36 -0700

    login

--- resources/views/register2.blade.php
+++ resources/views/register2.blade.php
@@ -2,11 +2,19 @@
 
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+    <div style="width:100%">
+    {{--<div class="pagecontainer"><img src="{{URL::to('/')}}/images/On-Line Accounting-small.jpg" ></div>--}}
+        <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
+            <div class="skip">&nbsp;</div>
+            <div class="trans row">
+            <div class="col-md-8 col-md-offset-2">
+                <div style="width:100%;">
+                    <div class="kinetic600">
+                        Register Contact Info
+                    </div>
+                </div>
             <div class="panel panel-default">
-                <div class="panel-heading">Register Contact Info</div>
+
                 <div class="panel-body">
                        <form class="form-horizontal" role="form" method="POST" action="/register2">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -64,7 +72,7 @@
                             </div>
                         </div>
                         <div class="form-group">
-                            <label class="col-md-4 control-label">Social Security Number</label>
+                            <label class="col-md-4 control-label">Social Security Number optional</label>
                             <div class="col-md-6">
                                 <input type="text" class="form-control" name="social_security">
                             </div>
@@ -80,6 +88,18 @@
                 </div>
             </div>
         </div>
+
+    </div>
+      <div class="skip">&nbsp;</div>
+        <div class="skip">&nbsp;</div>
+          <div class="skip">&nbsp;</div>
+            <div class="skip">&nbsp;</div>
+              <div class="skip">&nbsp;</div>
+                <div class="skip">&nbsp;</div>
+                  <div class="skip">&nbsp;</div>
+                    <div class="skip">&nbsp;</div>
+                      <div class="skip">&nbsp;</div>
+                        <div class="skip">&nbsp;</div>
     </div>
 </div>
 </div>

415d3e446d33ab259fc82945fd61bde523148264
Jay Potter <Jay.Potter@Materialink.com>
2018-06-04 14:23:34 -0700

    login

--- resources/views/registration_complete.blade.php
+++ resources/views/registration_complete.blade.php
@@ -3,7 +3,7 @@
 
 
      @section('content')
-     <div style="position:absolute; top:52px; z-index:-1">
+     <div style="position:absolute; top:52px; z-index:-1; right:0; left:auto; width:100%;">
          <h1>Registration is already Completed</h1>
 
      </div>

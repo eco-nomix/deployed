9aeb1f5a832e80499b8f211a0a0ef599063942f3
Jay Potter <Jay.Potter@Materialink.com>
2018-06-05 11:04:03 -0700

    login

--- resources/views/action.blade.php
+++ resources/views/action.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.blank')
 
 @section('content')
-<div style="z-index:-10; top:0px; left:-0px; position:absolute; width:1410px; height:1010px; border-style:solid; border-color:blue; border-width:1px;">
+<div style="z-index:-10; top:0px; left:-0px; position:absolute; width:1450px; height:1010px; border-style:solid; border-color:blue; border-width:1px;">
 
        <div style="z-index:-5; top:0px; left:0px; position:absolute; height:500px; width:900px; " >
           <img src="{{$imageUrl}}" width="900">

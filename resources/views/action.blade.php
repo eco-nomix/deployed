a7f8b2ea24e89017cddf876e93c97f6cfa27c24a
Jay Potter <Jay.Potter@Materialink.com>
2018-06-05 11:36:40 -0700

    login

--- resources/views/action.blade.php
+++ resources/views/action.blade.php
@@ -24,7 +24,7 @@
        <div class="Kineticaction3" style=" position:relative; top:100px; z-index:12; font-size:60px; left:101px; width:400px">
                 Free Information<br><br>&nbsp;&nbsp;Instant Access
        </div>
-       <div style=" height:500px;width:500px; top:0px; left:0px; position:relative; ">
+       <div style=" height:500px;width:500px; top:0px; left:0px; position:absolute; ">
 
             <div style=" height:500px;width:500px;   position:relative; left:0px; top:0px;">
                    <img src="{{$image2}}" height="500px">

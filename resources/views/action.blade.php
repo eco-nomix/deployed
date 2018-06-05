8f3b8f6e7faa577c243d45cf72ca638d3769b059
Jay Potter <Jay.Potter@Materialink.com>
2018-06-05 12:16:22 -0700

    login

--- resources/views/action.blade.php
+++ resources/views/action.blade.php
@@ -31,16 +31,79 @@
                     Free Information<br><br>&nbsp;&nbsp;Instant Access
            </div>
         </div>
-       {{----}}
-
-
-      {{----}}
-
-    </td>
+     </td>
 </tr>
 <tr height="500">
     <td width="900"></td>
-    <td width="500"></td>
+    <td width="500">
+        <div style="width:100%; height:100%">
+
+              <form class="form-horizontal" role="form" method="POST" action="/register">
+                  <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                  <div class="form-group">
+                       <div class="col-md-8">
+                           &nbsp;
+                       </div>
+                  </div>
+                  <div class="form-group">
+                      <label class="col-md-4 control-label">First Name</label>
+                      <div class="col-md-8">
+                          <input type="text" class="form-control" name="first_name" value="">
+                      </div>
+                  </div>
+
+                  <div class="form-group">
+                      <label class="col-md-4 control-label">Last Name</label>
+                      <div class="col-md-8">
+                          <input type="text" class="form-control" name="last_name" value="">
+                      </div>
+                  </div>
+
+                  <div class="form-group">
+                      <label class="col-md-4 control-label">E-Mail Address</label>
+                      <div class="col-md-8">
+                          <input type="email" class="form-control" name="email" value="">
+                      </div>
+                  </div>
+
+                  <div class="form-group">
+                      <label class="col-md-4 control-label">New User Name</label>
+                      <div class="col-md-8">
+                          <input type="text" class="form-control" name="username" value="">
+                      </div>
+                  </div>
+
+                  <div class="form-group">
+                      <label class="col-md-4 control-label">New Password</label>
+                      <div class="col-md-8">
+                          <input type="password" class="form-control" name="password">
+                      </div>
+                  </div>
+
+                  <div class="form-group">
+                      <label class="col-md-4 control-label">Confirm New Password</label>
+                      <div class="col-md-8">
+                          <input type="password" class="form-control" name="password_confirmation">
+                      </div>
+                  </div>
+                  <div class="form-group">
+
+                      <div class="col-md-8">
+                           &nbsp;
+                      </div>
+                  </div>
+                  <div class="form-group">
+                      <div class="col-md-8 col-md-offset-4">
+                          <button type="submit" class="btn btn-primary">
+                              Get Started Free
+                          </button>
+                      </div>
+                  </div>
+              </form>
+
+          </div>
+
+    </td>
 </tr>
 </table>
 {{--<div style="z-index:-10; top:0px; left:0px; position:absolute; width:1850px; height:510px; border-style:solid; border-color:blue; border-width:1px;">--}}
@@ -52,72 +115,7 @@
                       {{--<div style="width:450px; height:506px;border:black 2px solid; ">--}}
                           {{--<div class="panel panel-default">--}}
                               {{--<div class="panel-heading message2 center" >Get Started Today!</div>--}}
-                              {{--<div class="panel-body">--}}
-
-                                  {{--<form class="form-horizontal" role="form" method="POST" action="/register">--}}
-                                      {{--<input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
-                                      {{--<div class="form-group">--}}
-                                           {{--<div class="col-md-8">--}}
-                                               {{--&nbsp;--}}
-                                           {{--</div>--}}
-                                      {{--</div>--}}
-                                      {{--<div class="form-group">--}}
-                                          {{--<label class="col-md-4 control-label">First Name</label>--}}
-                                          {{--<div class="col-md-8">--}}
-                                              {{--<input type="text" class="form-control" name="first_name" value="">--}}
-                                          {{--</div>--}}
-                                      {{--</div>--}}
-
-                                      {{--<div class="form-group">--}}
-                                          {{--<label class="col-md-4 control-label">Last Name</label>--}}
-                                          {{--<div class="col-md-8">--}}
-                                              {{--<input type="text" class="form-control" name="last_name" value="">--}}
-                                          {{--</div>--}}
-                                      {{--</div>--}}
-
-                                      {{--<div class="form-group">--}}
-                                          {{--<label class="col-md-4 control-label">E-Mail Address</label>--}}
-                                          {{--<div class="col-md-8">--}}
-                                              {{--<input type="email" class="form-control" name="email" value="">--}}
-                                          {{--</div>--}}
-                                      {{--</div>--}}
-
-                                      {{--<div class="form-group">--}}
-                                          {{--<label class="col-md-4 control-label">New User Name</label>--}}
-                                          {{--<div class="col-md-8">--}}
-                                              {{--<input type="text" class="form-control" name="username" value="">--}}
-                                          {{--</div>--}}
-                                      {{--</div>--}}
-
-                                      {{--<div class="form-group">--}}
-                                          {{--<label class="col-md-4 control-label">New Password</label>--}}
-                                          {{--<div class="col-md-8">--}}
-                                              {{--<input type="password" class="form-control" name="password">--}}
-                                          {{--</div>--}}
-                                      {{--</div>--}}
-
-                                      {{--<div class="form-group">--}}
-                                          {{--<label class="col-md-4 control-label">Confirm New Password</label>--}}
-                                          {{--<div class="col-md-8">--}}
-                                              {{--<input type="password" class="form-control" name="password_confirmation">--}}
-                                          {{--</div>--}}
-                                      {{--</div>--}}
-                                      {{--<div class="form-group">--}}
-
-                                          {{--<div class="col-md-8">--}}
-                                               {{--&nbsp;--}}
-                                          {{--</div>--}}
-                                      {{--</div>--}}
-                                      {{--<div class="form-group">--}}
-                                          {{--<div class="col-md-8 col-md-offset-4">--}}
-                                              {{--<button type="submit" class="btn btn-primary">--}}
-                                                  {{--Get Started Free--}}
-                                              {{--</button>--}}
-                                          {{--</div>--}}
-                                      {{--</div>--}}
-                                  {{--</form>--}}
-
-                              {{--</div>--}}
+                          </div>
 
                           {{--</div>--}}
                       {{--</div>--}}

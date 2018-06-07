15e998641dfec41f74bb54c70141726c646ff8c0
Jay Potter <Jay.Potter@Materialink.com>
2018-06-06 22:57:04 -0700

    email verification

--- resources/views/businesscards.blade.php
+++ resources/views/businesscards.blade.php
@@ -34,7 +34,7 @@
                           performed by the referred member.
                         </div>
                         <div class="form-group col-md-12 ">
-                            <img src="{{URL::to('/')}}/images/business_card.png">
+                            <img src="{{URL::to('/')}}/images/kineticGold-BusinessCard.jpeg">
                         </div>
                         <div class="form-group col-md-12 ">
                             Your business card will be extremely helpful in helping you build your organization.

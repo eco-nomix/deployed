d3fd46971fac40b67a37b23eac387315d863f435
Jay Potter <Jay.Potter@Materialink.com>
2018-06-06 23:20:30 -0700

    email verification

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,12 +3,20 @@
 
 
 @section('content')
-<div style="position:absolute; top:52px; z-index:-1">
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+<div style="position:absolute; top:52px; z-index:-1;  right:0; left:auto; width:100%">
+    <div style="width:100%">
+    {{--<div class="pagecontainer"><img src="{{URL::to('/')}}/images/On-Line Accounting-small.jpg" ></div>--}}
+        <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
+            <div class="skip">&nbsp;</div>
+            <div class="trans row">
+            <div class="col-md-8 col-md-offset-2">
+                <div style="width:100%;">
+                    <div class="kinetic600">
+                        Thank you for Registering
+                    </div>
+                </div>
             <div class="panel panel-default display">
-                <div class="panel-heading">About KineticGold</div>
+
                 <div class="panel-body">
 
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -96,6 +104,15 @@
 
             </div>
         </div>
+    </div>
+    </div>
+     <div class="skip">&nbsp;</div>
+      <div class="skip">&nbsp;</div>
+       <div class="skip">&nbsp;</div>
+        <div class="skip">&nbsp;</div>
+         <div class="skip">&nbsp;</div>
+          <div class="skip">&nbsp;</div>
+           <div class="skip">&nbsp;</div>
     </div>
 </div>
 </div>

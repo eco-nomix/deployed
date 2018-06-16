0de25fb4a63cda7de5bb32e58bb5c621b12b1339
Jay Potter <Jay.Potter@Materialink.com>
2018-06-15 22:55:32 -0700

    reset

--- resources/views/presentation.blade.php
+++ resources/views/presentation.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div style="position:absolute; top:52px; z-index:-1">
  <iframe src="http://www.youtube.com/embed/m0nXhyKceOE"
-   width="560" height="315" frameborder="0" allowfullscreen></iframe>
+   width="960" height="540" frameborder="0" allowfullscreen></iframe>
 </div>
 
 @endsection

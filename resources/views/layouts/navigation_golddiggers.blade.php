a00bb55332599f132c191706aa66a6919ddc7c72
Jay Potter <Jay.Potter@Materialink.com>
2018-06-18 15:32:16 -0700

    update for gold diggers

--- resources/views/layouts/navigation_golddiggers.blade.php
+++ resources/views/layouts/navigation_golddiggers.blade.php
@@ -25,7 +25,7 @@
                         <li><a href="{{URL::to('/')}}/membercost2">Membership Cost</a></li>
 
 
-                        <li><a href="{{URL::to('/')}}/benefits">Member Benefits</a></li>
+                        <li><a href="{{URL::to('/')}}/benefits2">Member Benefits</a></li>
                           <li><a href="{{URL::to('/')}}/requirements">Member Requirements</a></li>
                         <li><a href="{{URL::to('/')}}/mem_agreement">Membership Agreement</a></li>
                         <li><a href="{{URL::to('/')}}/mem_terms">Membership Terms and Conditions</a></li>

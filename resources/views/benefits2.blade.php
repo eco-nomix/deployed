024282683505b65a93ad42913b82fbd18460d2d1
Jay Potter <Jay.Potter@Materialink.com>
2018-06-18 16:00:38 -0700

    update for gold diggers

--- resources/views/benefits2.blade.php
+++ resources/views/benefits2.blade.php
@@ -7,96 +7,108 @@
  <div style="width:100%">
      <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover; background-attachment:fixed;  background-image:url('/images/denmark.jpeg');">
          <div class="skip">&nbsp;</div>
-             <div class="trans row">
-                 <div class="col-md-8 col-md-offset-2">
-                     <div style="width:100%;">
-                         <div class="kinetic600">
-                              Benefits
-                         </div>
+         <div class="trans row">
+             <div class="col-md-8 col-md-offset-2">
+                 <div style="width:100%;">
+                     <div class="kinetic600">
+                          Benefits
                      </div>
-                     <div class="panel panel-default display">
+                 </div>
+                 <div class="panel panel-default display">
 
-                        <div class="panel-body">
-
-                         <input type="hidden" name="_token" value="{{ csrf_token() }}">    <div class="col-md-12 indent">
-         <div class="form-group col-md-12 ">
-         KineticGold is a unique company dedicated in delivering the best banking platform possible to its customers. <br><br>
-        KineticGold's goal is to make each customer more financially stable.
-        <br><br>Summary of KineticGold's Benefits.
-
-       </div>
-        <div class="form-group col-md-12 ">
-        <ol>
-            <li> <p>A member receives a bank account with Kinetic Golds<a href="\offshorebank"> Off-Shore bank.</a> This comes with a <a href="\debitcards">Debit Card</a> that is branded with
-            Visa or Mastercard, giving him access to merchants and ATMs around the world.  For any Deposit that a member makes he receive a <a href="\rewards">Reward</a> of 10%.
-            Since there is no membership fee, the benefits of Membership is immediately realized without risk.</p>
-            </li>
-
-            <li>
-                <p>A member can promote KineticGold's banking platform and receive <a href="\profitsharing">Profit Sharing</a> of 1% of whatever the sponsored member deposits.</p>
-            </li>
-            <li>
-               <p>The Profit Sharing continue for up to 5 generations of referrals in total.  The member receives a .4% Profit Sharing on the 2nd to 5th generations.
-                Because the 5 generation of down-line members can grow both <a href="\limitations">extremely wide</a> and deep,  the number of people down-line can
-                be grow over time to a large organization.  Each deposit by a member will result in Profit Sharing for the upline members
-                through 5 generations.  This can start to add up pretty quickly.  Since there are no on-going fees to participate this could
-                represent <a href="\potential">significant income</a> through a large organization over time.</p>
-
-            </li>
-            <li>
-                <p>Profit Sharing can be earned by all Members.</p>
-            </li>
-
-            <li>
-                <p>All Profit Sharing is credited immediately to the member.  The site provides <a href="\accounting">instant updates</a>
-                so the member always is aware of makeup of their group, down through 5 generations and any Profit Sharing that have been
-                processed to their credit.  </p>
-
-            </li>
-
-            <li>
-                <p>The member can purchase <a href="\businesscards">business cards</a> with their name printed on them plus a member's
-                referring link.  This link can be delivered even using electronic links.  This enables a member to refer someone
-                simply by given them a card.  When the prospective member receives the link, they will be directed to this site.
-                If they join the referral is already established so you get the credit for the referral.</p>
-
-            </li>
-            <li>
-                <p>There are no <a href="\requirements">special requirements</a> in order to receive any Profit Sharing from those that you refer.
-                All product is sold through this website.  You do not make any direct sales and collect no money from customers.</p>
-            </li>
-            <li>
-                <p>There are <a href="\membercost">no hidden fees</a> or charges.  Even a small sponsored organization
-                can generate a positive income for the member without ever having to put up any cash.</p>
-            </li>
+                    <div class="panel-body">
 
 
-            <li>
-                <p>There are no mandatory automatic <a href="\autoship">auto-ship products</a>.
-                No filling up your garage with unwanted or overstocked products.</p>
-            </li>
-            <li>
-                <p>KineticGold is poised to create the maximum income potential in <a href="\comparison">comparison</a> to Multi-Level marketing programs.
-            </li>
-        </ol>
-        </div>
+                         <div class="form-group col-md-12 ">
+                            Gold Diggers Association is dedicated to make our members more financially aware and stable.<br><br>
+
+                            <br><br>Summary of The Gold Diggers Association Benefits.
+                         </div>
+                        <div class="form-group col-md-12 ">
+                            <ul>
+                              <li>Members will receive the monthly Gold Digger's Association newsletter</li>
+                              <li>As new programs become available to Members, the website will be immediately updated along with an announcement in the monthly newsletter.</li>
+                              <li>Some of the programs offered to our Members will be exclusive to our members, but that will not always be the case.</li>
+                              <li>Gold Digger's Association will track 'sponsorship' of its members that can be utilized by the various programs offered to it's members</li>
+                              <li>Any financial benefits from participation with a program referred by Gold Diggers will be paid directly to the participants by the sponsoring program.
+                                   Gold Digger's doesn't pay any benefits to its members.</li>
+                              <li>Gold Digger's will only be offering information and training to its members.</li>
+                              <li>Kinetic Gold has entered into an agreement with Gold Diggers to provide exclusive benefits to the members of the Association.
+                                   <ul>
+                                        <li>Members will receive a KineticGold Gold-backed cryptocurrency account.</li>
+                                         <li>Members will receive an e-wallet which can access this account</li>
+                                         <li>Members will receive a banking account at an offshore bank affiliated with KineticGold.
+                                             <ul>
+                                                <li>Your account is linked with your KineticGold account.</li>
+                                                <li>Your account is denominated in ounces of Gold.</li>
+                                                <li>Gold in in storage at a secure vault in Dubai.</li>
+                                                <li>Account is insured to the full amount of deposit.</li>
+                                                <li>Account will have a Mastercard/Visa Debit card associated with it.</li>
+                                                <li>You set the limit that can be spent per transaction.</li>
+                                                <li>This type of account is normally not available and would be quite expensive to obtain</li>
+                                             </ul>
+                                        </li>
+                                        <li>The banking account will interact directly with their cryptocurrency account.</li>
+                                       <li>The banking account will include a Debit card where you can access your accounts anywhere in the world through the Visa/Mastercard platform.</li>
+                                       <li>Members automatically qualify for KineticGold rewards program.  Rewards are automatically credited into your savings account at your offshore bank account.</li>
+                                       <li>Members can participate in KineticGold's profit sharing and mining program
+                                       </li>
+                                         <li>Establish and update your down-line for Profit-Sharing within the Blockchain.
+                                           <ul>
+                                               <li>Awards for deposits is controlled by the Blockchain</li>
+                                               <li>Profit-Sharing is controlled by the Blockchain</li>
+                                           </ul>
+                                       </li>
+                                   </ul>
+                              </li>
 
 
 
-     <div class="form-group col-md-12 ">
-       <div class="col-md-12 standout">
-          <p>All these significant differences show you why KineticGold is positioned to make a real difference in your life.
-             Using the right banking platform, for the right reasons, will help you to prepare
-             for some pretty unpredictable times ahead.  </p><p> And last of all is an on-going income stream that is coming to you,
-             not because of having to make constant purchases of unwanted products, but using a new banking system to pay your existing expenses,  and
-              because of
-             simple referrals to an organization that thinks like you.</p><p> At last there is a company you can look forward to working with.</p>
-       </div>
+
+
+
+                              <li>Help you Promote your downline organization
+                                  <ul>
+                                      <li>Make members aware of Association Events
+                                          <ul>
+                                              <li>Training on how Kinetic Gold works</li>
+                                              <li>Training on sponsoring your organization</li>
+                                          </ul>
+
+                                      </li>
+                                  </ul>
+                               </li>
+                               <li>Maintain the downline organization
+                                  <ul>
+                                      <li>You can see who is in your downline at the GoldDiggers website.</li>
+                                      <li>You can contact members of your downline through the GoldDiggers website.</li>
+                                  </ul>
+                               </li>
+                               <li>Gold Diggers will have products to promote KineticGold and the Association but none of them will be required to purchase under any situation.</li>
+
+                          </ul>
+                        </div>
+
+
+
+                         <div class="form-group col-md-12 ">
+                           <div class="col-md-12 standout">
+                              <p>All these significant differences show you why KineticGold is positioned to make a real difference in your life.
+                                 Using the right banking platform, for the right reasons, will help you to prepare
+                                 for some pretty unpredictable times ahead.  </p><p> And last of all is an on-going income stream that is coming to you,
+                                 not because of having to make constant purchases of unwanted products, but using a new banking system to pay your existing expenses,  and
+                                  because of
+                                 simple referrals to an organization that thinks like you.</p><p> At last there is a company you can look forward to working with.</p>
+                           </div>
+                        </div>
+                    </div>
+                 </div>
+             </div>
+         </div>
+         <div class="skip">&nbsp;</div>
+         <div class="skip">&nbsp;</div>
     </div>
-    </div>
-<div class="skip">&nbsp;</div>
-<div class="skip">&nbsp;</div>
-</div>
+  </div>
 
 </div>
 @stop

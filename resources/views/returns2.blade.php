aa31442e2cfc0beaa7fdd6d164b6494fab37cac1
Jay Potter <Jay.Potter@Materialink.com>
2018-06-18 17:57:53 -0700

    update for gold diggers

--- resources/views/returns2.blade.php
+++ resources/views/returns2.blade.php
@@ -26,11 +26,11 @@
                         </div>
                         <div class="form-group col-md-12 ">
                          <ol type="A">
-                           <li>KineticGold offers a one hundred percent (100%) thirty-day money back guarantee for all non customized products.</li>
-                           <li>With respect to return of Sales Aids, KineticGold  offers refund opportunities depending on the product or service purchased.
+                           <li>The Gold Diggers Association offers a one hundred percent (100%) thirty-day money back guarantee for all non customized products.</li>
+                           <li>With respect to return of Sales Aids, The Gold Diggers Association  offers refund opportunities depending on the product or service purchased.
                            Shipping and handling incurred will not be refunded.</li>
                            <li>Customized products (such as business cards) may only be returned if the customized information (name, address, etc) is other than as ordered. </li>
-                           <li>KineticGold offers a one hundred percent (100%) three day money back guarantee for membership in the KineticGold Membership association during the Pre-Launch phase.  Once the Launch takes place this will be a non-refundable fee, as the expenses of setting up the account will be immediate.</li>
+                           <li>The Gold Diggers Association offers a one hundred percent (100%) three day money back guarantee for membership in the The Gold Diggers Association Membership association during the Pre-Launch phase.  Once the Launch takes place this will be a non-refundable fee, as the expenses of setting up the account will be immediate.</li>
                          </ol>
                         </div>
                         <div class="form-group col-md-12 ">
@@ -40,14 +40,14 @@
                             <ol type="A">
                               <li>All returns, whether by a Customer, or Member, must be made as follows:
                                     <ol type="i">
-                                        <li>Obtain Return Merchandise Authorization from KineticGold;</li>
-                                        <li>Ship items to the address provided to KineticGold Customer service when order was placed</li>
+                                        <li>Obtain Return Merchandise Authorization from The Gold Diggers Association;</li>
+                                        <li>Ship items to the address provided to The Gold Diggers Association Customer service when order was placed</li>
                                         <li>A copy of the orinal invoice is returned with the returned products or service. Such invoice must reference the RMA and include the reason for the return.</li>
                                         <li>Ship back product in manufacturer’s box exactly as was delivered.</li>
                                     </ol></li>
-                                <li>All returns must be shipped to KineticGold pre-paid, as KineticGold does not accept shipping collect packages. KineticGold recommends shipping
+                                <li>All returns must be shipped to The Gold Diggers Association pre-paid, as The Gold Diggers Association does not accept shipping collect packages. The Gold Diggers Association recommends shipping
                                 returned products by UPS or FedEx with tracking and insurance as risk of loss or damage in shipping of the returned product shall be borne solely
-                                by the Customer, or Member. If returned product is not received at KineticGold's Distribution Center, it is the responsibility of the Customer,
+                                by the Customer, or Member. If returned product is not received at The Gold Diggers Association's Distribution Center, it is the responsibility of the Customer,
                                 or Member to trace the shipment and no credit will be applied.</li>
                                 <li>The return of $500 or more of products for a refund within a calendar year, by a Member, may constitute grounds for involuntary termination.</li>
                   </ol>

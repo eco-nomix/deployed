2f40f8d097370c4b707beae4b46f7ba116eebcda
Jay Potter <Jay.Potter@Materialink.com>
2018-10-14 14:05:38 -0700

    tib 3

--- resources/views/tib.blade.php
+++ resources/views/tib.blade.php
@@ -21,13 +21,41 @@
 
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     <div class="form-group col-md-12 ">
-                        TIB Foundation is an association that is focused on making our members aware of opportunities in investments, banking and financing.  It will do this in the form
-                        of a monthly newletter that is sent to all of it's members detailing information about various investments, banking programs and opportunities, their risks and potential rewards along with a
-                        a detailed 'how-to' method to get involved if you are interested.
+                       The primary service of TIB FOUNDATION is assisting
+                       people in developing beginning level estate planning based
+                       on mortgage debt reduction.. TIB 1 FOUNDATION is not a mortgage
+                       lender or broker or fiduciary concern. It provides a service that
+                       allows potential home buyers access to the ability to own a home
+                       regardless of credit worthiness. Once a home is obtained TIB FOUNDATION
+                       will provide the homeowner with a system of debt reduction. The company
+                       uses traditional Mortgage pooling and other financial tools to accomplish
+                       it’s goals. Because the methods used are time tested and proven TIB FOUNDATION
+                       at no time presents undue risk of loss to homeowners.  </div>
+
+                    <div class="form-group col-md-12 ">
+                        TIB Foundation provides both basic education in the processes for the homeowner
+                        to reduce their debt.  This is provided through a subscription basis which delivers
+                        videos prepared by the TIB FOUNDATION to show various steps that need to be taken.  The Cost of
+                        the subscription is $50 per month.
+                    </div>
+                     <div class="form-group col-md-12 ">
+                        There are no requirements for members of the TIB Foundation to participate within any program.  This information is provided as 'information' only and is not
+                        to be construed as investment advise.  All member's need to do their own due diligence to determine if any particular opportunity fit's with your investment goals and/or
+                        styles, limits and desires.
+                    </div>  
+                    <div class="form-group col-md-12 ">
+                        TIB Foundation also will provide direct assistance to the homeowner
+                        to eliminate the homeowners debt by providing access to trained legal representation
+                        and provides both basic education in the processes for the homeowner
+                        to reduce their debt.  This is provided through a contract basis between the
+                        homeowner and the attorney.  The Cost for this service will be determined by the complexity
+                        of the case, but financing makes the process very easy to afford, almost always less than the mortgage payment
+                        that it would remove.
                     </div>
                     <div class="form-group col-md-12 ">
-                        It also provides Video Training in managing and controlling real estate difficulties including foreclosures to its members.
+                        Direct phone consultation is also available for members and outside council at a rate of $300 per hour.
                     </div>
+
                     <div class="form-group col-md-12 ">
                         The Goal of the TIB Foundation is to enable its members to It also provides Video Training in managing and controlling real estate difficulties including foreclosures to its members.
                     </div>
@@ -35,11 +63,7 @@
                     <div class="form-group col-md-12 ">
                         It also provides special promotions and benefits to its members that are only open to its members.
                     </div>
-                    <div class="form-group col-md-12 ">
-                        There are no requirements for members of the TIB Foundation to participate within any program.  This information is provided as 'information' only and is not
-                        to be construed as investment advise.  All member's need to do their own due diligence to determine if any particular opportunity fit's with your investment goals and/or
-                        styles, limits and desires.
-                    </div>
+
 
 
 

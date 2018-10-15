b8d33b1f25c2aaf467ce60175dd85d1600aeca5e
Jay Potter <Jay.Potter@Materialink.com>
2018-10-14 17:24:41 -0700

    tib 3

--- resources/views/autoship3.blade.php
+++ resources/views/autoship3.blade.php
@@ -7,7 +7,8 @@
 <div style="position:absolute; top:52px; z-index:-1;  right:0; left:auto; width:100%">
     <div style="width:100%">
     {{--<div class="pagecontainer"><img src="{{URL::to('/')}}/images/On-Line Accounting-small.jpg" ></div>--}}
-           <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover;
+         <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover;
+                                                                                                   background-attachment:fixed;  background-image:url('/images/pexels-photo-97079.jpeg');">
 
             <div class="skip">&nbsp;</div>
             <div class="trans row">

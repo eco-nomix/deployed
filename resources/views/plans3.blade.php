e70408a2308acc9566c7f910e5a99631b82ee5f1
Jay Potter <Jay.Potter@Materialink.com>
2018-10-15 14:49:22 -0700

    tib5

--- resources/views/plans3.blade.php
+++ resources/views/plans3.blade.php
@@ -6,6 +6,7 @@
 <div style="width:100%;">
 <div style="z-index:-5; overflow-y:scroll; height: 100%; background-size:cover;
  background-attachment:fixed;  background-image:url('/images/pexels-photo-97079.jpeg');">
+ 
   <div class="skip">&nbsp;</div>
 
     <div class="trans row">

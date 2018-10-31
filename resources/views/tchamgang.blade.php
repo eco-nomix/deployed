f6735b839c843547766bec241ad322dec8909023
Jay Potter <Jay.Potter@Materialink.com>
2018-10-31 10:10:45 -0700

    test4

--- resources/views/tchamgang.blade.php
+++ resources/views/tchamgang.blade.php
@@ -5,10 +5,10 @@
 <div style="position:absolute; top:52px; z-index:-1; width:100%">
 
 
-        <table width="100%" height="100%" border="1">
+        <table width="100%" height="750px" border="1">
             <tr>
             <td width="300px">
-                <table width="100%" height="800px" border="1">
+                <table width="100%" height="750px" border="1">
                     <tr height="50%">
                         <td>
                              &nbsp;
@@ -23,7 +23,7 @@
                 </table>
             </td>
             <td width="300px">
-                <table width="100%" height="800px" border="1">
+                <table width="100%" height="750px" border="1">
                     <tr height="50%">
                        <td>
                             &nbsp;

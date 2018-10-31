eb1e72e6faa667c00c9014568ad1c3a4fa7de1d0
Jay Potter <Jay.Potter@Materialink.com>
2018-10-31 12:34:10 -0700

    tchamgang picts

--- resources/views/tchamgang.blade.php
+++ resources/views/tchamgang.blade.php
@@ -5,45 +5,36 @@
 <div style="position:absolute; top:52px; z-index:-1; width:100%">
 
 
-        <table width="100%" height="750px" border="1">
+        <table width="600" height="750" border="1">
             <tr>
-            <td width="300px">
-                <table width="100%" height="750px" border="1">
-                    <tr >
-                        <td style="vertical-align: top;">
-                        {{--<img src="../images/fchamgang 1.jpeg" width="300" height="375"  />--}}
-                        </td>
 
-                    </tr>
-                    <tr >
-                         <td style="vertical-align: top;">
-                          {{--<img src="../images/fchamgang 1.jpeg" width="300" height="375" />--}}
 
-                         </td>
-                    </tr>
-                </table>
-            </td>
-            <td width="300px">
-                <table width="100%" height="750px" border="1">
-                    <tr >
-                       <td style="vertical-align: top;">
-                             {{--<img src="../images/fchamgang 1.jpeg" width="300" height="375"/>--}}
-                                                  ;
-                       </td>
+                <td width="300px" style="vertical-align: top;">
+                {{--<img src="../images/fchamgang 1.jpeg" width="300" height="375"  />--}}
+                </td>
 
-                   </tr>
-                   <tr >
-                        <td style="vertical-align: top;">
+
+                 <td width="300px" style="vertical-align: top;">
+                  {{--<img src="../images/fchamgang 1.jpeg" width="300" height="375" />--}}
+
+                 </td>
+
+            </tr>
+
+             <tr>
+
+
+                            <td width="300px" style="vertical-align: top;">
+                            {{--<img src="../images/fchamgang 1.jpeg" width="300" height="375"  />--}}
+                            </td>
+
+
+                             <td width="300px" style="vertical-align: top;">
                               {{--<img src="../images/fchamgang 1.jpeg" width="300" height="375" />--}}
 
-                        </td>
-                   </tr>
-                </table>
+                             </td>
 
-            </td>
-            <td >
-
-            </td>
+                        </tr>  
             </tr>
         </table>
 

582996849bd4a5bc78543df5c3b4353f1f0529f1
Jay Potter <Jay.Potter@Materialink.com>
2018-10-31 13:35:32 -0700

    tchamgang picts

--- resources/views/tchamgang.blade.php
+++ resources/views/tchamgang.blade.php
@@ -53,10 +53,12 @@ function myFunction() {
 }
 
 function changeImage() {
+
    var imageno = Math.floor(Math.random() * (4+1));
    var imagesrc = 'img'+imageno;
    var newimageno = Math.floor(Math.random() * (36+1));
    var imagepath = "../images/fchamgang "+newimageno+".jpeg";
+   console.log = imagepath;
    document.getElementById("demo").innerHTML = imagesrc+"  "+imagepath;
 //   document.getElementById(imagesrc).src = imagepath;
 }

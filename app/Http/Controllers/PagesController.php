96a797a3f30516095820303a159f1873462986ea
Jay Potter <Jay.Potter@Materialink.com>
2018-10-30 20:40:05 -0700

    test4

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -74,19 +74,19 @@ class PagesController extends Controller
             return view('tib',$data);
         }
         if(stristr($name,"tchamgang")){
-            $ch = curl_init('https://textbelt.com/text');
-            $data = array(
-                'phone' => '8646085404',
-                'message' => '11335577',
-                'key' => 'textbelt',
-            );
-
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-            $response = curl_exec($ch);
-            curl_close($ch);
+//            $ch = curl_init('https://textbelt.com/text');
+//            $data = array(
+//                'phone' => '8646085404',
+//                'message' => '11335577',
+//                'key' => 'textbelt',
+//            );
+//
+//            curl_setopt($ch, CURLOPT_POST, 1);
+//            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+//            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//
+//            $response = curl_exec($ch);
+//            curl_close($ch);
             $data = $this->userData($request);
             $data['title'] = 'tchamgang';
             $data['description'] = 'tchamgang';

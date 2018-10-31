62f3449022780c484571fdd43deba6d750b371ad
Jay Potter <Jay.Potter@Materialink.com>
2018-10-30 21:25:00 -0700

    test4

--- resources/views/tchamgang.blade.php
+++ resources/views/tchamgang.blade.php
@@ -5,17 +5,17 @@
 <div style="position:absolute; top:52px; z-index:-1; width:100%">
 
     <div class="container"></div>
-        <table width="100%" height="600px" border="1">
+        <table width="100%" height="100%" border="1">
             <tr>
             <td width="33%">
-                <table width="100%" height="600px" border="1">
-                    <tr height="300px">
+                <table width="100%" height="100%" border="1">
+                    <tr height="50%">
                         <td>
                              &nbsp;
                         </td>
 
                     </tr>
-                    <tr height="300px">
+                    <tr height="50%">
                          <td>
                               &nbsp;
                          </td>

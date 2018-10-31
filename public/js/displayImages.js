d104f9cbedafa0e96b1fbd471df32b4be9f26430
Jay Potter <Jay.Potter@Materialink.com>
2018-10-31 14:12:43 -0700

    images

--- public/js/displayImages.js
+++ public/js/displayImages.js
@@ -10,13 +10,13 @@
 
     function changeImage() {
 
-        var imageno = Math.floor(Math.random() * (4 + 1));
+        var imageno = Math.floor(Math.random() * (4))+1;
         var imagesrc = 'img' + imageno;
-        var newimageno = Math.floor(Math.random() * (36 + 1));
+        var newimageno = Math.floor(Math.random() * (36))+1;
         var imagepath = "../images/fchamgang " + newimageno + ".jpeg";
         console.log(imagesrc + "  " + imagepath);
        document.getElementById("demo").innerHTML = imagesrc+"  "+imagepath;
-//   document.getElementById(imagesrc).src = imagepath;
+       document.getElementById(imagesrc).src = imagepath;
     }
 
 

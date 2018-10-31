3b8dbd7ef3c1a019199a60d1065504437430d2a1
Jay Potter <Jay.Potter@Materialink.com>
2018-10-31 12:17:05 -0700

    tchamgang picts

--- resources/views/tchamgang.blade.php
+++ resources/views/tchamgang.blade.php
@@ -17,7 +17,7 @@
                     </tr>
                     <tr height="50%">
                          <td style="vertical-align: top;">
-                          <img src="../images/fchamgang 2.jpeg" width="300" height="375" />
+                          <img src="../images/fchamgang 1.jpeg" width="300" height="375" />
 
                                </td>
                     </tr>
@@ -27,14 +27,14 @@
                 <table width="100%" height="750px" border="1">
                     <tr height="50%">
                        <td style="vertical-align: top;">
-                             <img src="../images/fchamgang 3.jpeg" width="300" height="375"/>
+                             <img src="../images/fchamgang 1.jpeg" width="300" height="375"/>
                                                   ;
                        </td>
 
                    </tr>
                    <tr height="50%">
                         <td style="vertical-align: top;">
-                              <img src="../images/fchamgang 4.jpeg" width="300" height="375" />
+                              <img src="../images/fchamgang 1.jpeg" width="300" height="375" />
 
                         </td>
                    </tr>

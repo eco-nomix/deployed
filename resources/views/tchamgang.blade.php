a43a91dcddb1e7d5bd93e4a1143ea91742847495
Jay Potter <Jay.Potter@Materialink.com>
2018-10-31 10:01:12 -0700

    test4

--- resources/views/tchamgang.blade.php
+++ resources/views/tchamgang.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('content')
-<div style="position:absolute; top:52px; z-index:-1; width:100%">
+<div style="position:absolute; top:52px; z-index:-1; width:100%; height:100%">
 
 
         <table width="100%" height="100%" border="1">

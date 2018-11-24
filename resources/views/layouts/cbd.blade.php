047bf72adf09f1d8c655edfd8e9409e7addcbd46
Jay Potter <Jay.Potter@Materialink.com>
2018-11-24 13:40:20 -0800

    test2

--- resources/views/layouts/cbd.blade.php
+++ resources/views/layouts/cbd.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>{{ $title }}</title>
      <meta name="description" content="@if($description){{$description}}@endif">

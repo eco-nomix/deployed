b63f2252055a63b3cc9d9dde82a0948bf491177d
Jay Potter <Jay.Potter@Materialink.com>
2018-11-24 15:42:17 -0800

    test2

--- resources/views/layouts/cbd.blade.php
+++ resources/views/layouts/cbd.blade.php
@@ -2,7 +2,9 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta content="True" name="HandheldFriendly">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+   <meta name="viewport" content="width=device-width">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>{{ $title }}</title>
      <meta name="description" content="@if($description){{$description}}@endif">

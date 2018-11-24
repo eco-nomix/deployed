da4bf6ae8e9a7524ce5f41f1e7826f4156cb9a19
Jay Potter <Jay.Potter@Materialink.com>
2018-11-24 12:27:25 -0800

    cbd4

--- resources/views/layouts/navigation_cbd.blade.php
+++ resources/views/layouts/navigation_cbd.blade.php
@@ -1,5 +1,5 @@
 
-    <div class="navcontainer" >
+    <div class="navcontainer4" >
         {{--<div class="navbar-brand brand-image">--}}
              {{--<a href="{{URL::to('/')}}"><sp class="tibSm">TCHAMGANG</sp></a>--}}
         {{--</div>--}}

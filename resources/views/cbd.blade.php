bdd00dd8e4f56b29ea1e8f0a5a26fc745f16f81f
Jay Potter <Jay.Potter@Materialink.com>
2018-11-24 18:15:14 -0800

    test2

--- resources/views/cbd.blade.php
+++ resources/views/cbd.blade.php
@@ -7,45 +7,50 @@
 
 </div>
 
-<div style="z-index:-1; width:100%;position:absolute;top:92px">
+<div style="z-index:-1; width:100%;position:absolute;top:92px;background: #ccc; padding: 10px; text-align: justify">
+     {{--<img id="img1" src="../images/cbd 1.jpeg" width="280" height="375"  />--}}
+     {{--<div style="float: right; margin-top: 10px; background-color: #ff0000; width: 30%;  margin: 10px;">my div 2</div>--}}
 
-
-        <table width="560" height="750" border="1">
-            <tr>
-
-
-                <td width="280px" height="375" style="vertical-align: top;">
-                <img id="img1" src="../images/cbd 1.jpeg" width="280" height="375"  />
-                </td>
-
-
-                 <td width="280px" height="375" style="vertical-align: top;">
-                  <img id="img2" src="../images/cbd 2.jpeg" width="280" height="375" />
-
-                 </td>
-
-            </tr>
-
-             <tr>
-
-
-                <td width="280px" height="375" style="vertical-align: top;">
-                <img id="img3" src="../images/cbd 3.jpeg" width="280" height="375"  />
-                </td>
-
-
-                 <td width="280px" height="375" style="vertical-align: top;">
-                  <img id="img4" src="../images/cbd 4.jpeg" width="280" height="375" />
-
-                 </td>
-
-
-            </tr>
-        </table>
-
-
-
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.
+    </p>
 </div>
+        {{--<table width="560" height="750" border="1">--}}
+            {{--<tr>--}}
+
+
+                {{--<td width="280px" height="375" style="vertical-align: top;">--}}
+                {{--<img id="img1" src="../images/cbd 1.jpeg" width="280" height="375"  />--}}
+                {{--</td>--}}
+
+
+                 {{--<td width="280px" height="375" style="vertical-align: top;">--}}
+                  {{--<img id="img2" src="../images/cbd 2.jpeg" width="280" height="375" />--}}
+
+                 {{--</td>--}}
+
+            {{--</tr>--}}
+
+             {{--<tr>--}}
+
+
+                {{--<td width="280px" height="375" style="vertical-align: top;">--}}
+                {{--<img id="img3" src="../images/cbd 3.jpeg" width="280" height="375"  />--}}
+                {{--</td>--}}
+
+
+                 {{--<td width="280px" height="375" style="vertical-align: top;">--}}
+                  {{--<img id="img4" src="../images/cbd 4.jpeg" width="280" height="375" />--}}
+
+                 {{--</td>--}}
+
+
+            {{--</tr>--}}
+        {{--</table>--}}
+
+
+
+
 
 
 @endsection

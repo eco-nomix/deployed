9ca7eea6e100244e48054c0343c5c6da270f944f
Jay Potter <Jay.Potter@Materialink.com>
2018-11-24 19:39:15 -0800

    test2

--- resources/views/cbd.blade.php
+++ resources/views/cbd.blade.php
@@ -2,6 +2,21 @@
 
 
 @section('content')
+<style>
+.h1{
+    font-size:5.7vw;
+}
+.h2{
+    font-size:4.0vw;
+}
+.h3{
+    font-size:3.2vw;
+}
+.p{
+    font-size:2.8vw;
+}
+
+</style>
 <div style="position:absolute; text-align:center; top:52px; z-index:-1; width:100%">
     <h1>CBD Care Group</h1>
 
@@ -21,7 +36,7 @@
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.
      </p>
 </div>
-<div>  lkj </div>
+
 {{--<div style="z-index:2; width:100%;background: #ccc; padding: 10px; text-align: justify; font-size:14px;">--}}
     {{--<div  style="float: left; margin-top: 10px; background-color: #ff0000; width: 20%;  margin: 10px;">--}}
      {{--<img id="img2" src="../images/fchamgang 2.jpeg" style="width:100%"  />--}}

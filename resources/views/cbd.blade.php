425e7227fac135c3b177f93c3334f096a3c74596
Jay Potter <Jay.Potter@Materialink.com>
2018-11-24 20:08:41 -0800

    test2

--- resources/views/cbd.blade.php
+++ resources/views/cbd.blade.php
@@ -19,15 +19,23 @@ p{
     font-size:3.5vw;
 }
 
+div{
+    width:100%;
+    background: #ccc;
+    padding: 10px;
+    text-align: justify;
+
+}
+
 </style>
-<div style="position:absolute; text-align:center; top:52px; z-index:-1; width:100%">
+<div style="text-align:center; top:52px; border:1px solid red;">
     <h1>CBD Care Group</h1>
 
 </div>
 
-<div style="z-index:-1; width:100%;position:absolute;top:100+10vw;background: #ccc; padding: 10px; text-align: justify; font-size:6cw;">
-    <div  style="float: right; margin-top: 10px; background-color: #ff0000; width: 20%;  margin: 10px;">
-     <img id="img1" src="../images/fchamgang 1.jpeg" style="width:100%"  />
+<div style="position:absolute;top:calc(100px+10vw); border:1px solid blue;">
+    <div  style="float: right; width: 20%;  ">
+        <img id="img1" src="../images/fchamgang 1.jpeg" style="width:100%"  />
      </div>
      <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.

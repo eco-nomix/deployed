db645f2f39d7b110963af208e2c90621a4003d5f
Jay Potter <Jay.Potter@Materialink.com>
2018-11-24 22:12:10 -0800

    test2

--- resources/views/cbd.blade.php
+++ resources/views/cbd.blade.php
@@ -45,6 +45,16 @@ p{
 
 
 </div>
+<div id="div2" >
+    <div  style="float: right; width: 25%;  ">
+        <img id="img2" src="../images/fchamgang 2.jpeg" style="width:100%"  />
+     </div>
+     <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.
+     </p>
+
+
+</div>
 
 {{--<div style="z-index:2; width:100%;background: #ccc; padding: 10px; text-align: justify; font-size:14px;">--}}
     {{--<div  style="float: left; margin-top: 10px; background-color: #ff0000; width: 20%;  margin: 10px;">--}}

c5a44bd6002c9f17aefa2a45582fafa9b88b83dd
Jay Potter <Jay.Potter@Materialink.com>
2018-11-24 18:32:35 -0800

    test2

--- resources/views/cbd.blade.php
+++ resources/views/cbd.blade.php
@@ -19,6 +19,13 @@
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.
     </p>
+     <img id="img3" src="../images/fchamgang 3.jpeg"  style="float: right; margin-top: 10px; background-color: #ff0000; width: 30%;  margin: 10px;" />
+
+     <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.
+     </p>
+       <img id="img4" src="../images/fchamgang 4.jpeg"  style="float: left; margin-top: 10px; background-color: #ff0000; width: 30%;  margin: 10px;" />
+
 </div>
         {{--<table width="560" height="750" border="1">--}}
             {{--<tr>--}}

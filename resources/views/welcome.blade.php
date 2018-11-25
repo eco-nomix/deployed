62a75ab4e88e265a9568fb302ce071279934afce
Jay Potter <Jay.Potter@Materialink.com>
2018-11-24 23:48:09 -0800

    test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,7 +70,7 @@ p{
     </div>
 
 
-    <div class="trans display  back-white col-sm-10 col-sm-offset-1" >
+    <div class="trans display  back-white col-xs-10 col-xs-offset-1 col-md-10 col-md-offset-1" >
             <p>Kinetic Gold is a new banking platform and cryptocurrency that is in its Pre-Launch phase.  </p>
             <p>Unlike other cryptocurrenciences, Kinetic Gold has been designed to be utilized like a standard bank account
                on a regular basis for normal banking activities.

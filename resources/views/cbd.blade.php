41453e77e94c3cfbcef637ce2c5c40a254837513
Jay Potter <Jay.Potter@Materialink.com>
2018-11-24 20:28:04 -0800

    test2

--- resources/views/cbd.blade.php
+++ resources/views/cbd.blade.php
@@ -20,7 +20,7 @@ p{
     max-font-size:16px;
 }
 #div0{
-  text-align:center; position:absolute;top:55px; border:1px solid red;
+  width:100%;text-align:center; position:absolute;top:55px; border:1px solid red;
 }
 #div1{
   text-align:center; position:absolute; margin:10px; border:1px solid blue;

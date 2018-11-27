f56b9c05519ec8f1cb0101160f6cb204b7a10cd4
Jay Potter <Jay.Potter@Materialink.com>
2018-11-27 09:50:14 -0800

    test2

--- resources/views/cbd.blade.php
+++ resources/views/cbd.blade.php
@@ -8,7 +8,7 @@
 
     <div id="div0" >
       <div  style="float: right; width: 100%; margin:10px; ">
-        <img id="img1" src="../images/freedom.jpeg" style="width:100%"  />
+        <img id="img0" src="../images/freedom.jpeg" style="width:100%"  />
       </div>
          <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.

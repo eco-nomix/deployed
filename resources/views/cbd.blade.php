13620ba8810aa542e637e071ba4206029dd1c408
Jay Potter <Jay.Potter@Materialink.com>
2018-11-27 09:36:08 -0800

    test2

--- resources/views/cbd.blade.php
+++ resources/views/cbd.blade.php
@@ -4,9 +4,9 @@
 @section('content')
 
 <div id="main">
-    <div id="div0" >
+    <div  >
 
-    <img src="../images/freedom.jpg" alt="" title="freedom" width="1200" height="400" data-bgposition="left center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
+    <img src="../images/freedom.jpg" alt="" title="freedom" width="1200" height="400" >
 
      <div STYLE="position:absolute; top:250px; left:20px; width:200px; height:25px">
     <h1">CBD Care Group</h1>
@@ -14,46 +14,46 @@
 
     </div>
 
-    <div id="div1" >
-      <div  style="float: right; width: 25%; margin:10px; ">
-        <img id="img1" src="../images/fchamgang 1.jpeg" style="width:100%"  />
-         </div>
-         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.
-        </p>
+    {{--<div id="div1" >--}}
+      {{--<div  style="float: right; width: 25%; margin:10px; ">--}}
+        {{--<img id="img1" src="../images/fchamgang 1.jpeg" style="width:100%"  />--}}
+         {{--</div>--}}
+         {{--<p>--}}
+            {{--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.--}}
+        {{--</p>--}}
 
 
-    </div>
-    <div id="div2" >
-        <div  style="float: left; width: 25%; margin:10px; ">
-        <img id="img2" src="../images/fchamgang 2.jpeg" style="width:100%"  />
-         </div>
-         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.
-         </p>
+    {{--</div>--}}
+    {{--<div id="div2" >--}}
+        {{--<div  style="float: left; width: 25%; margin:10px; ">--}}
+        {{--<img id="img2" src="../images/fchamgang 2.jpeg" style="width:100%"  />--}}
+         {{--</div>--}}
+         {{--<p>--}}
+            {{--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.--}}
+         {{--</p>--}}
 
 
-    </div>
-    <div id="div3" >
-       <div  style="float: right; width: 25%; margin:10px; ">
-        <img id="img3" src="../images/fchamgang 3.jpeg" style="width:100%"  />
-      </div>
-      <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.
-      </p>
+    {{--</div>--}}
+    {{--<div id="div3" >--}}
+       {{--<div  style="float: right; width: 25%; margin:10px; ">--}}
+        {{--<img id="img3" src="../images/fchamgang 3.jpeg" style="width:100%"  />--}}
+      {{--</div>--}}
+      {{--<p>--}}
+            {{--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.--}}
+      {{--</p>--}}
 
 
-    </div>
-    <div id="div4" >
-      <div  style="float: left; width: 25%; margin:10px; ">
-        <img id="img4" src="../images/fchamgang 4.jpeg" style="width:100%"  />
-       </div>
-      <p>
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.
-         </p>
+    {{--</div>--}}
+    {{--<div id="div4" >--}}
+      {{--<div  style="float: left; width: 25%; margin:10px; ">--}}
+        {{--<img id="img4" src="../images/fchamgang 4.jpeg" style="width:100%"  />--}}
+       {{--</div>--}}
+      {{--<p>--}}
+             {{--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.--}}
+         {{--</p>--}}
 
 
-    </div>
+    {{--</div>--}}
 </div>
 
 @endsection

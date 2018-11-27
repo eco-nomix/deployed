3caa64bfb89182e33cfadfc4ec34d84c4f68e023
Jay Potter <Jay.Potter@Materialink.com>
2018-11-27 11:55:16 -0800

    test2

--- public/js/cbdImages.js
+++ public/js/cbdImages.js
@@ -17,14 +17,22 @@ $(document).ready(function() {
 
 
     function changeImage() {
+        text1[1] = "Latest Technology";
+        text1[2] = "Reduce Pain";
+        text1[3] = "Natural Ingredients";
+        text1[4] = "Reduce Stress";
+
         ctr = ctr+1;
         if (ctr>4) {
             ctr=1;
         }
+        document.getElementById("text1").innerHTML = text1[ctr];
         pagevar = pagevar+1;
         if (pagevar>36) {
             pagevar=1;
         }
+        id = 'text'+ctr;
+
         //var imageno = Math.floor(Math.random() * (4))+1;
 
         imagesrc = 'img' + ctr;

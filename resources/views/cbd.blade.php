8a2afe1c405c60033eb20fe8f111e155c486ad7b
Jay Potter <Jay.Potter@Materialink.com>
2018-11-30 13:36:41 -0800

    test3

--- resources/views/cbd.blade.php
+++ resources/views/cbd.blade.php
@@ -23,7 +23,7 @@
 
     </div>
 
-    <div id="div1" style="width:100vw" >
+    <div id="div1" style="width:95vw" >
       {{--<div  style="float: right; width: 25%; margin:10px; ">--}}
         {{--<img id="img1" src="../images/fchamgang 1.jpeg" style="width:100%"  />--}}
          {{--</div>--}}

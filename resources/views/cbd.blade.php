26e197e1a398c7c2796b7df419feaeb8c0c16bb6
Jay Potter <Jay.Potter@Materialink.com>
2018-11-30 12:46:24 -0800

    new reset

--- resources/views/cbd.blade.php
+++ resources/views/cbd.blade.php
@@ -28,7 +28,7 @@
         {{--<img id="img1" src="../images/fchamgang 1.jpeg" style="width:100%"  />--}}
          {{--</div>--}}
          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna tortor, tincidunt ac tristique sit amet, condimentum ut erat. Mauris sem mi, ultrices et hendrerit vitae, hendrerit non tortor. Aliquam erat volutpat. Morbi eget leo lorem, ut placerat nisl. Aenean auctor volutpat condimentum. Morbi adipiscing leo et felis faucibus suscipit nec at odio. Pellentesque convallis turpis non sapien facilisis quis volutpat magna venenatis. Etiam nisi metus, imperdiet vitae lobortis sit amet, pharetra ut leo.
+            test
         </p>
 
 

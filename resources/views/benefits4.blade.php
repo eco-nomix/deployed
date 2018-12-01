a35eafdce8bf163077404871fb68dd8f69c53635
Jay Potter <Jay.Potter@Materialink.com>
2018-11-30 21:04:43 -0800

    test3

--- resources/views/benefits4.blade.php
+++ resources/views/benefits4.blade.php
@@ -21,53 +21,53 @@
     </div>
 
     <div id="div1" style="width:98vw" >
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-10 col-md-offset-1">
              {{--<div style="width:100%;">--}}
                  <div class="cbd600">
                       Benefits
                  </div>
+        </div>
              {{--</div>--}}
-             <div class="panel panel-default cbddisplay">
-                <div class="panel-body">
-                   <div class="form-group col-md-12 ">
-                         TIB Foundation is an association that is focused on making our members
-                         aware of the processes associated with reducing or even eliminating
-                         home mortage costs.  It will do this in the form
-                         of a monthly newletter that is sent to all of it's members detailing
-                         information about various opportunities, programs, their risks and potential rewards along with a
-                         a detailed 'how-to' method to get involved if you are interested.
-                    </div>
-                    <div class="form-group col-md-12 ">
-                         There are no requirements for members of the TIB Foundation to participate
-                         within any opportunity or program.  This information is provided as
-                         'information' only and is not
-                         to be construed as investment advise.  All member's need to do their own
-                         due diligence to determine if any particular opportunity fit's with your
-                         investment goals and/or
-                         styles, limits and desires.
-                    </div>
-                    <div class="form-group col-md-12 ">
-                        <strong>TIB Foundation Benefits are detailed as follows:</strong>
-                        <ul>
-                          <li>Members will receive the monthly TIB Foundation newsletter.</li>
-                          <li>Educational Videos concerning the TIB Foundation goals of helping our
-                          members become financially more stable will be produced each month.  All
-                          of the Videos produced
-                             over the life of the program will be available on demand to our members.</li>
-                          <li>Some of the programs offered to our Members will be exclusive to our
-                          members, but that will not always be the case.</li>
-                          <li>TIB Foundation will track 'sponsorship' of its members that can be
-                          utilized by the various programs offered to it's members</li>
-                          <li>Any financial benefits from participation within a program referred
-                          by TIB Foundation will be paid directly to the participants by the
-                          sponsoring program.
-                               TIB Foundation doesn't pay any benefits to its members.</li>
-                          <li>TIB Foundation will only be offering information and training to
-                          its members.</li>
-                          <li>Members may elect to contract for services with Attorneys to represent
-                          them in court.  TIB Foundation is not a law firm, but can recommend firms that utilize the principles that are taught by the TIB Foundation</li>
-                        </ul>
-                    </div>
+         <div class="panel panel-default cbddisplay">
+            <div class="panel-body">
+               <div class="form-group col-md-12 ">
+                     TIB Foundation is an association that is focused on making our members
+                     aware of the processes associated with reducing or even eliminating
+                     home mortage costs.  It will do this in the form
+                     of a monthly newletter that is sent to all of it's members detailing
+                     information about various opportunities, programs, their risks and potential rewards along with a
+                     a detailed 'how-to' method to get involved if you are interested.
+                </div>
+                <div class="form-group col-md-12 ">
+                     There are no requirements for members of the TIB Foundation to participate
+                     within any opportunity or program.  This information is provided as
+                     'information' only and is not
+                     to be construed as investment advise.  All member's need to do their own
+                     due diligence to determine if any particular opportunity fit's with your
+                     investment goals and/or
+                     styles, limits and desires.
+                </div>
+                <div class="form-group col-md-12 ">
+                    <strong>TIB Foundation Benefits are detailed as follows:</strong>
+                    <ul>
+                      <li>Members will receive the monthly TIB Foundation newsletter.</li>
+                      <li>Educational Videos concerning the TIB Foundation goals of helping our
+                      members become financially more stable will be produced each month.  All
+                      of the Videos produced
+                         over the life of the program will be available on demand to our members.</li>
+                      <li>Some of the programs offered to our Members will be exclusive to our
+                      members, but that will not always be the case.</li>
+                      <li>TIB Foundation will track 'sponsorship' of its members that can be
+                      utilized by the various programs offered to it's members</li>
+                      <li>Any financial benefits from participation within a program referred
+                      by TIB Foundation will be paid directly to the participants by the
+                      sponsoring program.
+                           TIB Foundation doesn't pay any benefits to its members.</li>
+                      <li>TIB Foundation will only be offering information and training to
+                      its members.</li>
+                      <li>Members may elect to contract for services with Attorneys to represent
+                      them in court.  TIB Foundation is not a law firm, but can recommend firms that utilize the principles that are taught by the TIB Foundation</li>
+                    </ul>
                 </div>
             </div>
         </div>

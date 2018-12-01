3950aecc2f9630ffbad7ec14d25cc5ea360670d3
Jay Potter <Jay.Potter@Materialink.com>
2018-11-30 16:13:28 -0800

    test3

--- resources/views/cbd.blade.php
+++ resources/views/cbd.blade.php
@@ -23,7 +23,7 @@
 
     </div>
 
-    <div id="div1" style="width:95vw" >
+    <div id="div1" style="width:92vw" >
       {{--<div  style="float: right; width: 25%; margin:10px; ">--}}
         {{--<img id="img1" src="../images/fchamgang 1.jpeg" style="width:100%"  />--}}
          {{--</div>--}}
@@ -50,89 +50,92 @@
         is created equal, meaning it is critical to find the right CBD product for you particular situation.
         </p>
         <div>
-        <img style="float:left; padding:0px 3px 3px 0px; width:30vw; height:20vw" src="/images/field.jpeg"
-        alt="cbd hemp growing in a field" />
-        <p><strong>Where Was the Hemp Grown
-        and What Could Be In It?</strong></p>
-        <p>Hemp is capable of absorbing both the good and the bad
-        from the air, water, and soil in which it’s grown. This makes it all the more important to
-        know that your CBD comes from organically grown hemp that can be tracked to its
-        US-grown source. In addition through testing should be preformed on all products you could ingest.
-        You don't need your CBD to have accumulated
-        toxic substances such as pesticides, herbicides, or heavy metals. These substances have been linked to various
-         cancers such as Leukemia, Myeloma, lymphoma and cancers of the skin, lip, stomach, brain and prostate.</p>
+            <img style="float:left; padding:0px 3px 3px 0px; width:30vw; height:20vw" src="/images/field.jpeg"
+            alt="cbd hemp growing in a field" />
+            <p><strong>Where Was the Hemp Grown
+            and What Could Be In It?</strong></p>
+            <p>Hemp is capable of absorbing both the good and the bad
+            from the air, water, and soil in which it’s grown. This makes it all the more important to
+            know that your CBD comes from organically grown hemp that can be tracked to its
+            US-grown source. In addition through testing should be preformed on all products you could ingest.
+            You don't need your CBD to have accumulated
+            toxic substances such as pesticides, herbicides, or heavy metals. These substances have been linked to various
+             cancers such as Leukemia, Myeloma, lymphoma and cancers of the skin, lip, stomach,
+             brain and prostate.</p>
         </div>
-        <div><p><strong>How much THC is in the CBD product?</strong></p>
-        <p>In order to qualify as a legal hemp product, CBD products must contain less than 0.03% THC.  Make sure your products have low levels, or zero, THC in them. </p>
-        <p>Remember even small amounts of THC can cause you to fail a drug test or have mind-altering affects.</p>
-        </div>
-
-        <div><p><strong>Is the CBD Potent and Pure?</strong></p>
-        <ul>The product should be free of
-        <li>Pesticides</li>
-        <li>Bacteria and Fungus</li>
-        <li>Residual Solvents</li>
-        <li>Heavy metals</li></ul>
+        <div>
+            <p><strong>How much THC is in the CBD product?</strong></p>
+            <p>In order to qualify as a legal hemp product, CBD products must contain less than 0.03% THC.  Make sure your products have low levels, or zero, THC in them. </p>
+            <p>Remember even small amounts of THC can cause you to fail a drug test or have mind-altering affects.</p>
         </div>
 
         <div>
-        <p><strong>How is the CBD Being Extracted?</strong></p>
-        <img style="float:left; padding:0px 3px 3px 0px;  width:30vw; height:20vw" src="./images/apex.png"
-                  alt="apex co2 extraction" />
-        <p>Getting CBD from hemp is a complicated process. The cheapest and easiest ways
-        to extract CBD from hemp commonly involve harsh solvents that can leave chemical
-        residue in the CBD. The best, and most reliable extraction method, uses carbon dioxide
-        (CO<sub>2</sub>) under high pressure and extremely low temperatures to pull out as
-        much CBD as possible without introducing contaminants. The CO<sub>2</sub> simply evaporates
-        once the pressure is released, leaving virtually no trace of
-        extraction on the CBD.</p>
+            <p><strong>Is the CBD Potent and Pure?</strong></p>
+            <ul>The product should be free of
+            <li>Pesticides</li>
+            <li>Bacteria and Fungus</li>
+            <li>Residual Solvents</li>
+            <li>Heavy metals</li></ul>
+        </div>
+
+        <div>
+            <p><strong>How is the CBD Being Extracted?</strong></p>
+            <img style="float:left; padding:0px 3px 3px 0px;  width:30vw; height:20vw" src="./images/apex.png"
+                      alt="apex co2 extraction" />
+            <p>Getting CBD from hemp is a complicated process. The cheapest and easiest ways
+            to extract CBD from hemp commonly involve harsh solvents that can leave chemical
+            residue in the CBD. The best, and most reliable extraction method, uses carbon dioxide
+            (CO<sub>2</sub>) under high pressure and extremely low temperatures to pull out as
+            much CBD as possible without introducing contaminants. The CO<sub>2</sub> simply evaporates
+            once the pressure is released, leaving virtually no trace of
+            extraction on the CBD.</p>
         </div>
         <div>
-        <p><strong>Benefits of CO<sub>2</sub> Extraction:</strong></p>
-        <ul>
-        <li>Ensures high quality</li>
-        <li>Uses no harsh solvents/chemicals</li>
-        <li>Free of butane &amp; propane, and ethanol</li>
-        <li>It’s a safe and proven solvent, widely used for foods and dietary supplements</li>
-        </ul>
+            <p><strong>Benefits of CO<sub>2</sub> Extraction:</strong></p>
+            <ul>
+            <li>Ensures high quality</li>
+            <li>Uses no harsh solvents/chemicals</li>
+            <li>Free of butane &amp; propane, and ethanol</li>
+            <li>It’s a safe and proven solvent, widely used for foods and dietary supplements</li>
+            </ul>
         </div>
         <div>
-        <p><strong> What Claims are Being Made?</strong></p>
-        <p>It is a violation of the FDA DSHEA guidelines
-        to make medical claims about the efficacy of CBD products in the treatment of
-        any medical condition or symptom. Although preliminary research has shown
-        tremendous promise of CBD helping people in pretty remarkable ways,
-        legitimate CBD companies will refrain from making any direct medical claims.
-        </p>
+            <p><strong> What Claims are Being Made?</strong></p>
+            <p>It is a violation of the FDA DSHEA guidelines
+            to make medical claims about the efficacy of CBD products in the treatment of
+            any medical condition or symptom. Although preliminary research has shown
+            tremendous promise of CBD helping people in pretty remarkable ways,
+            legitimate CBD companies will refrain from making any direct medical claims.
+            </p>
         </div>
         <div>
-        <img style="float:left; padding:0px 3px 3px 0px;  width:30vw; height:20vw" src="./images/discount.jpeg"
-          alt="is cheaper cbd oil ok to buy" />
-         <p><strong>Is Cheaper Better?</strong></p>
-        <p>When it comes to CBD, cheaper is most certainly not always better because the
-        production of quality CBD just isn’t cheap. CO<sub>2</sub> extraction utilizes
-        complex equipment and a high level of expertise as opposed to the cheaper and easier
-        chemical extraction processes that can leave residue from toxic solvents like butane,
-        propane, and ethanol in the CBD. While the CO<sub>2</sub> extraction might be more expensive
-        , it does insure quality, purity, and potency – especially when
-        used to extract CBD from hemp that has been organically grown in the United States.</p>
-        <p><strong>Reasons Higher-Quality CBD May Cost More</strong></p>
-        <ul>
-        <li>Organically grown</li>
-        <li>Extracted using CO<sub>2</sub> method</li>
-        <li>Higher concentrations of CBD</li>
-        <li>Grown in the United States</li>
-        <li>Tested in third-party labs</li>
-        <li>Made from high-quality, full-spectrum extract, so other beneficial compounds are present</li>
-        </ul>
+            <img style="float:left; padding:0px 3px 3px 0px;  width:30vw; height:20vw" src="./images/discount.jpeg"
+              alt="is cheaper cbd oil ok to buy" />
+             <p><strong>Is Cheaper Better?</strong></p>
+            <p>When it comes to CBD, cheaper is most certainly not always better because the
+            production of quality CBD just isn’t cheap. CO<sub>2</sub> extraction utilizes
+            complex equipment and a high level of expertise as opposed to the cheaper and easier
+            chemical extraction processes that can leave residue from toxic solvents like butane,
+            propane, and ethanol in the CBD. While the CO<sub>2</sub> extraction might be more expensive
+            , it does insure quality, purity, and potency – especially when
+            used to extract CBD from hemp that has been organically grown in the United States.</p>
+            <p><strong>Reasons Higher-Quality CBD May Cost More</strong></p>
+            <ul>
+            <li>Organically grown</li>
+            <li>Extracted using CO<sub>2</sub> method</li>
+            <li>Higher concentrations of CBD</li>
+            <li>Grown in the United States</li>
+            <li>Tested in third-party labs</li>
+            <li>Made from high-quality, full-spectrum extract, so other beneficial compounds are present</li>
+            </ul>
         </div>
         <div>
-        <p><strong>Additional Things to Look For</strong></p>
-        <p>The CBD product can be enhanced in a number of ways.</p>
-        <p>* There could be additional herbal supplements that can both amplify the effects of the CBD alone and the CBD can impact the delivery of the herbal suppliment.</p>
-        <p>* The CBD product can be <strong>micro-encapsulated</strong>.  This impacts the absorption of the CBD.  If the product is treated this way, you can find that a smaller dosage can be more effective</p>
-        <p>* The CBD Product can be <strong>polarized</strong>.  This also assists in the delivery of the CBD to critical areas of the body.
-        </p>
+            <p><strong>Additional Things to Look For</strong></p>
+            <p>The CBD product can be enhanced in a number of ways.</p>
+            <p>* There could be additional herbal supplements that can both amplify the effects of the CBD alone and the CBD can impact the delivery of the herbal suppliment.</p>
+            <p>* The CBD product can be <strong>micro-encapsulated</strong>.  This impacts the absorption of the CBD.  If the product is treated this way, you can find that a smaller dosage can be more effective</p>
+            <p>* The CBD Product can be <strong>polarized</strong>.  This also assists in the delivery of the CBD to critical areas of the body.
+            </p>
         </div>
 
     </div>

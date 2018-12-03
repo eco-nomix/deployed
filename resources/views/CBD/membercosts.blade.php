2586dba6aa239754c67662ec008f2539ee7ad39a
Jay Potter <Jay.Potter@Materialink.com>
2018-12-03 11:06:37 -0800

    test3

--- resources/views/CBD/membercosts.blade.php
+++ resources/views/CBD/membercosts.blade.php
@@ -27,35 +27,35 @@
                       Member Costs
                  </div>
         </div>
-             {{--</div>--}}
-         <div class="panel panel-default cbddisplay">
-            <div class="panel-body">
-               <div class="form-group col-md-12 ">
-                     There are no initial, startup, annual, or monthly costs for CBD Card Group,
-                      or required products to purchase</div>
-                </div>
-                <div class="form-group col-md-12 ">
-                       <strong>The only Requirement is that you are required to register</strong>
-                </div>
-                <div class="form-group col-md-12 ">
-                    Registration is a simple process just
-                        <ul>
-                            <li>Provide your basic information (name, email, desired username and password, verify your email,update and contact information)</li>
-                            <li>Submit your registration Fee</li>
-                        </ul>
-                </div>
-                  <div class="form-group col-md-12 ">
-                      Once you are registered
-                      <ul>
-                        <li>You may purchase products at the members discounted price.</li>
-                        <li>You may participate in CBD Care Group's Profit Sharing Program.</li>
-                        <li>To receive payments from the CBD Care Group's Profit Sharing Program you may be required to provide additional information.</li>
-                      </ul>
-                  </div>
-
+    </div>
+     <div class="panel panel-default cbddisplay">
+        <div class="panel-body">
+           <div class="form-group col-md-12 ">
+                 There are no initial, startup, annual, or monthly costs for CBD Card Group,
+                  or required products to purchase</div>
             </div>
+            <div class="form-group col-md-12 ">
+                   <strong>The only Requirement is that you are required to register</strong>
+            </div>
+            <div class="form-group col-md-12 ">
+                Registration is a simple process just
+                    <ul>
+                        <li>Provide your basic information (name, email, desired username and password, verify your email,update and contact information)</li>
+                        <li>Submit your registration Fee</li>
+                    </ul>
+            </div>
+              <div class="form-group col-md-12 ">
+                  Once you are registered
+                  <ul>
+                    <li>You may purchase products at the members discounted price.</li>
+                    <li>You may participate in CBD Care Group's Profit Sharing Program.</li>
+                    <li>To receive payments from the CBD Care Group's Profit Sharing Program you may be required to provide additional information.</li>
+                  </ul>
+              </div>
+
         </div>
     </div>
+  
 </div>
 
 @stop

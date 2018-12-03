49a1fd4b86b9905d678745326b3e70ced84a0b3d
Jay Potter <Jay.Potter@Materialink.com>
2018-12-03 11:22:45 -0800

    test3

--- resources/views/CBD/membercosts.blade.php
+++ resources/views/CBD/membercosts.blade.php
@@ -20,9 +20,8 @@
         </div>
     </div>
 
-    <div id="div1" style="width:98vw" >
+    <div id="div1" style="width:98vw">
         <div class="col-md-10 col-md-offset-1">
-
              <div class="cbd600">
                   Member Costs
              </div>
@@ -30,9 +29,9 @@
     </div>
      <div class="panel panel-default cbddisplay">
         <div class="panel-body">
-           <div class="form-group col-md-12 ">
+            <div class="form-group col-md-12 ">
                  There are no initial, startup, annual, or monthly costs for CBD Card Group,
-                  or required products to purchase</div>
+                  or required products to purchase
             </div>
             <div class="form-group col-md-12 ">
                    <strong>The only Requirement is that you are required to register</strong>
@@ -44,14 +43,14 @@
                         <li>Submit your registration Fee</li>
                     </ul>
             </div>
-              <div class="form-group col-md-12 ">
-                  Once you are registered
-                  <ul>
-                    <li>You may purchase products at the members discounted price.</li>
-                    <li>You may participate in CBD Care Group's Profit Sharing Program.</li>
-                    <li>To receive payments from the CBD Care Group's Profit Sharing Program you may be required to provide additional information.</li>
-                  </ul>
-              </div>
+            <div class="form-group col-md-12 ">
+              Once you are registered
+              <ul>
+                <li>You may purchase products at the members discounted price.</li>
+                <li>You may participate in CBD Care Group's Profit Sharing Program.</li>
+                <li>To receive payments from the CBD Care Group's Profit Sharing Program you may be required to provide additional information.</li>
+              </ul>
+            </div>
 
         </div>
     </div>

41c2fe45d9c9ed02da52a6afc9e94f890c31b8dd
Jay Potter <Jay.Potter@Materialink.com>
2018-12-03 17:11:09 -0800

    test3

--- resources/views/CBD/memberterms4.blade.php
+++ resources/views/CBD/memberterms4.blade.php
@@ -31,33 +31,31 @@
         <div class="panel-body">
             <div class="form-group col-md-12 ">
 
-
-
-            These Terms and Conditions exist for the benefit of KineticGold.org and its registered trademark KineticGold,
+            These Terms and Conditions exist for the benefit of cbdcaregroups.com and its registered trademark CBD Care Groupd,
             at its sole discretion, reserves the right to terminate the account of any registered user caught in violation
              of the Terms and Conditions without notice.
             <br><br>
-            KineticGold.org reserves the right to change these terms and conditions at any time, and you agree to abide
-            by the most recent version of this Terms and Conditions Agreement each time you view and use the website KineticGold, KineticGold's
+            Cbdcaregroups.com reserves the right to change these terms and conditions at any time, and you agree to abide
+            by the most recent version of this Terms and Conditions Agreement each time you view and use the website CBD Care Group, CBD Care Group's
              debit cards, and/or training material.  Do not use the business cards, debit cards and/or training material
              if you do not agree to all of these terms and conditions.
             <br><br>
-             KineticGold reserves the right to update, modify, add or change these terms and conditions without notice.
+            CBD Care Group reserves the right to update, modify, add or change these terms and conditions without notice.
                                     <br><br>
              ILLEGAL ACTIVITY<br>
-            The member agrees not to use KineticGold.org or any of it's linked websites for promotions of any page, newsgroup, email or any distribution
-            method that KineticGold, its Internet Service Providers, or otherwise is considered unlawful
+            The member agrees not to use cbdcaregroups.com or any of it's linked websites for promotions of any page, newsgroup, email or any distribution
+            method that CBD Care Group, its Internet Service Providers, or otherwise is considered unlawful
             according to any controlling legal authority.  This includes, but is not limited to, pornography, computer viruses,
             prostitution, obscenity or Spam.
             <br><br>
             THIRD-PARTY <br>
-            KineticGold will in no way be liable for the actions of third parties that may in any way cause members harm.
+            CBD Care Group will in no way be liable for the actions of third parties that may in any way cause members harm.
              <br><br>
             TERMINATION<br>
-            The registration and/or Membership of any reg KineticGold user can be terminated at anytime by KineticGold without giving any reason.  Registered Users can cancel their account anytime directly from their Personal Options pages on the website.
+            The registration and/or Membership of any reg CBD Care Group user can be terminated at anytime by CBD Care Group without giving any reason.  Registered Users can cancel their account anytime directly from their Personal Options pages on the website.
             <br><br>
             PAYMENT PROCESSOR<br>
-            KineticGold Rewards and Profit Sharing is paid through the KineticGold account and its related Debit card.
+            CBD Care Group Rewards and Profit Sharing is paid through the CBD Care Group account and its related Debit card.
 
             <br><br>
             ETHICS<br>
@@ -65,41 +63,41 @@
             Any accrued Profit Sharing or Rewards earned prior to termination will be available for the usage of the User.
             <br><br>
             CANCELLATION/TERMINATION OF MEMBERSHIP<br>
-            Any Registered User that is canceled/terminated by KineticGold will loose all benefits associated with KineticGold.  Any sponsored organization
-            be reassigned to KineticGold.org.  The member will be allow to close out and receive all funds associated with the account.
+            Any Registered User that is canceled/terminated by CBD Care Group will loose all benefits associated with CBD Care Group.  Any sponsored organization
+            be reassigned to cbdcaregroups.com.  The member will be allow to close out and receive all funds associated with the account.
             <br><br>
             RELATIONSHIP OF PARTIES<br>
             While the parties shall work hand-in-hand for the benefit of both, the parties acknowledge and agree that the Registered User shall,
-            from a legal perspective, act as an independent contractor and is not an employee of KineticGold.  Nothing in this Agreement or these
+            from a legal perspective, act as an independent contractor and is not an employee of CBD Care Group.  Nothing in this Agreement or these
             Terms and Conditions shall create a partnership, joint venture, agency, or franchise between the parties in the legal sense of these
-            terms.  The Registered Use has no authority to sign any document in behalf of KineticGold.
+            terms.  The Registered Use has no authority to sign any document in behalf of CBD Care Group.
             <br><br>
             DISPUTES<br>
             Agreements shall be interpreted under the the laws of the Nevis.  <br><br>
             COLATERIAL<br>
-            All KineticGold logos, and graphics contained within KineticGold.org are copyrighted.
+            All CBD Care Group logos, and graphics contained within cbdcaregroups.com are copyrighted.
              <br><br>
             USE<br>
-              Distribution or copyrighted KineticGold material is expressly prohibited.  KineticGold.org provides Registered Users with approved KineticGold literature
-              for marketing.  Any 3rd party or self-created KineticGold material must be first approved by KineticGold.org before it may be used.
+              Distribution or copyrighted CBD Care Group material is expressly prohibited.  cbdcaregroups.com provides Registered Users with approved KineticGold literature
+              for marketing.  Any 3rd party or self-created CBD Care Group material must be first approved by cbdcaregroups.com before it may be used.
             <br><br>
             DAMAGING INTENT<br>
-            Any Registered User of KineticGold who engages in chat, email, posting of any material about KineticGold, or that could be damaging to KineticGold or KineticGold.org,
-            may be santioned by KineticGold.  Depending on seriousness, KineticGold may deem it appropriate to take appropriate action.
+            Any Registered User of CBD Care Group who engages in chat, email, posting of any material about CBD Care Group, or that could be damaging to KineticGold or KineticGold.org,
+            may be santioned by CBD Care Group  Depending on seriousness, CBD Care Group may deem it appropriate to take appropriate action.
             <br><br>
              UCE/UBE or SPAM<br>
-              KineticGold.org strictly prohibits the use of SPAM.  This enforcement is at the sole discretion of KineticGold.org and for the
+              cbdcaregroups.com strictly prohibits the use of SPAM.  This enforcement is at the sole discretion of cbdcaregroups.com and for the
         benefit of all Registered Users.  Registered Users proven to be participating in such activities will have their
         account(s) terminated and they will forfeit any and all current or future Profit Sharing.
             <br><br>
             CONTACT INFORMATION<br>
         It is the responsibility of the Registered User to Keep their personal records up to date at the members area.
-        KineticGold.org will not be responsible for communication error due to incorrect or out of date contact information.
+        cbdcaregroups.com will not be responsible for communication error due to incorrect or out of date contact information.
         Continued failed attempts to make contact with you may result in the termination of your account.
             <br><br>
             TAX<br>
             You, the Registered User, are responsible for any and all taxes payable in your resident jurisdiction, for any income you
-            receive either from KineticGolds Rewards or Profit Sharing.
+            receive either from CBD Care Groups Rewards or Profit Sharing.
             <br><br>
             NO WARRANTY<br>
             All information, training, etc. is provided “As Is” and without any warranty, express, implied, or otherwise,
@@ -107,18 +105,18 @@
             <br><br>
             NO ASSIGNMENT<br>
             Registered Users may not assign or transfer any rights or obligations under this Agreement without the prior
-            written consent of KineticGold.
+            written consent of CBD Care Group.
             <br><br>
     SEVERABILITY
     <br>
-            Registered Users and KineticGold.org agree that if any portion of this agreement is found illegal or unenforceable,
+            Registered Users and cbdcaregroups.com agree that if any portion of this agreement is found illegal or unenforceable,
             that portion shall be severed and the remainder of the agreement shall be given full force and effect.
             <br><br>  LIMITATION OF LIABILITY; SOLE AND EXCLUSIVE: INDEMNIFICATION<br>
-            You agree that KineticGold.org is NOT responsible for the success or failure of your business or your business decisions
+            You agree that cbdcaregroups.com is NOT responsible for the success or failure of your business or your business decisions
             relating to any information presented by our company or our companies produces and/or services.
             <br><br>
   ACCURATENESS OF INFORMATION<br>
-            KineticGold.org makes no representations as to the accurateness or reliability of any advertising message or
+            Cbdcaregroups.com makes no representations as to the accurateness or reliability of any advertising message or
             marketing tool offered through any referral, outside company or on this site.  While we believe the people/companies
             we work with to be honest and ethical, we recommend you do your own due diligence relating to any specific claims
             or promised made in the marketing message, as it relates to your use and purchase.

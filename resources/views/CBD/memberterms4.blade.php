3b2fa2356f01ad066b3c3aacc43ed6755d95bba2
Jay Potter <Jay.Potter@Materialink.com>
2018-12-03 17:04:43 -0800

    test3

--- resources/views/CBD/memberterms4.blade.php
+++ resources/views/CBD/memberterms4.blade.php
@@ -31,67 +31,116 @@
         <div class="panel-body">
             <div class="form-group col-md-12 ">
 
-                 By registering with cbdcaregroups.com and using this website, each user agrees, understands, and warrants the following:
-                 <br><br>
-                 CBD Care Group is a for-profit website created for the purpose of registering memberships and selling products.
-                 Access to cbdcaregroups.com is generally open to the public, with the only limitation being to
-                 individualized pages that are accessible only through logging in.
-                 <br><br>
-                 By registering with cbdcaregroups.com, each user agrees to view text and/or material that is or may be false,
-                 defamatory, inaccurate, abusive, vulgar, hateful, harassing, obscene, profane, sexually oriented, threatening,
-                 invasive of a person’s privacy, or otherwise objectionable.  In fact, "objectionable” is the default state of
-                 all material on the internet, and each user of cbdcaregroups.com is expected to understand the same.  However, a
-                 user may not post any text or material that violates any United States or California law, or the policies of
-
-                 as explained herein.  Each user also explicitly agrees not to post to cbdcaregroups.com any copyrighted, trademarked,
-                 or otherwise protected material, unless the user owns the material, or the material is presented as “fair use” under
-                 United States and/or California law.
-                 <br><br>
-                 cbdcaregroups.com does not allow any type of solicitation, including but not limited to advertisements, junk mail,
-                 directed mail, “spam”, chain letters, or pyramid schemes, Ponzi schemes, or “multi-level marketing” beyond the official
-                 site functionality of cbdcaregroups.com.
-                 <br><br>
-                 Each user understands and agrees that it is impossible for anyone associated with cbdcaregroups.com to
-                 confirm the validity or provenance of any content posted by users of the site.  No person associated with
-                 cbdcaregroups.com monitors or is responsible for the posted content.   No person associated with CBD Care Groups
-
-                 warrants the accuracy, completeness, or usefulness of any information presented.  To the contrary, it is more
-                 likely that the information presented in each posting is inaccurate, incomplete, and/or useless.  The posted messages
-                 express only the views of the author, and not necessary the views of any administrator or owner of
-.
-                 Anyone who feels that any posted content is objectionable or inaccurate is encourage to notify an administrator,
-                 but not response or corrective action should ever be expected.  Nevertheless, the administration and owners of
-                 this website have the right to remove or modify any content posted to cbdcaregroups.com (including member profile
-                 information), without notice or explanation.
-                 <br><br>
-                 Each user is solely responsible for the content of the user’s posted text and materials.  Furthermore, by using
-                 cbdcaregroups.com, each user agrees to indemnify, defend, and hold harmless the owners and administrators of this
-                 website for any claim related to the user’s posted text or material.
-                 <br><br>
-                 The owners and administrators of cbdcaregroups.com adhere to two written rules with respect to posted text and
-                 material:  (1) no pornography: and (2) no overt racism.  These limited rules are intended to allow a very reasonable
-                 method that the user may promote their products through cbdcaregroups.com.  Any user that does not follow this basic
-                 guideline may have their product descriptions edited or the products removed completely.
-                 <br><br>
-                 CBD Care Group does not use any type of advertising or tracking software.
-                 <br><br>
-                 By joining and using cbdcaregroups.com, each user agrees, understands and warrants that, as with every use of
-                 the internet, the User’s Anonymity cannot be protected.  To the contrary, Each user’s personally identifying
-                 information is always viewable by the administrators and owners of cbdcaregroups.com.  The owners of cbdcaregroups.com
-,
-                 also reserve the right to reveal the user’s identity (or any other related information collected herein) as required by law.
-                 <br><br>
-                 cbdcaregroups.com shall maintain a money-back policy for all of its products less a reasonable restocking fee.
-                 Vendors selling products through cbdcaregroups.com shall also be subject to such.  Any customized products created for
-                 the user such as business cards and debit-cards are exempt from a return policy.
-                 <br><br>
-                 Registered users can request that their benefits be cancelled at any time, without cost, through cbdcaregroups.com website.
-                 <br><br>
-                 This Registration Agreement shall be governed by, and construed and enforced in accordance with the laws of the
-                United States without regard to conflicts of law, principles or provisions.
 
 
-             </div>
+            These Terms and Conditions exist for the benefit of KineticGold.org and its registered trademark KineticGold,
+            at its sole discretion, reserves the right to terminate the account of any registered user caught in violation
+             of the Terms and Conditions without notice.
+            <br><br>
+            KineticGold.org reserves the right to change these terms and conditions at any time, and you agree to abide
+            by the most recent version of this Terms and Conditions Agreement each time you view and use the website KineticGold, KineticGold's
+             debit cards, and/or training material.  Do not use the business cards, debit cards and/or training material
+             if you do not agree to all of these terms and conditions.
+            <br><br>
+             KineticGold reserves the right to update, modify, add or change these terms and conditions without notice.
+                                    <br><br>
+             ILLEGAL ACTIVITY<br>
+            The member agrees not to use KineticGold.org or any of it's linked websites for promotions of any page, newsgroup, email or any distribution
+            method that KineticGold, its Internet Service Providers, or otherwise is considered unlawful
+            according to any controlling legal authority.  This includes, but is not limited to, pornography, computer viruses,
+            prostitution, obscenity or Spam.
+            <br><br>
+            THIRD-PARTY <br>
+            KineticGold will in no way be liable for the actions of third parties that may in any way cause members harm.
+             <br><br>
+            TERMINATION<br>
+            The registration and/or Membership of any reg KineticGold user can be terminated at anytime by KineticGold without giving any reason.  Registered Users can cancel their account anytime directly from their Personal Options pages on the website.
+            <br><br>
+            PAYMENT PROCESSOR<br>
+            KineticGold Rewards and Profit Sharing is paid through the KineticGold account and its related Debit card.
+
+            <br><br>
+            ETHICS<br>
+            Any Registered User acting in an unethical or unprofessional manner may be terminated with all future Profit Sharing and Rewards forfeited.
+            Any accrued Profit Sharing or Rewards earned prior to termination will be available for the usage of the User.
+            <br><br>
+            CANCELLATION/TERMINATION OF MEMBERSHIP<br>
+            Any Registered User that is canceled/terminated by KineticGold will loose all benefits associated with KineticGold.  Any sponsored organization
+            be reassigned to KineticGold.org.  The member will be allow to close out and receive all funds associated with the account.
+            <br><br>
+            RELATIONSHIP OF PARTIES<br>
+            While the parties shall work hand-in-hand for the benefit of both, the parties acknowledge and agree that the Registered User shall,
+            from a legal perspective, act as an independent contractor and is not an employee of KineticGold.  Nothing in this Agreement or these
+            Terms and Conditions shall create a partnership, joint venture, agency, or franchise between the parties in the legal sense of these
+            terms.  The Registered Use has no authority to sign any document in behalf of KineticGold.
+            <br><br>
+            DISPUTES<br>
+            Agreements shall be interpreted under the the laws of the Nevis.  <br><br>
+            COLATERIAL<br>
+            All KineticGold logos, and graphics contained within KineticGold.org are copyrighted.
+             <br><br>
+            USE<br>
+              Distribution or copyrighted KineticGold material is expressly prohibited.  KineticGold.org provides Registered Users with approved KineticGold literature
+              for marketing.  Any 3rd party or self-created KineticGold material must be first approved by KineticGold.org before it may be used.
+            <br><br>
+            DAMAGING INTENT<br>
+            Any Registered User of KineticGold who engages in chat, email, posting of any material about KineticGold, or that could be damaging to KineticGold or KineticGold.org,
+            may be santioned by KineticGold.  Depending on seriousness, KineticGold may deem it appropriate to take appropriate action.
+            <br><br>
+             UCE/UBE or SPAM<br>
+              KineticGold.org strictly prohibits the use of SPAM.  This enforcement is at the sole discretion of KineticGold.org and for the
+        benefit of all Registered Users.  Registered Users proven to be participating in such activities will have their
+        account(s) terminated and they will forfeit any and all current or future Profit Sharing.
+            <br><br>
+            CONTACT INFORMATION<br>
+        It is the responsibility of the Registered User to Keep their personal records up to date at the members area.
+        KineticGold.org will not be responsible for communication error due to incorrect or out of date contact information.
+        Continued failed attempts to make contact with you may result in the termination of your account.
+            <br><br>
+            TAX<br>
+            You, the Registered User, are responsible for any and all taxes payable in your resident jurisdiction, for any income you
+            receive either from KineticGolds Rewards or Profit Sharing.
+            <br><br>
+            NO WARRANTY<br>
+            All information, training, etc. is provided “As Is” and without any warranty, express, implied, or otherwise,
+            regarding its accuracy or performance.
+            <br><br>
+            NO ASSIGNMENT<br>
+            Registered Users may not assign or transfer any rights or obligations under this Agreement without the prior
+            written consent of KineticGold.
+            <br><br>
+    SEVERABILITY
+    <br>
+            Registered Users and KineticGold.org agree that if any portion of this agreement is found illegal or unenforceable,
+            that portion shall be severed and the remainder of the agreement shall be given full force and effect.
+            <br><br>  LIMITATION OF LIABILITY; SOLE AND EXCLUSIVE: INDEMNIFICATION<br>
+            You agree that KineticGold.org is NOT responsible for the success or failure of your business or your business decisions
+            relating to any information presented by our company or our companies produces and/or services.
+            <br><br>
+  ACCURATENESS OF INFORMATION<br>
+            KineticGold.org makes no representations as to the accurateness or reliability of any advertising message or
+            marketing tool offered through any referral, outside company or on this site.  While we believe the people/companies
+            we work with to be honest and ethical, we recommend you do your own due diligence relating to any specific claims
+            or promised made in the marketing message, as it relates to your use and purchase.
+            <br><br>
+            CROSS SPONSORING/CROSS RECRUITING<br>
+            Cross sponsoring, cross recruiting and cross line jumping are prohibited per the company’s terms and conditions and the
+            Participants agreement.  “Cross Sponsoring” means a soliciting a Register User or any closely related person or
+            entity into a downline different from the existing downline for the Associate, or a closely related person or entity.
+             “Cross jumping” means an Registered User or any closely related person or entity voluntarily taking a business
+             that is not in the same downline as the one in which the Registered User first was placed.  “Closely related person or entity”
+             is any person in the household of the Registered User [e.g. Spouse, Son, Daughter, Parents living in the same household]
+             or any Legal entity which is controlled by the Registered User.
+            <br><br>
+            INCOME REPRESENTATION<br>
+            Marketing Associates shall not make any income representations except those set forth herein or otherwise specifically
+            set forth in official company material
+            <br><br>
+
+
+
+
+            </div>
 
         </div>
     </div>
